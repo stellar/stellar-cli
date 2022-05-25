07fa9f2465f3beabcf66998a3edec94849a52d2f
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-05-24 19:45:34 -0700

    Initial commit (#3)

---
+++ src/invoke.rs
@@ -0,0 +1,82 @@
+use std::{error::Error, fmt::Debug, fmt::Display, fs};
+
+use clap::Parser;
+use stellar_contract_env_host::{
+    xdr::{Error as XdrError, ScVal, ScVec},
+    Host, Vm,
+};
+
+use crate::strval::{self, StrValError};
+
+#[derive(Parser, Debug)]
+pub struct Invoke {
+    #[clap(long, parse(from_os_str))]
+    file: std::path::PathBuf,
+    #[clap(long = "fn")]
+    function: String,
+    #[clap(long = "arg", multiple_occurrences = true)]
+    args: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum InvokeError {
+    Error(Box<dyn Error>),
+    StrValError(StrValError),
+    XdrError(XdrError),
+}
+
+impl Error for InvokeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::Error(e) => e.source(),
+            Self::StrValError(e) => e.source(),
+            Self::XdrError(e) => e.source(),
+        }
+    }
+}
+
+impl Display for InvokeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invoke error: ")?;
+        Ok(match self {
+            Self::Error(e) => std::fmt::Display::fmt(&e, f)?,
+            Self::StrValError(e) => std::fmt::Display::fmt(&e, f)?,
+            Self::XdrError(e) => std::fmt::Display::fmt(&e, f)?,
+        })
+    }
+}
+
+impl From<Box<dyn Error>> for InvokeError {
+    fn from(e: Box<dyn Error>) -> Self {
+        Self::Error(e)
+    }
+}
+
+impl From<StrValError> for InvokeError {
+    fn from(e: StrValError) -> Self {
+        Self::StrValError(e)
+    }
+}
+
+impl From<XdrError> for InvokeError {
+    fn from(e: XdrError) -> Self {
+        Self::XdrError(e)
+    }
+}
+
+impl Invoke {
+    pub fn run(&self) -> Result<(), InvokeError> {
+        let contents = fs::read(&self.file).unwrap();
+        let mut h = Host::default();
+        let vm = Vm::new(&h, &contents).unwrap();
+        let args = self
+            .args
+            .iter()
+            .map(|a| strval::from_string(&h, a))
+            .collect::<Result<Vec<ScVal>, StrValError>>()?;
+        let res = vm.invoke_function(&mut h, &self.function, &ScVec(args.try_into()?))?;
+        let res_str = strval::to_string(&h, res);
+        println!("{}", res_str);
+        Ok(())
+    }
+}
---
+++ src/strval.rs
@@ -0,0 +1,106 @@
+use std::{error::Error, fmt::Display};
+
+use stellar_contract_env_host::{
+    xdr::{ScObject, ScVal},
+    Host,
+};
+
+#[derive(Debug)]
+pub enum StrValError {
+    UnknownError,
+    UnknownType,
+    InvalidNumberOfParts,
+    InvalidValue,
+}
+
+impl Error for StrValError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::UnknownError => None,
+            Self::UnknownType => None,
+            Self::InvalidNumberOfParts => None,
+            Self::InvalidValue => None,
+        }
+    }
+}
+
+impl Display for StrValError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "parse error: ")?;
+        Ok(match self {
+            Self::UnknownError => write!(f, "an unknown error occurred")?,
+            Self::UnknownType => write!(f, "unknown type specified")?,
+            Self::InvalidNumberOfParts => {
+                write!(f, "wrong number of parts must be 2 separated by colon (:)")?
+            }
+            Self::InvalidValue => write!(f, "value is not parseable to type")?,
+        })
+    }
+}
+
+impl From<std::num::ParseIntError> for StrValError {
+    fn from(_: std::num::ParseIntError) -> Self {
+        StrValError::InvalidValue
+    }
+}
+
+impl From<()> for StrValError {
+    fn from(_: ()) -> Self {
+        StrValError::UnknownError
+    }
+}
+
+pub fn from_string(_h: &Host, s: &str) -> Result<ScVal, StrValError> {
+    let parts: Vec<&str> = s.split(':').collect();
+    if parts.len() != 2 {
+        return Err(StrValError::InvalidNumberOfParts);
+    }
+    let typ = parts[0];
+    let val = parts[1];
+    let val: ScVal = match typ {
+        "i32" => ScVal::I32(val.parse()?),
+        "u32" => ScVal::U32(val.parse()?),
+        "i64" => {
+            let v: i64 = val.parse()?;
+            if v >= 0 {
+                ScVal::U63(v as u64)
+            } else {
+                ScVal::Object(Some(Box::new(ScObject::I64(val.parse()?))))
+            }
+        }
+        "u64" => ScVal::Object(Some(Box::new(ScObject::U64(val.parse()?)))),
+        _ => return Err(StrValError::UnknownType),
+    };
+    Ok(val)
+}
+
+pub fn to_string(_h: &Host, v: ScVal) -> String {
+    match v {
+        ScVal::I32(v) => format!("i32:{}", v),
+        ScVal::U32(v) => format!("u32:{}", v),
+        ScVal::U63(v) => format!("i64:{}", v),
+        ScVal::Static(_) => todo!(),
+        ScVal::Symbol(_) => todo!(),
+        ScVal::Bitset(_) => todo!(),
+        ScVal::Status(_) => todo!(),
+        ScVal::Object(None) => panic!(""),
+        ScVal::Object(Some(b)) => match *b {
+            ScObject::Box(_) => todo!(),
+            ScObject::Vec(_) => todo!(),
+            ScObject::Map(_) => todo!(),
+            ScObject::U64(v) => format!("u64:{}", v),
+            ScObject::I64(v) => format!("i64:{}", v),
+            ScObject::String(_) => todo!(),
+            ScObject::Binary(_) => todo!(),
+            ScObject::Bigint(_) => todo!(),
+            ScObject::Bigrat(_) => todo!(),
+            ScObject::Ledgerkey(_) => todo!(),
+            ScObject::Operation(_) => todo!(),
+            ScObject::OperationResult(_) => todo!(),
+            ScObject::Transaction(_) => todo!(),
+            ScObject::Asset(_) => todo!(),
+            ScObject::Price(_) => todo!(),
+            ScObject::Accountid(_) => todo!(),
+        },
+    }
+}
