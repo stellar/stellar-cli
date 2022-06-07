8bc2d671760a8752293df514e39e44375591068b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-06-06 17:24:36 -0700

    Add inspect sub-command (#11)

---
+++ src/inspect.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use std::{fmt::Debug, fs, io};
+use stellar_contract_env_host::{Host, HostError, Vm};
+
+use crate::contractid;
+
+#[derive(Parser, Debug)]
+pub struct Inspect {
+    #[clap(long, parse(from_os_str))]
+    file: std::path::PathBuf,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("io")]
+    Io(#[from] io::Error),
+    #[error("host")]
+    Host(#[from] HostError),
+}
+
+impl Inspect {
+    pub fn run(&self) -> Result<(), Error> {
+        let contents = fs::read(&self.file)?;
+        let h = Host::default();
+        let vm = Vm::new(&h, contractid::ZERO, &contents)?;
+        println!("Functions:");
+        for f in vm.functions() {
+            println!(
+                " • {} ({}) -> ({})",
+                f.name,
+                vec!["val"; f.param_count].join(", "),
+                vec!["res"; f.result_count].join(", ")
+            );
+        }
+        Ok(())
+    }
+}
