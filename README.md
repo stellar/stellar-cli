8779f0adf5b4627518e915676712afbd9c053451
Siddharth Suresh <siddharth@stellar.org>
2022-07-19 10:13:56 -0700

    invoke with contractID or wasm file

--- README.md
+++ README.md
@@ -10,35 +10,41 @@ cargo install stellar-contract-cli --git https://github.com/stellar/stellar-cont
 
 ## Usage
 
-### Directly invoking a function in auser specified WASM contract
+All values passed to `--arg` are the JSON representation of SCVals.
+
+### Directly invoking a function in a user specified WASM contract
 
 ```
-stellar-contract-cli invoke --file <WASMFILE> vm-fn --fn <FUNCNAME> --arg i32:1 --arg i32:2
+stellar-contract-cli invoke --file <WASMFILE> vm-fn --fn <FUNCNAME> --arg '{"i32":32}' --arg '{"i32":4}'
 ```
 
 Example using the [example_add_i32](https://github.com/stellar/rs-stellar-contract-sdk/tree/main/examples/add_i32) contract:
 
 ```
-$ stellar-contract-cli invoke --file example_add_i32.wasm --fn add --arg i32:1 --arg i32:2
-i32:3
+$ stellar-contract-cli invoke --file example_add_i32.wasm --fn add --arg '{"i32":32}' --arg '{"i32":4}'
+
+{
+  "i32": 36
+}
 ```
 
-### Calling a host function
-
-For both options below, the file should contain a JSON representation of the `SCVec` xdr object, which is passed directly to `invoke_function` in the host. The order of the items in the `SCVec` is specified below.
+### Creating and invoking a contract in storage
 
 #### Creating a contract
 
 ```
-// args = SCVec(contractCode, salt, ed25519, signature)
-stellar-contract-cli invoke --file <JSONARGS> create-contract
+Example:
+stellar-contract-cli deploy --id 3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c --file example_add_i32.wasm
 ```
-
-The input file is the 
 
 #### Invoking an existing contract
 
 ```
-// args = SCVec(contractID, functionName, args...)
-stellar-contract-cli invoke --file <JSONARGS> call
+stellar-contract-cli invoke --fn add --id <HEX_CONTRACTID>  --arg '{"i32":32}' --arg '{"i32":4}'
+
+Example:
+stellar-contract-cli invoke --fn add --id 3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c --arg '{"i32":32}' --arg '{"i32":4}'
+
+
+stellar-contract-cli invoke --fn put --id 3d4017c3e843895a92b70aa74d1b7ebc9c982ccf2ec4968cc0cd55f12af4660c --arg '{"symbol": [69, 50, 53, 49, 57]}' --arg '{"symbol": [69, 50, 53]}'
 ```
--- src/invoke.rs
+++ src/invoke.rs
@@ -1,57 +1,41 @@
-use std::{
-    fmt::Debug,
-    fs::{self, File},
-    io,
-    rc::Rc,
-};
+use std::{fmt::Debug, fs, io, rc::Rc};
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
 use stellar_contract_env_host::{
     budget::CostType,
     storage::Storage,
-    xdr::{Error as XdrError, HostFunction, ScVal, ScVec},
+    xdr::{Error as XdrError, HostFunction, ScObject, ScVal, ScVec},
     Host, HostError, Vm,
 };
 
-use crate::strval::{self, StrValError};
+use hex::{FromHex, FromHexError};
 
 use crate::snapshot;
 
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Call,
-    VmFn {
-        /// Name of function to invoke
-        #[clap(long = "fn")]
-        function: String,
-        /// Argument to pass to the contract function
-        #[clap(long = "arg", value_name = "arg", multiple = true)]
-        args: Vec<String>,
-    },
-}
-
 #[derive(Parser, Debug)]
 pub struct Cmd {
-    //TODO: move this into each subcommand instead?
-    /// If command == vm-fn, then this is the WASM file containing contract, otherwise, it's a JSON SCVec
-    #[clap(long, parse(from_os_str))]
-    file: std::path::PathBuf,
+    /// Name of function to invoke
+    #[clap(long = "fn")]
+    function: String,
     /// File to read and write ledger
     #[clap(long, parse(from_os_str), default_value("ledger.json"))]
     snapshot_file: std::path::PathBuf,
     /// Output the cost of the invocation to stderr
     #[clap(long = "cost")]
     cost: bool,
-    #[clap(subcommand)]
-    command: Commands,
+    #[clap(long, parse(from_os_str))]
+    file: Option<std::path::PathBuf>,
+    #[clap(long = "id", conflicts_with("file"))]
+    contract_id: Option<String>,
+    /// Argument to pass to the contract function
+    #[clap(long = "arg", value_name = "arg", multiple = true)]
+    args: Vec<String>,
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("io")]
     Io(#[from] io::Error),
-    #[error("strval")]
-    StrVal(#[from] StrValError),
     #[error("xdr")]
     Xdr(#[from] XdrError),
     #[error("host")]
@@ -60,6 +44,8 @@ pub enum Error {
     Snapshot(#[from] snapshot::Error),
     #[error("serde")]
     Serde(#[from] serde_json::Error),
+    #[error("hex")]
+    FromHex(#[from] FromHexError),
 }
 
 impl Cmd {
@@ -74,25 +60,32 @@ impl Cmd {
 
         let mut h = Host::with_storage(storage);
 
-        match &self.command {
-            Commands::Call => {
-                let mut file = File::open(&self.file).unwrap();
-                let args: ScVec = serde_json::from_reader(&mut file)?;
-                let _res = h.invoke_function(HostFunction::Call, args)?;
-            }
-            Commands::VmFn { function, args } => {
-                let contents = fs::read(&self.file).unwrap();
-                let args = args
-                    .iter()
-                    .map(|a| strval::from_string(&h, a))
-                    .collect::<Result<Vec<ScVal>, StrValError>>()?;
+        let args = &self
+            .args
+            .iter()
+            .map(|a| serde_json::from_str(a))
+            .collect::<Result<Vec<ScVal>, serde_json::Error>>()?;
 
-                //TODO: contractID should be user specified
-                let vm = Vm::new(&h, [0; 32].into(), &contents).unwrap();
-                let res = vm.invoke_function(&h, function, &ScVec(args.try_into()?))?;
-                let res_str = strval::to_string(&h, res);
-                println!("{}", res_str);
-            }
+        if let Some(f) = &self.file {
+            let contents = fs::read(f).unwrap();
+
+            //TODO: contractID should be user specified
+            let vm = Vm::new(&h, [0; 32].into(), &contents).unwrap();
+            let res = vm.invoke_function(&h, &self.function, &ScVec(args.try_into()?))?;
+            println!("{}", serde_json::to_string_pretty(&res)?);
+        } else if let Some(id) = &self.contract_id {
+            let contract_id: [u8; 32] = FromHex::from_hex(id)?;
+
+            let mut complete_args = vec![
+                ScVal::Object(Some(ScObject::Binary(contract_id.try_into()?))),
+                ScVal::Symbol((&self.function).try_into()?),
+            ];
+            complete_args.extend_from_slice(args.as_slice());
+
+            let res = h.invoke_function(HostFunction::Call, complete_args.try_into()?)?;
+            println!("{}", serde_json::to_string_pretty(&res)?);
+        } else {
+            panic!("exactly one of file or contract_id need to be specified");
         }
 
         if self.cost {
