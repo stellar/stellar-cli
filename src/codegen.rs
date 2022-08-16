5ff0f149af3df29ff84195d995520b3fa571e96b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-16 00:13:08 -0700

    update

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,14 +1,7 @@
-use std::{
-    fmt::Debug,
-    fs,
-    io::{self, Cursor},
-};
+use std::fmt::Debug;
 
 use clap::Parser;
-use soroban_env_host::{
-    xdr::{self, ReadXdr, ScSpecEntry},
-    Host, HostError, Vm,
-};
+use soroban_spec::GenerateFromFileError;
 
 #[derive(Parser, Debug)]
 pub struct Cmd {
@@ -19,14 +12,8 @@ pub struct Cmd {
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("xdr")]
-    Xdr(#[from] xdr::Error),
-    #[error("io")]
-    Io(#[from] io::Error),
-    #[error("host")]
-    Host(#[from] HostError),
-    #[error("contract spec not found")]
-    ContractSpecNotFound,
+    #[error("generate contract spec from file")]
+    GenerateFromFile(GenerateFromFileError),
     #[error("parse for format error")]
     ParseForFormatError(syn::Error),
 }
@@ -34,25 +21,19 @@ pub enum Error {
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
         let wasm_path_str = self.wasm.to_string_lossy();
-        let contents = fs::read(&self.wasm)?;
-        let h = Host::default();
-        let vm = Vm::new(&h, [0; 32].into(), &contents)?;
-        println!("// File: {}", wasm_path_str);
-        if let Some(spec) = vm.custom_section("contractspecv0") {
-            println!("// Contract Spec: {}", base64::encode(spec));
-            let mut cursor = Cursor::new(spec);
-            let specs = ScSpecEntry::read_xdr_iter(&mut cursor).collect::<Result<Vec<_>, _>>()?;
-            let code = soroban_spec::generate(&specs, &wasm_path_str);
-            let code_raw = code.to_string();
-            let (code_fmt, res) = match syn::parse_file(&code_raw) {
-                Ok(file) => (prettyplease::unparse(&file), Ok(())),
-                Err(e) => (code_raw, Err(Error::ParseForFormatError(e))),
-            };
-            println!("{}", code_fmt);
-            res
-        } else {
-            println!("// Contract Spec: Not Found");
-            Err(Error::ContractSpecNotFound)
+        let code =
+            soroban_spec::generate_from_file(&wasm_path_str).map_err(Error::GenerateFromFile)?;
+        let code_raw = code.to_string();
+        match syn::parse_file(&code_raw) {
+            Ok(file) => {
+                let code_fmt = prettyplease::unparse(&file);
+                println!("{}", code_fmt);
+                Ok(())
+            }
+            Err(e) => {
+                println!("{}", code_raw);
+                Err(Error::ParseForFormatError(e))
+            }
         }
     }
 }
