13a51da46ee7d9553554f703ef28d83443367646
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-08-23 16:59:21 -0700

    Use a parser independent of Vm/Host for inspecting wasm files (#84)

--- src/inspect.rs
+++ src/inspect.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use soroban_env_host::{
     xdr::{self, ReadXdr, ScEnvMetaEntry, ScSpecEntry},
-    Host, HostError, Vm,
+    HostError,
 };
 use std::{fmt::Debug, fs, io, io::Cursor, str::Utf8Error};
 
@@ -14,6 +14,8 @@ pub struct Cmd {
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error("parse wasm")]
+    WasmParse(wasmparser::BinaryReaderError),
     #[error("xdr")]
     Xdr(#[from] xdr::Error),
     #[error("io")]
@@ -26,20 +28,25 @@ pub enum Error {
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
-        let contents = fs::read(&self.wasm)?;
-        let h = Host::default();
-        let vm = Vm::new(&h, [0; 32].into(), &contents)?;
         println!("File: {}", self.wasm.to_string_lossy());
-        println!("Functions:");
-        for f in vm.functions() {
-            println!(
-                " • {} ({}) -> ({})",
-                f.name,
-                vec!["val"; f.param_count].join(", "),
-                vec!["res"; f.result_count].join(", ")
-            );
+
+        let contents = fs::read(&self.wasm)?;
+
+        let mut env_meta: Option<&[u8]> = None;
+        let mut spec: Option<&[u8]> = None;
+        for payload in wasmparser::Parser::new(0).parse_all(&contents) {
+            let payload = payload.map_err(Error::WasmParse)?;
+            if let wasmparser::Payload::CustomSection(section) = payload {
+                let out = match section.name() {
+                    "contractenvmetav0" => &mut env_meta,
+                    "contractspecv0" => &mut spec,
+                    _ => continue,
+                };
+                *out = Some(section.data());
+            };
         }
-        if let Some(env_meta) = vm.custom_section("contractenvmetav0") {
+
+        if let Some(env_meta) = env_meta {
             println!("Env Meta: {}", base64::encode(env_meta));
             let mut cursor = Cursor::new(env_meta);
             for env_meta_entry in ScEnvMetaEntry::read_xdr_iter(&mut cursor) {
@@ -52,7 +59,8 @@ impl Cmd {
         } else {
             println!("Env Meta: None");
         }
-        if let Some(spec) = vm.custom_section("contractspecv0") {
+
+        if let Some(spec) = spec {
             println!("Contract Spec: {}", base64::encode(spec));
             let mut cursor = Cursor::new(spec);
             for spec_entry in ScSpecEntry::read_xdr_iter(&mut cursor) {
