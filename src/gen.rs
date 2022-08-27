fb2d06287bbb00a6ff53646d2d73be2c79f9371f
tamirms <tamirms@gmail.com>
2022-08-27 01:17:40 +0100

    Add json output option for gen command (#91)
    Co-authored-by: Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>

--- src/gen.rs
+++ src/gen.rs
@@ -1,7 +1,7 @@
 use std::fmt::Debug;
 
 use clap::{ArgEnum, Parser};
-use soroban_spec::gen::rust;
+use soroban_spec::gen::{json, rust};
 
 #[derive(Parser, Debug)]
 pub struct Cmd {
@@ -17,6 +17,8 @@ pub struct Cmd {
 pub enum Output {
     /// Rust trait, client bindings, and test harness
     Rust,
+    /// Json representation of contract spec types
+    Json,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -24,13 +26,16 @@ pub enum Error {
     #[error("generate rust from file: {0}")]
     GenerateRustFromFile(rust::GenerateFromFileError),
     #[error("format rust error: {0}")]
-    FormatRustError(syn::Error),
+    FormatRust(syn::Error),
+    #[error("generate json from file: {0}")]
+    GenerateJsonFromFile(json::GenerateFromFileError),
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
         match self.output {
             Output::Rust => self.generate_rust(),
+            Output::Json => self.generate_json(),
         }
     }
 
@@ -47,8 +52,16 @@ impl Cmd {
             }
             Err(e) => {
                 println!("{}", code_raw);
-                Err(Error::FormatRustError(e))
+                Err(Error::FormatRust(e))
             }
         }
+    }
+
+    pub fn generate_json(&self) -> Result<(), Error> {
+        let wasm_path_str = self.wasm.to_string_lossy();
+        let json =
+            json::generate_from_file(&wasm_path_str, None).map_err(Error::GenerateJsonFromFile)?;
+        println!("{}", json);
+        Ok(())
     }
 }
