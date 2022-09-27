60325d7931343c8b15cf21634bd6489fab8594d3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-09-26 21:22:35 -0700

    Support getting all data for contract with read command (#155)
    * Support getting all data for contract with read command
    
    * Add option for choosing which account invoke happens as (#156)
    
    * Revert "Add option for choosing which account invoke happens as (#156)"
    
    This reverts commit 8790e206f296b9d3abd35bf1b7099e2867927e11.
    
    * allow

--- src/read.rs
+++ src/read.rs
@@ -1,12 +1,14 @@
-use std::{fmt::Debug, rc::Rc};
+use std::{
+    fmt::Debug,
+    io::{self, stdout},
+};
 
 use clap::{ArgEnum, Parser};
 use hex::FromHexError;
 use soroban_env_host::{
-    storage::Storage,
     xdr::{
-        self, Error as XdrError, LedgerEntryData, LedgerKey, LedgerKeyContractData, ReadXdr,
-        ScSpecTypeDef, ScVal, WriteXdr,
+        self, ContractDataEntry, Error as XdrError, LedgerEntryData, LedgerKey,
+        LedgerKeyContractData, ReadXdr, ScSpecTypeDef, ScVal, WriteXdr,
     },
     HostError,
 };
@@ -52,8 +54,6 @@ pub enum Error {
     CannotParseKey { key: String, error: StrValError },
     #[error("parsing XDR key {key}: {error}")]
     CannotParseXdrKey { key: String, error: XdrError },
-    #[error("missing key argument")]
-    MissingKey,
     #[error("reading file {filepath}: {error}")]
     CannotReadLedgerFile {
         filepath: std::path::PathBuf,
@@ -71,6 +71,10 @@ pub enum Error {
         result: ScVal,
         error: serde_json::Error,
     },
+    #[error("cannot print as csv: {error}")]
+    CannotPrintAsCsv { error: csv::Error },
+    #[error("cannot print: {error}")]
+    CannotPrintFlush { error: io::Error },
     #[error("xdr processing error: {0}")]
     Xdr(#[from] XdrError),
     #[error(transparent)]
@@ -80,6 +84,7 @@ pub enum Error {
 }
 
 impl Cmd {
+    #[allow(clippy::too_many_lines)]
     pub fn run(&self) -> Result<(), Error> {
         let contract_id: [u8; 32] =
             utils::contract_id_from_str(&self.contract_id).map_err(|e| {
@@ -89,59 +94,101 @@ impl Cmd {
                 }
             })?;
         let key = if let Some(key) = &self.key {
-            strval::from_string(key, &ScSpecTypeDef::Symbol).map_err(|e| Error::CannotParseKey {
-                key: key.clone(),
-                error: e,
-            })?
+            Some(
+                strval::from_string(key, &ScSpecTypeDef::Symbol).map_err(|e| {
+                    Error::CannotParseKey {
+                        key: key.clone(),
+                        error: e,
+                    }
+                })?,
+            )
         } else if let Some(key) = &self.key_xdr {
-            ScVal::from_xdr_base64(key.to_string()).map_err(|e| Error::CannotParseXdrKey {
-                key: key.clone(),
-                error: e,
-            })?
+            Some(
+                ScVal::from_xdr_base64(key.to_string()).map_err(|e| Error::CannotParseXdrKey {
+                    key: key.clone(),
+                    error: e,
+                })?,
+            )
         } else {
-            return Err(Error::MissingKey);
+            None
         };
 
-        // Initialize storage
         let state = snapshot::read(&self.ledger_file).map_err(|e| Error::CannotReadLedgerFile {
             filepath: self.ledger_file.clone(),
             error: e,
         })?;
+        let ledger_entries = state.1;
 
-        let snap = Rc::new(snapshot::Snap {
-            ledger_entries: state.1,
-        });
-        let mut storage = Storage::with_recording_footprint(snap);
-        let ledger_entry = storage.get(&LedgerKey::ContractData(LedgerKeyContractData {
-            contract_id: xdr::Hash(contract_id),
-            key,
-        }))?;
-
-        let value = if let LedgerEntryData::ContractData(entry) = ledger_entry.data {
-            entry.val
+        let contract_id = xdr::Hash(contract_id);
+        let entries: Vec<ContractDataEntry> = if let Some(key) = key {
+            ledger_entries
+                .get(&LedgerKey::ContractData(LedgerKeyContractData {
+                    contract_id,
+                    key,
+                }))
+                .iter()
+                .copied()
+                .cloned()
+                .filter_map(|val| {
+                    if let LedgerEntryData::ContractData(d) = val.data {
+                        Some(d)
+                    } else {
+                        None
+                    }
+                })
+                .collect()
         } else {
-            unreachable!();
+            ledger_entries
+                .iter()
+                .filter_map(|(k, v)| {
+                    if let LedgerKey::ContractData(kd) = k {
+                        if kd.contract_id == contract_id
+                            && kd.key != ScVal::Static(xdr::ScStatic::LedgerKeyContractCode)
+                        {
+                            if let LedgerEntryData::ContractData(vd) = &v.data {
+                                return Some(vd.clone());
+                            }
+                        }
+                    }
+                    None
+                })
+                .collect()
         };
 
-        match self.output {
-            Output::String => {
-                let res_str = strval::to_string(&value).map_err(|e| Error::CannotPrintResult {
-                    result: value,
-                    error: e,
-                })?;
-                println!("{}", res_str);
-            }
-            Output::Json => {
-                let res_str = serde_json::to_string_pretty(&value).map_err(|e| {
-                    Error::CannotPrintJsonResult {
-                        result: value,
+        let mut out = csv::Writer::from_writer(stdout());
+        for data in entries {
+            let output = match self.output {
+                Output::String => [
+                    strval::to_string(&data.key).map_err(|e| Error::CannotPrintResult {
+                        result: data.key.clone(),
                         error: e,
-                    }
-                })?;
-                println!("{}", res_str);
-            }
-            Output::Xdr => println!("{}", value.to_xdr_base64()?),
+                    })?,
+                    strval::to_string(&data.val).map_err(|e| Error::CannotPrintResult {
+                        result: data.val.clone(),
+                        error: e,
+                    })?,
+                ],
+                Output::Json => [
+                    serde_json::to_string_pretty(&data.key).map_err(|e| {
+                        Error::CannotPrintJsonResult {
+                            result: data.key.clone(),
+                            error: e,
+                        }
+                    })?,
+                    serde_json::to_string_pretty(&data.val).map_err(|e| {
+                        Error::CannotPrintJsonResult {
+                            result: data.val.clone(),
+                            error: e,
+                        }
+                    })?,
+                ],
+                Output::Xdr => [data.key.to_xdr_base64()?, data.val.to_xdr_base64()?],
+            };
+            out.write_record(output)
+                .map_err(|e| Error::CannotPrintAsCsv { error: e })?;
         }
+        out.flush()
+            .map_err(|e| Error::CannotPrintFlush { error: e })?;
 
         Ok(())
     }
