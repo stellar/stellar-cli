5483e4329d4b371277492544dee7d492502c0deb
Alfonso Acosta <alfonso@stellar.org>
2022-09-27 19:01:59 +0200

    WIP

--- src/deploy.rs
+++ src/deploy.rs
@@ -1,3 +1,4 @@
+use std::array::TryFromSliceError;
 use std::{fmt::Debug, fs, io};
 
 use clap::Parser;
@@ -6,17 +7,16 @@ use ed25519_dalek::Signer;
 use hex::FromHexError;
 use rand::Rng;
 use sha2::{Digest, Sha256};
-use soroban_env_host::xdr::LedgerKey::ContractData;
-use soroban_env_host::xdr::ScStatic::LedgerKeyContractCode;
 use soroban_env_host::xdr::{
-    DecoratedSignature, Hash, HashIdPreimageEd25519ContractId, HostFunction, InvokeHostFunctionOp,
-    LedgerFootprint, LedgerKey, LedgerKeyContractData, Memo, MuxedAccount, Operation,
-    OperationBody, Preconditions, ScObject, ScVal, SequenceNumber, Signature, SignatureHint,
-    Transaction, TransactionEnvelope, TransactionExt, TransactionSignaturePayload,
+    DecoratedSignature, Error as XdrError, Hash, HashIdPreimageEd25519ContractId, HostFunction,
+    InvokeHostFunctionOp, LedgerFootprint, LedgerKey::ContractData, LedgerKeyContractData, Memo,
+    MuxedAccount, Operation, OperationBody, Preconditions, ScObject,
+    ScStatic::LedgerKeyContractCode, ScVal, SequenceNumber, Signature, SignatureHint, Transaction,
+    TransactionEnvelope, TransactionExt, TransactionSignaturePayload,
     TransactionSignaturePayloadTaggedTransaction, TransactionV1Envelope, Uint256, VecM, WriteXdr,
 };
-use soroban_env_host::{xdr::Error as XdrError, HostError};
-use stellar_strkey::{DecodeError, StrkeyPrivateKeyEd25519};
+use soroban_env_host::HostError;
+use stellar_strkey::StrkeyPrivateKeyEd25519;
 
 use crate::snapshot::{self, get_default_ledger_info};
 use crate::utils;
@@ -38,6 +38,8 @@ pub struct Cmd {
 pub enum Error {
     #[error(transparent)]
     Host(#[from] HostError),
+    #[error("internal conversion error: {0}")]
+    TryFromSliceError(#[from] TryFromSliceError),
     #[error("xdr processing error: {0}")]
     Xdr(#[from] XdrError),
     #[error("reading file {filepath}: {error}")]
@@ -93,108 +95,125 @@ impl Cmd {
         )?;
         Ok(())
     }
+}
 
-    fn build_create_contract_tx(
-        contract: Vec<u8>,
-        sequence: i64,
-        fee: u32,
-        network_passphrase: String,
-        key: ed25519_dalek::Keypair,
-    ) -> Result<TransactionEnvelope, Error> {
-        let salt = rand::thread_rng().gen::<[u8; 32]>();
+fn build_create_contract_tx(
+    contract: Vec<u8>,
+    sequence: i64,
+    fee: u32,
+    network_passphrase: String,
+    key: ed25519_dalek::Keypair,
+) -> Result<TransactionEnvelope, Error> {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
 
-        let separator =
-            b"create_contract_from_ed25519(contract: Vec<u8>, salt: u256, key: u256, sig: Vec<u8>)";
-        let mut hasher = Sha256::new();
-        hasher.update(separator);
-        hasher.update(salt);
-        hasher.update(contract);
-        let hash = hasher.finalize();
+    let separator =
+        b"create_contract_from_ed25519(contract: Vec<u8>, salt: u256, key: u256, sig: Vec<u8>)";
+    let mut hasher = Sha256::new();
+    hasher.update(separator);
+    hasher.update(salt);
+    hasher.update(contract.clone());
+    let hash = hasher.finalize();
 
-        let contract_signature = key.sign(&hash);
+    let contract_signature = key.sign(&hash);
 
-        let preimage = HashIdPreimageEd25519ContractId {
-            ed25519: Uint256(key.secret.as_bytes().clone()),
-            salt: Uint256(salt.into()),
-        };
-        let preimage_xdr = preimage.to_xdr()?;
-        let contract_id = Sha256::digest(preimage_xdr);
+    let preimage = HashIdPreimageEd25519ContractId {
+        ed25519: Uint256(key.secret.as_bytes().clone()),
+        salt: Uint256(salt.into()),
+    };
+    let preimage_xdr = preimage.to_xdr()?;
+    let contract_id = Sha256::digest(preimage_xdr);
 
-        // TODO: clean up duplicated code and check whether the type conversions here make sense
-        let contract_parameter = ScVal::Object(Some(ScObject::Bytes(contract.try_into()?)));
-        let salt_parameter = ScVal::Object(Some(ScObject::Bytes(salt.into()?)));
-        let public_key_parameter =
-            ScVal::Object(Some(ScObject::Bytes(key.public.as_bytes().into()?)));
-        let signature_parameter =
-            ScVal::Object(Some(ScObject::Bytes(contract_signature.to_bytes().into()?)));
+    // TODO: clean up duplicated code and check whether the type conversions here make sense
+    let contract_parameter = ScVal::Object(Some(ScObject::Bytes(contract.try_into()?)));
+    let salt_parameter = ScVal::Object(Some(ScObject::Bytes(salt.try_into()?)));
+    let public_key_parameter =
+        ScVal::Object(Some(ScObject::Bytes(key.public.as_bytes().try_into()?)));
+    let signature_parameter = ScVal::Object(Some(ScObject::Bytes(
+        contract_signature.to_bytes().try_into()?,
+    )));
 
-        // TODO: reorder code properly
-        let lk = LedgerKey::ContractData(LedgerKeyContractData {
-            contract_id: Hash(contract_id.into()),
-            key: ScVal::Static(LedgerKeyContractCode),
-        });
+    // TODO: reorder code properly
+    let lk = ContractData(LedgerKeyContractData {
+        contract_id: Hash(contract_id.into()),
+        key: ScVal::Static(LedgerKeyContractCode),
+    });
 
-        let parameters: VecM<ScVal, 256000> = vec![
-            contract_parameter,
-            salt_parameter,
-            public_key_parameter,
-            signature_parameter,
-        ]
-        .try_into()?;
+    let parameters: VecM<ScVal, 256000> = vec![
+        contract_parameter,
+        salt_parameter,
+        public_key_parameter,
+        signature_parameter,
+    ]
+    .try_into()?;
 
-        let op = Operation {
-            source_account: None,
-            body: OperationBody::InvokeHostFunction(InvokeHostFunctionOp {
-                function: HostFunction::CreateContract,
-                parameters: parameters.into(),
-                footprint: LedgerFootprint {
-                    read_only: Default::default(),
-                    read_write: vec![lk].into()?,
-                },
-            }),
-        };
-        let tx = Transaction {
-            source_account: MuxedAccount::Ed25519(Uint256(key.public.as_bytes().clone())),
-            fee: fee,
-            seq_num: SequenceNumber(sequence),
-            cond: Preconditions::None,
-            memo: Memo::None,
-            operations: vec![op].into()?,
-            ext: TransactionExt::V0,
-        };
+    let op = Operation {
+        source_account: None,
+        body: OperationBody::InvokeHostFunction(InvokeHostFunctionOp {
+            function: HostFunction::CreateContract,
+            parameters: parameters.into(),
+            footprint: LedgerFootprint {
+                read_only: Default::default(),
+                read_write: vec![lk].try_into()?,
+            },
+        }),
+    };
+    let tx = Transaction {
+        source_account: MuxedAccount::Ed25519(Uint256(key.public.as_bytes().clone())),
+        fee: fee,
+        seq_num: SequenceNumber(sequence),
+        cond: Preconditions::None,
+        memo: Memo::None,
+        operations: vec![op].try_into()?,
+        ext: TransactionExt::V0,
+    };
 
-        // sign the transaction
-        let passphrase_hash = Sha256::digest(network_passphrase);
-        let signature_payload = TransactionSignaturePayload {
-            network_id: Hash(passphrase_hash.into()),
-            tagged_transaction: TransactionSignaturePayloadTaggedTransaction::Tx(tx.clone()),
-        };
-        let tx_hash = Sha256::digest(signature_payload.to_xdr().into());
-        let tx_signature: Vec<u8> = key.sign(&tx_hash).into()?;
+    // sign the transaction
+    let passphrase_hash = Sha256::digest(network_passphrase);
+    let signature_payload = TransactionSignaturePayload {
+        network_id: Hash(passphrase_hash.into()),
+        tagged_transaction: TransactionSignaturePayloadTaggedTransaction::Tx(tx.clone()),
+    };
+    let tx_hash = Sha256::digest(signature_payload.to_xdr()?);
+    let tx_signature = key.sign(&tx_hash);
 
-        let decorated_signature = DecoratedSignature {
-            hint: SignatureHint(tx_signature.as_slice()[28..].into()?),
-            signature: Signature(tx_signature.into()?),
-        };
+    let decorated_signature = DecoratedSignature {
+        hint: SignatureHint(tx_signature.to_bytes()[28..].try_into()?),
+        signature: Signature(tx_signature.to_bytes().try_into()?),
+    };
 
-        let envelope = TransactionEnvelope::Tx(TransactionV1Envelope {
-            tx: tx,
-            signatures: vec![decorated_signature].into()?,
-        });
+    let envelope = TransactionEnvelope::Tx(TransactionV1Envelope {
+        tx: tx,
+        signatures: vec![decorated_signature].try_into()?,
+    });
 
-        Ok(envelope)
-    }
+    Ok(envelope)
+}
 
-    fn parse_private_key(strkey: String) -> Result<ed25519_dalek::Keypair, Error> {
-        let seed = stellar_strkey::StrkeyPrivateKeyEd25519::from_string(&strkey)
-            .map_err(|_| Error::CannotParsePrivateKey)?;
-        // TODO: improve error?
-        let secret_key = ed25519_dalek::SecretKey::from_bytes(&seed.0)
-            .map_err(|_| Error::CannotParsePrivateKey)?;
-        let public_key = (&secret_key).into();
-        Ok(ed25519_dalek::Keypair {
-            secret: secret_key,
-            public: public_key,
-        })
+fn parse_private_key(strkey: &str) -> Result<ed25519_dalek::Keypair, Error> {
+    let seed =
+        StrkeyPrivateKeyEd25519::from_string(&strkey).map_err(|_| Error::CannotParsePrivateKey)?;
+    let secret_key =
+        ed25519_dalek::SecretKey::from_bytes(&seed.0).map_err(|_| Error::CannotParsePrivateKey)?;
+    let public_key = (&secret_key).into();
+    Ok(ed25519_dalek::Keypair {
+        secret: secret_key,
+        public: public_key,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_parse_private_key() {
+        let seed = "SBFGFF27Y64ZUGFAIG5AMJGQODZZKV2YQKAVUUN4HNE24XZXD2OEUVUP";
+        let keypair = parse_private_key(seed).unwrap();
+
+        let expected_public_key: [u8; 32] = [
+            0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10,
+            0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e,
+            0x1f,
+        ];
+        assert_eq!(expected_public_key, keypair.public.to_bytes());
     }
 }
