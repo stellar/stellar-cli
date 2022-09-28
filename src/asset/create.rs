00d412dab84de76ca8d4c07dd150d60bae7ba356
Paul Bellamy <paul@stellar.org>
2022-09-28 18:44:13 +0100

    Create asset wrapper in sandbox

--- src/asset/create.rs
+++ src/asset/create.rs
@@ -1,17 +1,53 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, rc::Rc};
 
 use clap::Parser;
+use soroban_env_host::{
+    budget::Budget,
+    storage::Storage,
+    xdr::{
+        AccountId, Error as XdrError, HostFunction, PublicKey, ScHostStorageErrorCode,
+        ScObject, ScStatus, ScVal, Uint256,
+    },
+    Host, HostError,
+};
+use stellar_strkey::StrkeyPublicKeyEd25519;
+
+use crate::{
+    snapshot,
+    strval::{self, StrValError},
+};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
+    #[error(transparent)]
+    // TODO: the Display impl of host errors is pretty user-unfriendly
+    //       (it just calls Debug). I think we can do better than that
+    Host(#[from] HostError),
+    #[error("reading file {filepath}: {error}")]
+    CannotReadLedgerFile {
+        filepath: std::path::PathBuf,
+        error: snapshot::Error,
+    },
+    #[error("committing file {filepath}: {error}")]
+    CannotCommitLedgerFile {
+        filepath: std::path::PathBuf,
+        error: snapshot::Error,
+    },
+    #[error("cannot print result {result:?}: {error}")]
+    CannotPrintResult { result: ScVal, error: StrValError },
+    #[error("xdr processing error: {0}")]
+    Xdr(#[from] XdrError),
 }
 
 #[derive(Parser, Debug)]
 pub struct Cmd {
     /// Administrator account for the asset
     /// TODO: Do we need this? Or use source of deployer?
-    #[clap(long)]
-    admin: String,
+    #[clap(
+        long,
+        default_value = "GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWHF"
+    )]
+    admin: StrkeyPublicKeyEd25519,
 
     /// Number of decimal places for the asset
     #[clap(long, default_value = "7")]
@@ -24,10 +60,79 @@ pub struct Cmd {
     /// Short name of the asset, e.g. "XLM"
     #[clap(long)]
     symbol: String,
+
+    /// File to persist ledger state
+    #[clap(long, parse(from_os_str), default_value(".soroban/ledger.json"))]
+    ledger_file: std::path::PathBuf,
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
+        // Initialize storage and host
+        // TODO: allow option to separate input and output file
+        let state =
+            snapshot::read(&self.ledger_file).map_err(|e| Error::CannotReadLedgerFile {
+                filepath: self.ledger_file.clone(),
+                error: e,
+            })?;
+
+        let snap = Rc::new(snapshot::Snap {
+            ledger_entries: state.1.clone(),
+        });
+        let h = Host::with_storage_and_budget(
+            Storage::with_recording_footprint(snap),
+            Budget::default()
+        );
+
+        h.set_source_account(AccountId(PublicKey::PublicKeyTypeEd25519(Uint256(
+            self.admin.0,
+        ))));
+
+        let mut ledger_info = state.0.clone();
+        ledger_info.sequence_number += 1;
+        ledger_info.timestamp += 5;
+        h.set_ledger_info(ledger_info.clone());
+
+        // TODO: Let user specify salt (and key?)
+        let salt_val = [0u8; 32];
+
+        let res_str = self.invoke_function(&h, &salt_val)?;
+        println!("{}", res_str);
+
+        let (storage, _, _) = h.try_finish().map_err(|_h| {
+            HostError::from(ScStatus::HostStorageError(
+                ScHostStorageErrorCode::UnknownError,
+            ))
+        })?;
+
+        snapshot::commit(state.1, ledger_info, &storage.map, &self.ledger_file).map_err(|e| {
+            Error::CannotCommitLedgerFile {
+                filepath: self.ledger_file.clone(),
+                error: e,
+            }
+        })?;
         Ok(())
+    }
+
+    fn invoke_function(
+        &self,
+        h: &Host,
+        salt: &[u8; 32],
+    ) -> Result<String, Error> {
+
+        let final_args =
+            vec![
+                ScVal::Object(Some(ScObject::Bytes(salt.try_into()?))),
+            ]
+                .try_into().expect("invalid arguments");
+
+        // let res = h.invoke_function(HostFunction::CreateTokenContractWithSourceAccount, final_args)?;
+        let res = h.invoke_function(HostFunction::Call, final_args)?;
+        let res_str = strval::to_string(&res).map_err(|e| Error::CannotPrintResult {
+            result: res,
+            error: e,
+        })?;
+
+        Ok(res_str)
     }
 }
