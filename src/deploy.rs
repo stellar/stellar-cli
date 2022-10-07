c137418ab34a6ed0f37dd407cb9224b6cf1dd641
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-07 12:46:22 -0700

    Group sandbox and rpc options together

--- src/deploy.rs
+++ src/deploy.rs
@@ -19,21 +19,29 @@ use crate::rpc::{self, Client};
 use crate::snapshot::{self, get_default_ledger_info};
 use crate::utils;
 
+const HEADING_SANDBOX: &str = "OPTIONS (SANDBOX)";
+const HEADING_RPC: &str = "OPTIONS (RPC)";
+
 #[derive(Parser, Debug)]
 pub struct Cmd {
-    /// Contract ID to deploy to (if using the sandbox)
-    #[clap(long = "id", conflicts_with = "rpc-url")]
+    /// Contract ID to deploy to
+    #[clap(
+        long = "id",
+        conflicts_with = "rpc-url",
+        help_heading = HEADING_SANDBOX,
+    )]
     contract_id: Option<String>,
     /// WASM file to deploy
     #[clap(long, parse(from_os_str))]
     wasm: std::path::PathBuf,
-    /// File to persist ledger state (if using the sandbox)
+    /// File to persist ledger state
     #[clap(
         long,
         parse(from_os_str),
         default_value = ".soroban/ledger.json",
         conflicts_with = "rpc-url",
-        env = "SOROBAN_LEDGER_FILE"
+        env = "SOROBAN_LEDGER_FILE",
+        help_heading = HEADING_SANDBOX,
     )]
     ledger_file: std::path::PathBuf,
     /// RPC server endpoint
@@ -42,20 +50,30 @@ pub struct Cmd {
         conflicts_with = "contract-id",
         requires = "secret-key",
         requires = "network-passphrase",
-        env = "SOROBAN_RPC_URL"
+        env = "SOROBAN_RPC_URL",
+        help_heading = HEADING_RPC,
     )]
     rpc_url: Option<String>,
     /// Secret 'S' key used to sign the transaction sent to the rpc server
-    #[clap(long = "secret-key", env = "SOROBAN_SECRET_KEY")]
+    #[clap(
+        long = "secret-key",
+        env = "SOROBAN_SECRET_KEY",
+        help_heading = HEADING_RPC,
+    )]
     secret_key: Option<String>,
     /// Network passphrase to sign the transaction sent to the rpc server
-    #[clap(long = "network-passphrase", env = "SOROBAN_NETWORK_PASSPHRASE")]
+    #[clap(
+        long = "network-passphrase",
+        env = "SOROBAN_NETWORK_PASSPHRASE",
+        help_heading = HEADING_RPC,
+    )]
     network_passphrase: Option<String>,
 
     /// Custom salt 32-byte salt for the token id
     #[clap(
         long,
         conflicts_with_all = &["contract-id", "ledger-file"],
+        help_heading = HEADING_RPC,
     )]
     salt: Option<String>,
 }
