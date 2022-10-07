ddfc06cee4a3082430f36f672953e3f20383965e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-10-06 18:10:10 -0700

    Add git sha to version information (#198)

--- src/version.rs
+++ src/version.rs
@@ -2,13 +2,19 @@ use clap::Parser;
 use soroban_env_host::meta;
 use std::fmt::Debug;
 
+const GIT_SHA: Option<&str> = option_env!("GIT_SHA");
+
 #[derive(Parser, Debug)]
 pub struct Cmd;
 
 impl Cmd {
     #[allow(clippy::unused_self)]
     pub fn run(&self) {
-        println!("soroban-cli {}", env!("CARGO_PKG_VERSION"),);
-        println!("soroban-env interface {}", meta::INTERFACE_VERSION);
+        println!(
+            "soroban-cli {} ({})",
+            env!("CARGO_PKG_VERSION"),
+            GIT_SHA.unwrap_or_default(),
+        );
+        println!("soroban-env-interface-version: {}", meta::INTERFACE_VERSION);
     }
 }
