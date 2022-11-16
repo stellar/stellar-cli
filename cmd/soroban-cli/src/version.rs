fbe084baf0c62d658abd9170da328821665d6996
tsachiherman <24438559+tsachiherman@users.noreply.github.com>
2022-11-16 11:23:30 -0500

    fix fmt

--- cmd/soroban-cli/src/version.rs
+++ cmd/soroban-cli/src/version.rs
@@ -10,11 +10,7 @@ pub struct Cmd;
 impl Cmd {
     #[allow(clippy::unused_self)]
     pub fn run(&self) {
-        println!(
-            "soroban {} ({})",
-            env!("CARGO_PKG_VERSION"),
-            GIT_REVISION,
-        );
+        println!("soroban {} ({})", env!("CARGO_PKG_VERSION"), GIT_REVISION,);
         println!("soroban-env-interface-version: {}", meta::INTERFACE_VERSION);
     }
 }
