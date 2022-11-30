02d4f885090b0ebc7c648104fa3f4d606feebf14
Willem Wyndham <willem@ahalabs.dev>
2022-11-30 14:46:23 -0500

    feat: create Secret type and use rpassword to read secret inputs

--- cmd/soroban-cli/src/config/identity/add.rs
+++ cmd/soroban-cli/src/config/identity/add.rs
@@ -1,7 +1,9 @@
+use std::io::Write;
+
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("alias \"{name}\" invalid")]
-    AliasInvalid { name: String },
+    #[error(transparent)]
+    Secret(#[from] secret::Error),
 }
 
 #[derive(Debug, clap::Args)]
@@ -9,13 +11,8 @@ pub struct Cmd {
     /// alias to associate with the profile
     pub alias: String,
 
-    /// Add using secret_key
-    #[clap(long)]
-    pub secret_key: bool,
-
-    /// Add using 12 word seed phrase to generate secret_key
-    #[clap(long)]
-    pub seed_phrase: bool,
+    #[clap(flatten)]
+    pub secrets: SecretArgs,
 
     /// Generate a new key pair and print seed phrase
     #[clap(long)]
@@ -25,6 +22,68 @@ pub struct Cmd {
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
         println!("{self:#?}");
+
+        println!("{:#?}", self.secrets.read_secret()?);
         Ok(())
     }
+}
+
+mod secret {
+    #[derive(thiserror::Error, Debug)]
+    pub enum Error {
+        #[error("invalid: Secret Key: \"{key}\"")]
+        InvalidSecretKey { key: String },
+        #[error("seed_phrase must be 12 words long, found {len}")]
+        InvalidSeedPhrase { len: usize },
+        #[error("seceret input error")]
+        PasswordRead,
+    }
+}
+
+#[derive(Debug, clap::Args)]
+pub struct SecretArgs {
+    /// Add using secret_key
+    #[clap(long)]
+    pub secret_key: bool,
+
+    /// Add using 12 word seed phrase to generate secret_key
+    #[clap(long)]
+    pub seed_phrase: bool,
+}
+
+impl SecretArgs {
+    pub fn read_secret(&self) -> Result<Secret, secret::Error> {
+        if self.secret_key {
+            print!("Type a Secret Key: ");
+            read_password().map(Secret::PrivateKey)
+        } else if self.seed_phrase {
+            print!("Type a 12 word seed phrase: ");
+            let seed_phrase = read_password()?;
+            let seed_phrase = seed_phrase.split_whitespace().collect::<Vec<&str>>();
+            if seed_phrase.len() != 12 {
+                let len = seed_phrase.len();
+                return Err(secret::Error::InvalidSeedPhrase { len });
+            }
+            Ok(Secret::SeedPhrase(
+                seed_phrase.into_iter().map(ToString::to_string).collect(),
+            ))
+        } else {
+            Err(secret::Error::PasswordRead {})
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Secret {
+    PrivateKey(String),
+    SeedPhrase(Vec<String>),
+
+    MacOS,
+}
+
+fn read_password() -> Result<String, secret::Error> {
+    std::io::stdout()
+        .flush()
+        .map_err(|_| secret::Error::PasswordRead)?;
+    rpassword::read_password().map_err(|_| secret::Error::PasswordRead)
 }
--- cmd/soroban-cli/src/config/identity/rm.rs
+++ cmd/soroban-cli/src/config/identity/rm.rs
@@ -6,20 +6,8 @@ pub enum Error {
 
 #[derive(Debug, clap::Args)]
 pub struct Cmd {
-    /// alias to associate with the profile
+    /// alias to remove
     pub alias: String,
-
-    /// Add using secret_key
-    #[clap(long)]
-    pub secret_key: Option<bool>,
-
-    /// Add using 12 word seed phrase to generate secret_key
-    #[clap(long)]
-    pub seed_phrase: Option<bool>,
-
-    /// Generate a new key pair and print seed phrase
-    #[clap(long)]
-    pub generate: Option<bool>,
 }
 
 impl Cmd {
