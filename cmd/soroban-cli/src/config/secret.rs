140a9690d025332c0c40f70a0cf19cda24ef5542
Willem Wyndham <willem@ahalabs.dev>
2022-12-22 12:54:59 -0500

    fix: secret key args

--- cmd/soroban-cli/src/config/secret.rs
+++ cmd/soroban-cli/src/config/secret.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::io::Write;
-use stellar_strkey::{StrkeyPrivateKeyEd25519, StrkeyPublicKeyEd25519};
+use stellar_strkey::{ed25519::PrivateKey, ed25519::PublicKey};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -17,11 +17,11 @@ pub enum Error {
 #[derive(Debug, clap::Args)]
 pub struct Args {
     /// Add using secret_key
-    #[clap(long, conflicts_with = "seed_phrase")]
+    #[clap(long, conflicts_with = "seed-phrase")]
     pub secret_key: bool,
 
     /// Add using 12 word seed phrase to generate secret_key
-    #[clap(long, conflicts_with = "seceret_key")]
+    #[clap(long, conflicts_with = "secret-key")]
     pub seed_phrase: bool,
     // /// Use MacOS Keychain
     // #[clap(long)]
@@ -33,7 +33,8 @@ impl Args {
         if self.secret_key {
             println!("Type a secret key: ");
             let secret_key = read_password()?;
-            let secret_key = StrkeyPrivateKeyEd25519::from_string(&secret_key)
+
+            let secret_key = PrivateKey::from_string(&secret_key)
                 .map_err(|_| Error::InvalidSecretKey)?
                 .to_string();
             Ok(Secret::SecretKey { secret_key })
@@ -67,9 +68,9 @@ pub enum Secret {
 }
 
 trait AsKey {
-    fn public_key(&self) -> StrkeyPublicKeyEd25519;
+    fn public_key(&self) -> PublicKey;
 
-    fn private_key(&self) -> StrkeyPrivateKeyEd25519;
+    fn private_key(&self) -> PrivateKey;
 }
 
 fn read_password() -> Result<String, Error> {
