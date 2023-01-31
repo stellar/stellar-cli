de2d125e65025619bc2022e77cb50fa39282a594
tamirms <tamirms@gmail.com>
2023-01-31 16:18:18 +0000

    ingest all ledger entries

--- cmd/soroban-rpc/internal/ingest/ledgerentry.go
+++ cmd/soroban-rpc/internal/ingest/ledgerentry.go
@@ -5,7 +5,6 @@ import (
 	"io"
 
 	"github.com/stellar/go/ingest"
-	"github.com/stellar/go/xdr"
 
 	"github.com/stellar/soroban-tools/cmd/soroban-rpc/internal/db"
 )
@@ -33,50 +32,8 @@ func (r *Runner) ingestLedgerEntryChanges(ctx context.Context, reader ingest.Cha
 
 func ingestLedgerEntryChange(writer db.LedgerEntryWriter, change ingest.Change) error {
 	if change.Post == nil {
-		ledgerKey, relevant, err := getRelevantLedgerKeyFromData(change.Pre.Data)
-		if err != nil {
-			return err
-		}
-		if !relevant {
-			return nil
-		}
-
-		return writer.DeleteLedgerEntry(ledgerKey)
+		return writer.DeleteLedgerEntry(change.Pre.LedgerKey())
 	} else {
-		ledgerKey, relevant, err := getRelevantLedgerKeyFromData(change.Post.Data)
-		if err != nil {
-			return err
-		}
-		if !relevant {
-			return nil
-		}
-
-		return writer.UpsertLedgerEntry(ledgerKey, *change.Post)
+		return writer.UpsertLedgerEntry(change.Post.LedgerKey(), *change.Post)
 	}
-}
-
-func getRelevantLedgerKeyFromData(data xdr.LedgerEntryData) (xdr.LedgerKey, bool, error) {
-	var key xdr.LedgerKey
-	switch data.Type {
-	case xdr.LedgerEntryTypeAccount:
-		if err := key.SetAccount(data.Account.AccountId); err != nil {
-			return key, false, err
-		}
-	case xdr.LedgerEntryTypeTrustline:
-		if err := key.SetTrustline(data.TrustLine.AccountId, data.TrustLine.Asset); err != nil {
-			return key, false, err
-		}
-	case xdr.LedgerEntryTypeContractData:
-		if err := key.SetContractData(data.ContractData.ContractId, data.ContractData.Key); err != nil {
-			return key, false, err
-		}
-	case xdr.LedgerEntryTypeContractCode:
-		if err := key.SetContractCode(data.ContractCode.Hash); err != nil {
-			return key, false, err
-		}
-	default:
-		// we don't care about any other entry types for now
-		return key, false, nil
-	}
-	return key, true, nil
 }
