43b7cb37a70745952b0ad16eefb10326e6390e0c
Willem Wyndham <willem@ahalabs.dev>
2023-02-16 14:48:01 -0500

    feat: add `show` command and allow generating a secret key
    `config identity show` returns the identities corresponding private key.

--- cmd/soroban-cli/src/config/identity/generate.rs
+++ cmd/soroban-cli/src/config/identity/generate.rs
@@ -15,18 +15,35 @@ pub enum Error {
 pub struct Cmd {
     /// Name of identity
     pub name: String,
+
     /// Optional seed to use when generating seed phrase.
     /// Random otherwise.
     #[clap(long, short = 's')]
     pub seed: Option<String>,
 
+    /// Output the generated identity as a secret
+    #[clap(long, short = 'S')]
+    pub as_secret: bool,
+
     #[clap(flatten)]
     pub config_locator: locator::Args,
+
+    /// When generating a seceret key, which hd_path should be used from the original seed_phrase.
+    #[clap(long)]
+    pub hd_path: Option<usize>,
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
-        let secret = Secret::from_seed(self.seed.as_ref())?;
+        let seed_phrase = Secret::from_seed(self.seed.as_ref())?;
+        let secret = if self.as_secret {
+            let secret = seed_phrase.private_key(self.hd_path)?;
+            Secret::SecretKey {
+                secret_key: secret.to_string(),
+            }
+        } else {
+            seed_phrase
+        };
         self.config_locator.write_identity(&self.name, &secret)?;
         Ok(())
     }
