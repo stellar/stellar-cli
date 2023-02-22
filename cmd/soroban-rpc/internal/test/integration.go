a7a06c83e47607c2a31b452a4eadb8ed6b8e5e00
tamirms <tamirms@gmail.com>
2023-02-22 18:46:52 +0000

    Implement sendTransaction by proxying to core instead of horizon (#442)

--- cmd/soroban-rpc/internal/test/integration.go
+++ cmd/soroban-rpc/internal/test/integration.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http/httptest"
 	"os"
@@ -11,22 +10,13 @@ import (
 	"path"
 	"path/filepath"
 	"strconv"
-	"strings"
 	"sync"
 	"syscall"
 	"testing"
 	"time"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/plumbing/storer"
-	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/sirupsen/logrus"
-
-	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/clients/stellarcore"
-	"golang.org/x/mod/modfile"
 
 	"github.com/stellar/soroban-tools/cmd/soroban-rpc/internal/config"
 	"github.com/stellar/soroban-tools/cmd/soroban-rpc/internal/daemon"
@@ -40,17 +30,17 @@ const (
 	goModFile                   = "go.mod"
 	goMonorepoGithubPath        = "github.com/stellar/go"
 	friendbotURL                = "http://localhost:8000/friendbot"
+	// Needed when Core is run with ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING=true
+	checkpointFrequency = 8
 )
 
 type Test struct {
 	t *testing.T
 
 	composePath string // docker compose yml file
-	composeEnv  string // docker compose env file ( used to define env var to override the go monorepo commit )
 
-	daemon        *daemon.Daemon
-	server        *httptest.Server
-	horizonClient *horizonclient.Client
+	daemon *daemon.Daemon
+	server *httptest.Server
 
 	coreClient *stellarcore.Client
 
@@ -58,66 +48,70 @@ type Test struct {
 	shutdownCalls []func()
 }
 
-// The dockerComposeEnv and dockerComposeEnvMu are global objects, allowing multiple tests
-// to be executed and have the dockerComposeEnv being created only once.
-var dockerComposeEnv string
-var dockerComposeEnvMu sync.Mutex
-
 func NewTest(t *testing.T) *Test {
 	if os.Getenv("SOROBAN_RPC_INTEGRATION_TESTS_ENABLED") == "" {
 		t.Skip("skipping integration test: SOROBAN_RPC_INTEGRATION_TESTS_ENABLED not set")
 	}
 
-	composePath := findDockerComposePath()
-	needBuild := false
-	dockerComposeEnvMu.Lock()
-	if dockerComposeEnv == "" {
-		goMonorepoCommit := findGoMonorepoCommit(composePath)
-		dockerComposeEnv = makeDockerComposeEnv(goMonorepoCommit)
-		needBuild = true
-	}
-	dockerComposeEnvMu.Unlock()
 	i := &Test{
 		t:           t,
-		composePath: composePath,
-		composeEnv:  dockerComposeEnv,
-	}
-	if needBuild {
-		i.runComposeCommand("build")
+		composePath: findDockerComposePath(),
 	}
 	i.runComposeCommand("up", "--detach", "--quiet-pull", "--no-color")
 	i.prepareShutdownHandlers()
 	i.coreClient = &stellarcore.Client{URL: "http://localhost:" + strconv.Itoa(stellarCorePort)}
-	i.horizonClient = &horizonclient.Client{HorizonURL: "http://localhost:8000"}
 	i.waitForCore()
-	i.waitForHorizon()
+	i.waitForCheckpoint()
 	i.launchDaemon()
 
 	return i
 }
 
+func (i *Test) waitForCheckpoint() {
+	i.t.Log("Waiting for core to be up...")
+	for t := 30 * time.Second; t >= 0; t -= time.Second {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		info, err := i.coreClient.Info(ctx)
+		cancel()
+		if err != nil {
+			i.t.Logf("could not obtain info response: %v", err)
+			time.Sleep(time.Second)
+			continue
+		}
+		if info.Info.Ledger.Num <= checkpointFrequency {
+			i.t.Logf("checkpoint not reached yet: %v", info)
+			time.Sleep(time.Second)
+			continue
+		}
+		return
+	}
+	i.t.Fatal("Core could not reach checkpoint ledger after 30s")
+}
+
 func (i *Test) launchDaemon() {
+	coreBinaryPath := os.Getenv("SOROBAN_RPC_INTEGRATION_TESTS_CAPTIVE_CORE_BIN")
+	if coreBinaryPath == "" {
+		i.t.Fatal("missing SOROBAN_RPC_INTEGRATION_TESTS_CAPTIVE_CORE_BIN")
+	}
 	config := config.LocalConfig{
-		HorizonURL:                       "http://localhost:8000",
 		StellarCoreURL:                   "http://localhost:" + strconv.Itoa(stellarCorePort),
-		StellarCoreBinaryPath:            os.Getenv("SOROBAN_RPC_INTEGRATION_TESTS_CAPTIVE_CORE_BIN"),
+		CoreRequestTimeout:               time.Second * 2,
+		StellarCoreBinaryPath:            coreBinaryPath,
 		CaptiveCoreConfigPath:            path.Join(i.composePath, "captive-core-integration-tests.cfg"),
-		CaptiveCoreHTTPPort:              0,
 		CaptiveCoreStoragePath:           i.t.TempDir(),
+		CaptiveCoreHTTPPort:              0,
+		CaptiveCoreUseDB:                 true,
 		FriendbotURL:                     friendbotURL,
 		NetworkPassphrase:                StandaloneNetworkPassphrase,
 		HistoryArchiveURLs:               []string{"http://localhost:1570"},
 		LogLevel:                         logrus.DebugLevel,
-		TxConcurrency:                    10,
-		TxQueueSize:                      10,
 		SQLiteDBPath:                     path.Join(i.t.TempDir(), "soroban_rpc.sqlite"),
 		LedgerEntryStorageTimeout:        10 * time.Minute,
 		EventLedgerRetentionWindow:       17280,
 		TransactionLedgerRetentionWindow: 1440,
-		// Needed when Core is run with ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING=true
-		CheckpointFrequency: 8,
-		MaxEventsLimit:      10000,
-		DefaultEventsLimit:  100,
+		CheckpointFrequency:              checkpointFrequency,
+		MaxEventsLimit:                   10000,
+		DefaultEventsLimit:               100,
 	}
 	i.daemon = daemon.MustNew(config)
 	i.server = httptest.NewServer(i.daemon)
@@ -154,7 +148,7 @@ func (i *Test) launchDaemon() {
 func (i *Test) runComposeCommand(args ...string) {
 	integrationYaml := filepath.Join(i.composePath, "docker-compose.yml")
 
-	cmdline := append([]string{"--env-file", i.composeEnv, "-f", integrationYaml}, args...)
+	cmdline := append([]string{"-f", integrationYaml}, args...)
 	cmd := exec.Command("docker-compose", cmdline...)
 
 	i.t.Log("Running", cmd.Env, cmd.Args)
@@ -238,33 +232,6 @@ func (i *Test) waitForCore() {
 		return
 	}
 	i.t.Fatal("Core could not sync after 30s")
-}
-
-func (i *Test) waitForHorizon() {
-	for t := 60; t >= 0; t -= 1 {
-		time.Sleep(time.Second)
-
-		i.t.Log("Waiting for ingestion and protocol upgrade...")
-		root, err := i.horizonClient.Root()
-		if err != nil {
-			i.t.Logf("could not obtain root response %v", err)
-			continue
-		}
-
-		if root.HorizonSequence < 3 ||
-			int(root.HorizonSequence) != int(root.IngestSequence) {
-			i.t.Logf("Horizon ingesting... %v", root)
-			continue
-		}
-
-		if uint32(root.CurrentProtocolVersion) == stellarCoreProtocolVersion {
-			i.t.Logf("Horizon protocol version matches %d: %+v",
-				root.CurrentProtocolVersion, root)
-			return
-		}
-	}
-
-	i.t.Fatal("Horizon not ingesting...")
 }
 
 // UpgradeProtocol arms Core with upgrade and blocks until protocol is upgraded.
@@ -355,122 +322,4 @@ func findDockerComposePath() string {
 
 	// Directly jump down to the folder that should contain the configs
 	return filepath.Join(current, "cmd", "soroban-rpc", "internal", "test")
-}
-
-// load the go.mod file, and extract the version of the github.com/stellar/go entry.
-func loadParseGoMod(dir string) (string, error) {
-	fileName := path.Join(dir, goModFile)
-
-	cargoFileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Printf("Unable to read %s file : %v\n", goModFile, err)
-		return "", err
-	}
-
-	modFile, err := modfile.Parse("", cargoFileBytes, nil)
-	if err != nil {
-		fmt.Printf("Unable to read %s file : %v\n", goModFile, err)
-		return "", err
-	}
-	// scan all the stellar related required modules.
-	for _, require := range modFile.Require {
-		if !strings.Contains(require.Mod.Path, goMonorepoGithubPath) || require.Indirect {
-			continue
-		}
-		splittedVersion := strings.Split(require.Mod.Version, "-")
-		if len(splittedVersion) != 3 {
-			continue
-		}
-		return splittedVersion[2], nil
-	}
-	return "", errors.New("unable to find go monorepo")
-}
-
-func findCommitHash(shortCommitHash string) (string, error) {
-	path := goMonorepoGithubPath
-	if !strings.HasPrefix(path, "https://") {
-		path = "https://" + path
-	}
-	repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
-		URL: path,
-	})
-	if err != nil {
-		fmt.Printf("unable to clone repository at %s\n", path)
-		return "", err
-	}
-
-	lookoutCommit := strings.ToLower(shortCommitHash)
-	cIter, err := repo.Log(&git.LogOptions{
-		All:   true,
-		Order: git.LogOrderCommitterTime,
-	})
-	if err != nil {
-		fmt.Printf("unable to get log entries at %s for %s: %v\n", path, shortCommitHash, err)
-		return "", err
-	}
-	reviewedHashes := make(map[plumbing.Hash]bool, 0)
-	// ... just iterates over the commits, looking for a commit with a specific hash.
-	var revCommit string
-	var lookupRecursive func(c *object.Commit) error
-	lookupRecursive = func(c *object.Commit) error {
-		revString := strings.ToLower(c.Hash.String())
-		if strings.HasPrefix(revString, lookoutCommit) {
-			// found !
-			revCommit = revString
-			return storer.ErrStop
-		}
-		reviewedHashes[c.Hash] = true
-		for _, pHash := range c.ParentHashes {
-			if reviewedHashes[pHash] {
-				continue
-			}
-			var pCommit *object.Commit
-			pCommit, err = repo.CommitObject(pHash)
-			if err != nil {
-				fmt.Printf("unable to get commit hash %s", pHash.String())
-				return err
-			}
-			err = lookupRecursive(pCommit)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-	err = cIter.ForEach(lookupRecursive)
-	if err != nil && err != storer.ErrStop {
-		fmt.Printf("unable to iterate on log entries for %s : %v\n", path, err)
-		return "", err
-	}
-
-	if revCommit != "" {
-		return revCommit, nil
-	}
-	// otherwise, this commit might be in one of the branches.
-	return "", fmt.Errorf("unable to find full hash for commit hash '%s' for '%s'", lookoutCommit, path)
-}
-
-func findGoMonorepoCommit(composePath string) string {
-	projectRootPath := findProjectRoot(composePath)
-	shortCommitHash, err := loadParseGoMod(projectRootPath)
-	panicIf(err)
-	commitHash, err := findCommitHash(shortCommitHash)
-	panicIf(err)
-
-	return commitHash
-}
-
-func makeDockerComposeEnv(goMonorepoCommit string) string {
-	file, err := os.CreateTemp(os.TempDir(), "docker-compose-go-monorepo")
-	if err != nil {
-		fmt.Printf("Unable to create temporary file : %v\n", err)
-		panic(err)
-	}
-	fmt.Fprintf(file, "GOMONOREPO_COMMIT=%s\n", goMonorepoCommit)
-	err = file.Close()
-	if err != nil {
-		fmt.Printf("Unable to close temporary file : %v\n", err)
-		panic(err)
-	}
-	return file.Name()
 }
