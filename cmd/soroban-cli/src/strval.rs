2cfc07f06c91e8f0fccb2deccd12ef0e24ff2192
Willem Wyndham <willem@ahalabs.dev>
2023-04-06 10:07:11 -0400

    fix(CLI): Handle options (#555)
    While there was an example contract method, it wasn't being tested.
    
    Co-authored-by: Paul Bellamy <paul@stellar.org>

--- cmd/soroban-cli/src/strval.rs
+++ cmd/soroban-cli/src/strval.rs
@@ -438,7 +438,7 @@ impl Spec {
     /// May panic
     pub fn xdr_to_json(&self, val: &ScVal, output: &ScType) -> Result<Value, Error> {
         Ok(match (val, output) {
-            (ScVal::Void, ScType::Val)
+            (ScVal::Void, ScType::Val | ScType::Option(_))
             | (ScVal::Map(None) | ScVal::Vec(None), ScType::Option(_)) => Value::Null,
             (ScVal::Bool(_), ScType::Bool)
             | (ScVal::Void, ScType::Void)
@@ -463,6 +463,7 @@ impl Spec {
             | (ScVal::Address(_), ScType::Address)
             | (ScVal::Bytes(_), ScType::Bytes | ScType::BytesN(_)) => to_json(val)?,
 
+            (val, ScType::Option(inner)) => self.xdr_to_json(val, &inner.value_type)?,
             (ScVal::Map(Some(_)) | ScVal::Vec(Some(_)) | ScVal::U32(_), type_) => {
                 self.sc_object_to_json(val, type_)?
             }
