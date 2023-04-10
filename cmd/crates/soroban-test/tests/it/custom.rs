798c06c7d36d5267aed69e4fb211fd2c707c7a18
Willem Wyndham <willem@ahalabs.dev>
2023-04-10 16:18:19 -0400

    Fix: Add soroban-hello install to Makefile so custom test doesn't build

--- cmd/crates/soroban-test/tests/it/custom.rs
+++ cmd/crates/soroban-test/tests/it/custom.rs
@@ -1,27 +1,23 @@
+/*
+This function calls the soroban executable via cargo and checks that the output
+is correct. The PATH environment variable is set to include the target/bin
+directory, so that the soroban executable can be found.
+*/
+
 #[test]
-fn call_custom() {
+fn soroban_hello() {
+    // Get the current working directory
     let current_dir = std::env::current_dir().unwrap();
-    let install_path = current_dir.join("tests/fixtures/hello");
-
-    let root = current_dir
-        .join("../../../target/.bin")
-        .canonicalize()
-        .unwrap();
-    assert_cmd::Command::new("cargo")
-        .args([
-            "install",
-            "--path",
-            install_path.to_str().unwrap(),
-            "--root",
-            &format!("{root:?}"),
-        ])
-        .assert()
-        .success();
-    let bin_path = root.join("bin");
+    // Create a path to the target/bin directory
+    let target_bin_path = current_dir.join("../../../target/bin");
+    // Get the current PATH environment variable
     let path_key = std::env::var_os("PATH").unwrap();
+    // Create an iterator of paths from the PATH environment variable
     let current_paths = std::env::split_paths(&path_key);
-    let paths = std::env::join_paths(current_paths.chain(vec![bin_path])).unwrap();
+    // Add the target/bin directory to the iterator of paths
+    let paths = std::env::join_paths(current_paths.chain(vec![target_bin_path])).unwrap();
 
+    // Call soroban with the PATH variable set to include the target/bin directory
     assert_cmd::Command::cargo_bin("soroban")
         .unwrap()
         .arg("hello")
