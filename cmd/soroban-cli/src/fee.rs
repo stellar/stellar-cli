105747ac44eece96465f409e9771cb5f947fe926
Willem Wyndham <wyndham@cs.unc.edu>
2023-04-19 11:38:34 -0400

    Update cmd/soroban-cli/src/fee.rs
    Co-authored-by: Paul Bellamy <paul@paulbellamy.com>

--- cmd/soroban-cli/src/fee.rs
+++ cmd/soroban-cli/src/fee.rs
@@ -5,7 +5,7 @@ use crate::commands::HEADING_RPC;
 #[derive(Debug, clap::Args, Clone)]
 #[group(skip)]
 pub struct Args {
-    /// fee amount for transaction
+    /// fee amount for transaction, in stroops. 1 stroop = 0.0000001 xlm
     #[arg(long, default_value = "100", env = "SOROBAN_FEE", help_heading = HEADING_RPC)]
     pub fee: u32,
 }
