d4fab97bb75a67b2c66bed18acb05d2e461dcf3d
Paul Bellamy <paul@stellar.org>
2023-04-26 11:11:04 +0100

    Add validation tests

--- cmd/soroban-rpc/internal/config/config_option_test.go
+++ cmd/soroban-rpc/internal/config/config_option_test.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"go/types"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestConfigOptionGetTomlKey(t *testing.T) {
@@ -21,4 +23,71 @@ func TestConfigOptionGetTomlKey(t *testing.T) {
 
 	// Env-var not set, autogenerate from name
 	assert.Equal(t, "TEST_FLAG", ConfigOption{Name: "test-flag"}.getTomlKey())
+}
+
+func TestValidateRequired(t *testing.T) {
+	var strVal string
+	o := &ConfigOption{
+		Name:      "required-option",
+		OptType:   types.String,
+		ConfigKey: &strVal,
+		Validate:  required,
+	}
+
+	// unset
+	assert.ErrorContains(t, o.Validate(o), "required-option is required")
+
+	// set with blank value
+	require.NoError(t, o.setValue(""))
+	assert.ErrorContains(t, o.Validate(o), "required-option is required")
+
+	// set with valid value
+	require.NoError(t, o.setValue("not-blank"))
+	assert.NoError(t, o.Validate(o))
+}
+
+func TestValidatePositiveUint32(t *testing.T) {
+	var val uint32
+	o := &ConfigOption{
+		Name:      "positive-option",
+		OptType:   types.Uint32,
+		ConfigKey: &val,
+		Validate:  positive,
+	}
+
+	// unset
+	assert.ErrorContains(t, o.Validate(o), "positive-option must be positive")
+
+	// set with 0 value
+	require.NoError(t, o.setValue(uint32(0)))
+	assert.ErrorContains(t, o.Validate(o), "positive-option must be positive")
+
+	// set with valid value
+	require.NoError(t, o.setValue(uint32(1)))
+	assert.NoError(t, o.Validate(o))
+}
+
+func TestValidatePositiveInt(t *testing.T) {
+	var val int
+	o := &ConfigOption{
+		Name:      "positive-option",
+		OptType:   types.Uint32,
+		ConfigKey: &val,
+		Validate:  positive,
+	}
+
+	// unset
+	assert.ErrorContains(t, o.Validate(o), "positive-option must be positive")
+
+	// set with 0 value
+	require.NoError(t, o.setValue(0))
+	assert.ErrorContains(t, o.Validate(o), "positive-option must be positive")
+
+	// set with negative value
+	require.NoError(t, o.setValue(-1))
+	assert.ErrorContains(t, o.Validate(o), "positive-option must be positive")
+
+	// set with valid value
+	require.NoError(t, o.setValue(1))
+	assert.NoError(t, o.Validate(o))
 }
