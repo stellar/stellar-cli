b904b565bd8b8f4e09114d6af4e561d80a6a7fec
Paul Bellamy <paul@stellar.org>
2023-05-23 16:42:30 +0100

    Add test for ConfigOption.setValue

--- cmd/soroban-rpc/internal/config/config_option_test.go
+++ cmd/soroban-rpc/internal/config/config_option_test.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"go/types"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -106,7 +108,8 @@ func TestValidatePositiveInt(t *testing.T) {
 	require.NoError(t, o.setValue(1))
 	assert.NoError(t, o.Validate(o))
 }
-func TestUnassignableFiels(t *testing.T) {
+
+func TestUnassignableField(t *testing.T) {
 	var co ConfigOption
 	var b bool
 	co.Name = "mykey"
@@ -114,4 +117,148 @@ func TestUnassignableFiels(t *testing.T) {
 	err := co.setValue("abc")
 	require.Error(t, err)
 	require.Contains(t, err.Error(), co.Name)
+}
+
+func TestSetValue(t *testing.T) {
+	var b bool
+	var i int
+	var u32 uint32
+	var u64 uint64
+	var f64 float64
+	var s string
+
+	for _, scenario := range []struct {
+		name  string
+		key   interface{}
+		value interface{}
+		err   error
+	}{
+		{
+			name:  "valid-bool",
+			key:   &b,
+			value: true,
+			err:   nil,
+		},
+		{
+			name:  "valid-bool-string",
+			key:   &b,
+			value: "true",
+			err:   nil,
+		},
+		{
+			name:  "valid-bool-string-false",
+			key:   &b,
+			value: "false",
+			err:   nil,
+		},
+		{
+			name:  "valid-bool-string-uppercase",
+			key:   &b,
+			value: "TRUE",
+			err:   nil,
+		},
+		{
+			name:  "invalid-bool-string",
+			key:   &b,
+			value: "foobar",
+			err:   fmt.Errorf("invalid boolean value invalid-bool-string: foobar"),
+		},
+		{
+			name:  "invalid-bool-string",
+			key:   &b,
+			value: "foobar",
+			err:   fmt.Errorf("invalid boolean value invalid-bool-string: foobar"),
+		},
+		{
+			name:  "valid-int",
+			key:   &i,
+			value: 1,
+			err:   nil,
+		},
+		{
+			name:  "valid-int-string",
+			key:   &i,
+			value: "1",
+			err:   nil,
+		},
+		{
+			name:  "invalid-int-string",
+			key:   &i,
+			value: "abcd",
+			err:   fmt.Errorf("strconv.ParseInt: parsing \"abcd\": invalid syntax"),
+		},
+		{
+			name:  "valid-uint32",
+			key:   &u32,
+			value: 1,
+			err:   nil,
+		},
+		{
+			name:  "overflow-uint32",
+			key:   &u32,
+			value: uint64(math.MaxUint32) + 1,
+			err:   fmt.Errorf("overflow-uint32 overflows uint32"),
+		},
+		{
+			name:  "negative-uint32",
+			key:   &u32,
+			value: -1,
+			err:   fmt.Errorf("negative-uint32 cannot be negative"),
+		},
+		{
+			name:  "valid-uint",
+			key:   &u64,
+			value: 1,
+			err:   nil,
+		},
+		{
+			name:  "negative-uint",
+			key:   &u64,
+			value: -1,
+			err:   fmt.Errorf("negative-uint cannot be negative"),
+		},
+		{
+			name:  "valid-float",
+			key:   &f64,
+			value: 1.05,
+			err:   nil,
+		},
+		{
+			name:  "valid-float-int",
+			key:   &f64,
+			value: int64(1234),
+			err:   nil,
+		},
+		{
+			name:  "valid-float-string",
+			key:   &f64,
+			value: "1.05",
+			err:   nil,
+		},
+		{
+			name:  "invalid-float-string",
+			key:   &f64,
+			value: "foobar",
+			err:   fmt.Errorf("strconv.ParseFloat: parsing \"foobar\": invalid syntax"),
+		},
+		{
+			name:  "valid-string",
+			key:   &s,
+			value: "foobar",
+			err:   nil,
+		},
+	} {
+		t.Run(scenario.name, func(t *testing.T) {
+			co := ConfigOption{
+				Name:      scenario.name,
+				ConfigKey: scenario.key,
+			}
+			err := co.setValue(scenario.value)
+			if scenario.err != nil {
+				require.EqualError(t, err, scenario.err.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
 }
