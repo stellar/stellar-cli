dc7f5b8185a1c52c344c12c4125efb40e6cab653
Willem Wyndham <willem@ahalabs.dev>
2023-06-13 07:03:27 -0400

    feat!: convert bindings to subcommands and add a ts one (#644)
    * feat!: convert bindings to subcommands and add a ts one
    
    * fix: ensure target is empty
    
    * fix: remove if exists and fail if path is a file
    
    * feat: update bindings rev
    
    * fix: ensure parent dir
    
    * build: update to latest soroban-spec revision
    
    * fix: comment
    
    * build: update to latest soroban-spec revision
    
    * feat: update to new sdk
    
    * build: update to latest soroban-spec revision
    
    * feat(bindings-ts): configurable network settings
    
    * fix: update default RPC url
    
    * feat: update spec rev
    
    * feat: update spec
    
    * feat: update spec
    
    * feat: run `npm i` & `npm run build` during ts gen
    
    * build: use latest soroban-spec revision
    
    * Apply suggestions from code review
    
    Co-authored-by: Paul Bellamy <paul@paulbellamy.com>
    
    * feat: ts -> typescript and use newest spec rev
    
    * build: use latest soroban-spec revision
    
    * build: use latest soroban-spec revision
    
    * docs: make help doc match command name
    
    * feat: update spec and docs
    
    * build: use latest soroban-spec revision
    
    * build: use latest soroban-spec revision
    
    * build: use latest soroban-spec revision
    
    * build: use latest soroban-spec revision
    
    * fix: s/ts/typescript/
    
    * build: use latest soroban-spec revision
    
    * build: use released sdk version
    
    ---------
    
    Co-authored-by: Chad Ostrowski <221614+chadoh@users.noreply.github.com>
    Co-authored-by: Paul Bellamy <paul@paulbellamy.com>
    Co-authored-by: Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>

--- cmd/soroban-cli/src/commands/contract/bindings.rs
+++ cmd/soroban-cli/src/commands/contract/bindings.rs
@@ -1,70 +1,38 @@
-use std::fmt::Debug;
+pub mod json;
+pub mod rust;
+pub mod typescript;
 
-use clap::{arg, command, Parser, ValueEnum};
-use soroban_spec::gen::{
-    json,
-    rust::{self, ToFormattedString},
-};
+#[derive(Debug, clap::Subcommand)]
+pub enum Cmd {
+    /// Generate Json Bindings
+    Json(json::Cmd),
 
-use crate::wasm;
+    /// Generate Rust bindings
+    Rust(rust::Cmd),
 
-#[derive(Parser, Debug, Clone)]
-#[group(skip)]
-pub struct Cmd {
-    #[command(flatten)]
-    wasm: wasm::Args,
-    /// Type of output to generate
-    #[arg(long, value_enum)]
-    r#output: Output,
-}
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, ValueEnum)]
-pub enum Output {
-    /// Rust trait, client bindings, and test harness
-    Rust,
-    /// Json representation of contract spec types
-    Json,
+    /// Generate a TypeScript / JavaScript package
+    Typescript(typescript::Cmd),
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("generate rust from file: {0}")]
-    GenerateRustFromFile(rust::GenerateFromFileError),
-    #[error("format rust error: {0}")]
-    FormatRust(String),
-    #[error("generate json from file: {0}")]
-    GenerateJsonFromFile(json::GenerateFromFileError),
+    #[error(transparent)]
+    Json(#[from] json::Error),
+
+    #[error(transparent)]
+    Rust(#[from] rust::Error),
+
+    #[error(transparent)]
+    Typescript(#[from] typescript::Error),
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
-        match self.output {
-            Output::Rust => self.generate_rust(),
-            Output::Json => self.generate_json(),
+        match &self {
+            Cmd::Json(json) => json.run()?,
+            Cmd::Rust(rust) => rust.run()?,
+            Cmd::Typescript(ts) => ts.run()?,
         }
-    }
-
-    pub fn generate_rust(&self) -> Result<(), Error> {
-        let wasm_path_str = self.wasm.wasm.to_string_lossy();
-        let code =
-            rust::generate_from_file(&wasm_path_str, None).map_err(Error::GenerateRustFromFile)?;
-        match code.to_formatted_string() {
-            Ok(formatted) => {
-                println!("{formatted}");
-                Ok(())
-            }
-            Err(e) => {
-                println!("{code}");
-                Err(Error::FormatRust(e.to_string()))
-            }
-        }
-    }
-
-    pub fn generate_json(&self) -> Result<(), Error> {
-        let wasm_path_str = self.wasm.wasm.to_string_lossy();
-        let json =
-            json::generate_from_file(&wasm_path_str, None).map_err(Error::GenerateJsonFromFile)?;
-        println!("{json}");
         Ok(())
     }
 }
