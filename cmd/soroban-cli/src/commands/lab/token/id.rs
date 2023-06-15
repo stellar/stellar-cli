0c52112b97c9bdd45ea0b2971b60bd43ecfea6a7
Vincent Amouret <vincent.amouret@gmail.com>
2023-06-15 10:13:38 +0200

    Keep only the strkey id

--- cmd/soroban-cli/src/commands/lab/token/id.rs
+++ cmd/soroban-cli/src/commands/lab/token/id.rs
@@ -20,9 +20,8 @@ impl Cmd {
         let asset = parse_asset(&self.asset)?;
         let network = self.config.get_network()?;
         let contract_id = get_contract_id(&asset, &network.network_passphrase)?;
-        let contract_key = stellar_strkey::Contract(contract_id.0).to_string();
-        println!("Id: {contract_id}");
-        println!("Key: {contract_key}");
+        let strkey_contract_id = stellar_strkey::Contract(contract_id.0).to_string();
+        println!("{strkey_contract_id}");
         Ok(())
     }
 }
