db9aa07f197c60805079c7a010be054c1d143b95
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-06-21 17:31:49 -0400

    fix: allow generated JS libs to use localhost (#711)

--- cmd/crates/soroban-spec-typescript/fixtures/ts/src/server.ts
+++ cmd/crates/soroban-spec-typescript/fixtures/ts/src/server.ts
@@ -5,4 +5,4 @@ import { RPC_URL } from './constants.js'
  * SorobanClient.Server instance, initialized using {@link RPC_URL} used to
  * initialize this library.
  */
-export const Server = new SorobanClient.Server(RPC_URL);
+export const Server = new SorobanClient.Server(RPC_URL, { allowHttp: RPC_URL.startsWith('http://') });
