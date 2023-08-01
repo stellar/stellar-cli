6042f56ec4f53d0e5fbdb74050f43d178bc80179
Willem Wyndham <willem@ahalabs.dev>
2023-08-01 15:04:27 -0400

    fix: allow for https and provide better errors

--- cmd/soroban-cli/src/rpc/mod.rs
+++ cmd/soroban-cli/src/rpc/mod.rs
@@ -453,7 +453,10 @@ impl Client {
         let response = self.get_ledger_entries(keys).await?;
         let entries = response.entries.unwrap_or_default();
         if entries.is_empty() {
-            return Err(Error::NotFound("Account".to_string(), address.to_string()));
+            return Err(Error::NotFound("Account".to_string(), format!(r#"{address}
+Might need to fund account like:
+soroban config identity fund {address} --network <name>
+soroban config identity fund {address} --helper-url <url>"#)));
         }
         let ledger_entry = &entries[0];
         if let LedgerEntryData::Account(entry) =
