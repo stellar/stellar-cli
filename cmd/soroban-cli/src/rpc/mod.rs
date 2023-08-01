2da6148787133ec4f396ed006b5670b731e909e0
Willem Wyndham <willem@ahalabs.dev>
2023-08-01 15:18:26 -0400

    fix: docs and fmt

--- cmd/soroban-cli/src/rpc/mod.rs
+++ cmd/soroban-cli/src/rpc/mod.rs
@@ -454,10 +454,15 @@ impl Client {
         let response = self.get_ledger_entries(keys).await?;
         let entries = response.entries.unwrap_or_default();
         if entries.is_empty() {
-            return Err(Error::NotFound("Account".to_string(), format!(r#"{address}
+            return Err(Error::NotFound(
+                "Account".to_string(),
+                format!(
+                    r#"{address}
 Might need to fund account like:
 soroban config identity fund {address} --network <name>
-soroban config identity fund {address} --helper-url <url>"#)));
+soroban config identity fund {address} --helper-url <url>"#
+                ),
+            ));
         }
         let ledger_entry = &entries[0];
         let mut depth_limit_read = DepthLimitedRead::new(ledger_entry.xdr.as_bytes(), 100);
