b5af2e90aef4255beb00e2dbd63d74b251e4c844
Paul Bellamy <paul@stellar.org>
2023-08-16 14:49:22 +0100

    Update base64 dependency

--- cmd/soroban-cli/src/utils/contract_spec.rs
+++ cmd/soroban-cli/src/utils/contract_spec.rs
@@ -1,3 +1,4 @@
+use base64::{engine::general_purpose::STANDARD as base64, Engine as _};
 use std::{
     fmt::Display,
     io::{self, Cursor},
@@ -57,7 +58,7 @@ impl ContractSpec {
 
         let mut env_meta_base64 = None;
         let env_meta = if let Some(env_meta) = env_meta {
-            env_meta_base64 = Some(base64::encode(env_meta));
+            env_meta_base64 = Some(base64.encode(env_meta));
             let cursor = Cursor::new(env_meta);
             let mut depth_limit_read = DepthLimitedRead::new(cursor, 100);
             ScEnvMetaEntry::read_xdr_iter(&mut depth_limit_read)
@@ -68,7 +69,7 @@ impl ContractSpec {
 
         let mut meta_base64 = None;
         let meta = if let Some(meta) = meta {
-            meta_base64 = Some(base64::encode(meta));
+            meta_base64 = Some(base64.encode(meta));
             let cursor = Cursor::new(meta);
             let mut depth_limit_read = DepthLimitedRead::new(cursor, 100);
             ScMetaEntry::read_xdr_iter(&mut depth_limit_read)
@@ -79,7 +80,7 @@ impl ContractSpec {
 
         let mut spec_base64 = None;
         let spec = if let Some(spec) = spec {
-            spec_base64 = Some(base64::encode(spec));
+            spec_base64 = Some(base64.encode(spec));
             let cursor = Cursor::new(spec);
             let mut depth_limit_read = DepthLimitedRead::new(cursor, 100);
             ScSpecEntry::read_xdr_iter(&mut depth_limit_read)
