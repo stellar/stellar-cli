f25805890d357295c5db235b48ae8a3c2b37d11a
Paul Bellamy <paul@stellar.org>
2023-09-01 11:50:43 +0100

    fix formatting

--- cmd/soroban-cli/src/log/cost.rs
+++ cmd/soroban-cli/src/log/cost.rs
@@ -7,7 +7,7 @@ impl Debug for Cost<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         // TODO: Should we output the footprint here?
         writeln!(f, "==================== Cost ====================")?;
-        writeln!(f, "Cpu used: {}", self.0.instructions,)?;
+        writeln!(f, "CPU used: {}", self.0.instructions,)?;
         writeln!(f, "Bytes read: {}", self.0.read_bytes,)?;
         writeln!(f, "Bytes written: {}", self.0.write_bytes,)?;
         writeln!(f, "==============================================")?;
