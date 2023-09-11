41f9f8f4921529593acddb96ba4a622627e074cb
Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>
2023-09-11 15:33:02 -0400

    soroban-cli: add tests for version + xdr decoding (#947)
    * add few cli test.
    
    * linter
    
    * cargo fmt
    
    * exclude test from windows.

---
+++ cmd/crates/soroban-test/tests/it/version.rs
@@ -0,0 +1,12 @@
+use soroban_cli::commands::version::long;
+use soroban_test::TestEnv;
+
+#[test]
+fn version() {
+    let sandbox = TestEnv::default();
+    sandbox
+        .new_assert_cmd("version")
+        .assert()
+        .success()
+        .stdout(format!("soroban {}\n", long()));
+}
