340d0d9d18120a72392a44101ace6bc633e35bc4
Tsachi Herman <24438559+tsachiherman@users.noreply.github.com>
2023-09-12 17:22:58 -0400

    build: update rust dependencies (#953)
    * update
    
    * update
    
    * update
    
    * rollback some of the changes

--- cmd/soroban-cli/src/commands/version.rs
+++ cmd/soroban-cli/src/commands/version.rs
@@ -28,21 +28,9 @@ pub fn long() -> String {
         format!("soroban-env interface version {}", meta::INTERFACE_VERSION),
         format!(
             "stellar-xdr {} ({})
-xdr next ({})",
-            xdr.pkg, xdr.rev, xdr.xdr_next,
+xdr curr ({})",
+            xdr.pkg, xdr.rev, xdr.xdr_curr,
         ),
     ]
     .join("\n")
 }
-
-// Check that the XDR cannel in use is 'next' to ensure that the version output
-// is not forgotten when we eventually update to using curr. This is a bit of a
-// hack because of limits of what you can do in a constant context, but by being
-// a constant context this is checked at compile time.
-const _: () = {
-    #[allow(clippy::single_match)]
-    match soroban_env_host::VERSION.xdr.xdr.as_bytes() {
-        b"next" => (),
-        _ => panic!("xdr version channel needs updating"),
-    }
-};
