16dbe3cb1d8ba67124040d648fd63caa7a867ae0
Willem Wyndham <willem@ahalabs.dev>
2023-09-14 11:22:59 -0400

    feat: use sim preamble to restore needed entries
    Currently the test uses a sleep because it has been a struggle to get an entry's expiration

--- cmd/soroban-rpc/internal/test/cli_test.go
+++ cmd/soroban-rpc/internal/test/cli_test.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/creachadair/jrpc2"
 	"github.com/creachadair/jrpc2/jhttp"
@@ -197,7 +198,7 @@ func runSuccessfulCLICmd(t *testing.T, cmd string) string {
 	return strings.TrimSpace(stdout)
 }
 
-func runCLICommand(t *testing.T, cmd string) *icmd.Result {
+func cliCmd(t *testing.T, cmd string) icmd.Cmd {
 	args := []string{"run", "-q", "--", "--vv"}
 	parsedArgs, err := shlex.Split(cmd)
 	require.NoError(t, err, cmd)
@@ -207,7 +208,7 @@ func runCLICommand(t *testing.T, cmd string) *icmd.Result {
 		fmt.Sprintf("SOROBAN_RPC_URL=http://localhost:%d/", sorobanRPCPort),
 		fmt.Sprintf("SOROBAN_NETWORK_PASSPHRASE=%s", StandaloneNetworkPassphrase),
 	)
-	return icmd.RunCmd(c)
+	return c
 }
 
 func getCLIDefaultAccount(t *testing.T) string {
