0ecb92d3e7f24dfa67db1d2ffc8ca54aec7823b2
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-10-10 15:43:28 -0400

    feat(bindings-ts): support multi-auth workflows
    - switch from `ts-tests/package.json` to an `initialize.sh` script that
      uses a `.env` if available or defaults to environment variables. As
      before, this will build, deploy, and generate bindings for the new
      contracts, plus creating an `alice` and `bob` identities and minting
      separate amounts of two separate tokens to each of them
    - add eslint to catch missing `await`s
    - update to latest stellar-sdk instead of soroban-client
    - add transaction-rebuilding to increment transaction sequence numbers,
      so that all tests can pass when run in parallel
    - add `atomic_swap` and `token` contracts from
      https://github.com/stellar/soroban-examples to `test-wasms`
    - add tests for atomic swap functionality inspired by
      https://github.com/stellar/soroban-react-atomic-swap
    - let this logic guide needed updates to `bindings typescript`-generated
      libraries:
      - don't return flat values
      - instead, return a `new AssembledTransaction`, a class that has a
        `result` getter that can be used to get the result of the simulation
        right away, or can be used to `await tx.signAndSend()`, which then
        returns a `SentTransaction`
      - `SentTransaction` contains all possibly-relevant fields
        from the logic it performs, such as `sendTransactionAll`
      - `AssembledTransaction` has a `needsNonInvokerSigningBy()` getter
        and a `signAuthEntries()` to facilitate multi-auth workflows.
      - Since assembling transactions may now take place across multiple
        users' browsers, you can also call `json = tx.toJSON()` on an
        `AssembledTransaction` and then `contract.fromJSON[method](json)` on
        the next machine to continue signing, as shown in `test-swap.ts`
    
    Co-authored-by: Aristides Staffieri <aristides.staffieri@stellar.org>
    Co-authored-by: George <Shaptic@users.noreply.github.com>

--- cmd/crates/soroban-spec-typescript/ts-tests/src/test-custom-types.ts
+++ cmd/crates/soroban-spec-typescript/ts-tests/src/test-custom-types.ts
@@ -1,126 +1,136 @@
 import test from 'ava'
-import { publicKey, rpcUrl, wallet } from './util.js'
-import { Contract, Ok, Err, networks, Address } from 'test-custom-types'
+import { root, rpcUrl, wallet } from './util.js'
+import { Contract, Ok, Err, networks } from 'test-custom-types'
 
-const contract = new Contract({ ...networks.standalone, rpcUrl, wallet});
+const addr = root.address;
+const publicKey = root.keypair.publicKey();
+
+const contract = new Contract({ ...networks.standalone, rpcUrl, wallet });
 
 test('hello', async t => {
-  t.is(await contract.hello({ hello: 'tests' }), 'tests')
+  const { result } = await contract.hello({ hello: 'tests' })
+  t.is(result, 'tests')
 })
 
 test('woid', async t => {
-  t.is(await contract.woid(), undefined)
+  t.is((await contract.woid()).result, undefined)
 })
 
 test('u32_fail_on_even', async t => {
-  t.deepEqual(await contract.u32FailOnEven({ u32_: 1 }), new Ok(1))
-  t.deepEqual(await contract.u32FailOnEven({ u32_: 0 }), new Err({ message: "Please provide an odd number" }))
+  t.deepEqual(
+    (await contract.u32FailOnEven({ u32_: 1 })).result,
+    new Ok(1)
+  )
+  t.deepEqual(
+    (await contract.u32FailOnEven({ u32_: 0 })).result,
+    new Err({ message: "Please provide an odd number" })
+  )
 })
 
 test('u32', async t => {
-  t.is(await contract.u32({ u32_: 1 }), 1)
+  t.is((await contract.u32({ u32_: 1 })).result, 1)
 })
 
 test('i32', async t => {
-  t.is(await contract.i32({ i32_: 1 }), 1)
+  t.is((await contract.i32({ i32_: 1 })).result, 1)
 })
 
 test('i64', async t => {
-  t.is(await contract.i64({ i64_: 1n }), 1n)
+  t.is((await contract.i64({ i64_: 1n })).result, 1n)
 })
 
 test("strukt_hel", async (t) => {
-  let test = { a: 0, b: true, c: "world" }
-  t.deepEqual(await contract.struktHel({ strukt: test }), ["Hello", "world"])
+  const test = { a: 0, b: true, c: "world" }
+  t.deepEqual((await contract.struktHel({ strukt: test })).result, ["Hello", "world"])
 })
 
 test("strukt", async (t) => {
-  let test = { a: 0, b: true, c: "hello" }
-  t.deepEqual(await contract.strukt({ strukt: test }), test)
+  const test = { a: 0, b: true, c: "hello" }
+  t.deepEqual((await contract.strukt({ strukt: test })).result, test)
 })
 
 test('simple first', async t => {
   const simple = { tag: 'First', values: undefined } as const
-  t.deepEqual(await contract.simple({ simple }), simple)
+  t.deepEqual((await contract.simple({ simple })).result, simple)
 })
 
 test('simple second', async t => {
   const simple = { tag: 'Second', values: undefined } as const
-  t.deepEqual(await contract.simple({ simple }), simple)
+  t.deepEqual((await contract.simple({ simple })).result, simple)
 })
 
 test('simple third', async t => {
   const simple = { tag: 'Third', values: undefined } as const
-  t.deepEqual(await contract.simple({ simple }), simple)
+  t.deepEqual((await contract.simple({ simple })).result, simple)
 })
 
 test('complex with struct', async t => {
   const arg = { tag: 'Struct', values: [{ a: 0, b: true, c: 'hello' }] } as const
   const ret = { tag: 'Struct', values: [{ a: 0, b: true, c: 'hello' }] }
-  t.deepEqual(await contract.complex({ complex: arg }), ret)
+  t.deepEqual((await contract.complex({ complex: arg })).result, ret)
 })
 
 test('complex with tuple', async t => {
   const arg = { tag: 'Tuple', values: [[{ a: 0, b: true, c: 'hello' }, { tag: 'First', values: undefined }]] } as const
   const ret = { tag: 'Tuple', values: [[{ a: 0, b: true, c: 'hello' }, { tag: 'First', values: undefined }]] }
-  t.deepEqual(await contract.complex({ complex: arg }), ret)
+  t.deepEqual((await contract.complex({ complex: arg })).result, ret)
 })
 
 test('complex with enum', async t => {
   const arg = { tag: 'Enum', values: [{ tag: 'First', values: undefined }] } as const
   const ret = { tag: 'Enum', values: [{ tag: 'First', values: undefined }] }
-  t.deepEqual(await contract.complex({ complex: arg }), ret)
+  t.deepEqual((await contract.complex({ complex: arg })).result, ret)
 })
 
 test('complex with asset', async t => {
   const arg = { tag: 'Asset', values: [publicKey, 1n] } as const
-  const ret = { tag: 'Asset', values: [new Address(publicKey), 1n] }
-  t.deepEqual(await contract.complex({ complex: arg }), ret)
+  const ret = { tag: 'Asset', values: [addr, 1n] }
+  t.deepEqual((await contract.complex({ complex: arg })).result, ret)
 })
 
 test('complex with void', async t => {
   const complex = { tag: 'Void', values: undefined } as const
-  t.deepEqual(await contract.complex({ complex }), complex)
+  t.deepEqual((await contract.complex({ complex })).result, complex)
 })
 
 test('addresse', async t => {
-  t.deepEqual(await contract.addresse({ addresse: publicKey }), Address.fromString(publicKey))
+  t.deepEqual((await contract.addresse({ addresse: publicKey })).result, addr)
 })
 
 test('bytes', async t => {
   const bytes = Buffer.from('hello')
-  t.deepEqual(await contract.bytes({ bytes }), bytes)
+  t.deepEqual((await contract.bytes({ bytes })).result, bytes)
 })
 
 test('bytes_n', async t => {
   const bytes_n = Buffer.from('123456789') // what's the correct way to construct bytes_n?
-  t.deepEqual(await contract.bytesN({ bytes_n }), bytes_n)
+  t.deepEqual((await contract.bytesN({ bytes_n })).result, bytes_n)
 })
 
 test('card', async t => {
   const card = 11
-  t.is(await contract.card({ card }), card)
+  t.is((await contract.card({ card })).result, card)
 })
 
 test('boolean', async t => {
-  t.is(await contract.boolean({ boolean: true }), true)
+  t.is((await contract.boolean({ boolean: true })).result, true)
 })
 
 test('not', async t => {
-  t.is(await contract.not({ boolean: true }), false)
+  t.is((await contract.not({ boolean: true })).result, false)
 })
 
 test('i128', async t => {
-  t.is(await contract.i128({ i128: -1n }), -1n)
+  t.is((await contract.i128({ i128: -1n })).result, -1n)
 })
 
 test('u128', async t => {
-  t.is(await contract.u128({ u128: 1n }), 1n)
+  t.is((await contract.u128({ u128: 1n })).result, 1n)
 })
 
 test('multi_args', async t => {
-  t.is(await contract.multiArgs({ a: 1, b: true }), 1)
-  t.is(await contract.multiArgs({ a: 1, b: false }), 0)
+  t.is((await contract.multiArgs({ a: 1, b: true })).result, 1)
+  t.is((await contract.multiArgs({ a: 1, b: false })).result, 0)
 })
 
 test('map', async t => {
@@ -128,46 +138,46 @@ test('map', async t => {
   map.set(1, true)
   map.set(2, false)
   // map.set(3, 'hahaha') // should throw an error
-  t.deepEqual(await contract.map({ map }), map)
+  t.deepEqual((await contract.map({ map })).result, map)
 })
 
 test('vec', async t => {
   const vec = [1, 2, 3]
-  t.deepEqual(await contract.vec({ vec }), vec)
+  t.deepEqual((await contract.vec({ vec })).result, vec)
 })
 
 test('tuple', async t => {
   const tuple = ['hello', 1] as const
-  t.deepEqual(await contract.tuple({ tuple }), tuple)
+  t.deepEqual((await contract.tuple({ tuple })).result, tuple)
 })
 
 test('option', async t => {
   // this makes sense
-  t.deepEqual(await contract.option({ option: 1 }), 1)
+  t.deepEqual((await contract.option({ option: 1 })).result, 1)
 
   // this passes but shouldn't
-  t.deepEqual(await contract.option({ option: undefined }), undefined)
+  t.deepEqual((await contract.option({ option: undefined })).result, undefined)
 
   // this is the behavior we probably want, but fails
   // t.deepEqual(await contract.option(), undefined) // typing and implementation require the object
-  // t.deepEqual(await contract.option({}), undefined) // typing requires argument; implementation would be fine with this
-  // t.deepEqual(await contract.option({ option: undefined }), undefined)
+  // t.deepEqual((await contract.option({})).result, undefined) // typing requires argument; implementation would be fine with this
+  // t.deepEqual((await contract.option({ option: undefined })).result, undefined)
 })
 
 test('u256', async t => {
-  t.is(await contract.u256({ u256: 1n }), 1n)
+  t.is((await contract.u256({ u256: 1n })).result, 1n)
 })
 
 test('i256', async t => {
-  t.is(await contract.i256({ i256: -1n }), -1n)
+  t.is((await contract.i256({ i256: -1n })).result, -1n)
 })
 
 test('string', async t => {
-  t.is(await contract.string({ string: 'hello' }), 'hello')
+  t.is((await contract.string({ string: 'hello' })).result, 'hello')
 })
 
 test('tuple_strukt', async t => {
   const arg = [{ a: 0, b: true, c: 'hello' }, { tag: 'First', values: undefined }] as const
   const res = [{ a: 0, b: true, c: 'hello' }, { tag: 'First', values: undefined }]
-  t.deepEqual(await contract.tupleStrukt({ tuple_strukt: arg }), res)
+  t.deepEqual((await contract.tupleStrukt({ tuple_strukt: arg })).result, res)
 })
---
+++ cmd/crates/soroban-test/tests/fixtures/test-wasms/swap/src/test.rs
@@ -0,0 +1,112 @@
+#![cfg(test)]
+extern crate std;
+
+use super::*;
+use soroban_sdk::{
+    symbol_short,
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation},
+    token, Address, Env, IntoVal,
+};
+use token::Client as TokenClient;
+use token::StellarAssetClient as TokenAdminClient;
+
+fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
+    let contract_address = e.register_stellar_asset_contract(admin.clone());
+    (
+        TokenClient::new(e, &contract_address),
+        TokenAdminClient::new(e, &contract_address),
+    )
+}
+
+fn create_atomic_swap_contract(e: &Env) -> AtomicSwapContractClient {
+    AtomicSwapContractClient::new(e, &e.register_contract(None, AtomicSwapContract {}))
+}
+
+#[test]
+fn test_atomic_swap() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let a = Address::random(&env);
+    let b = Address::random(&env);
+
+    let token_admin = Address::random(&env);
+
+    let (token_a, token_a_admin) = create_token_contract(&env, &token_admin);
+    let (token_b, token_b_admin) = create_token_contract(&env, &token_admin);
+    token_a_admin.mint(&a, &1000);
+    token_b_admin.mint(&b, &5000);
+
+    let contract = create_atomic_swap_contract(&env);
+
+    contract.swap(
+        &a,
+        &b,
+        &token_a.address,
+        &token_b.address,
+        &1000,
+        &4500,
+        &5000,
+        &950,
+    );
+
+    assert_eq!(
+        env.auths(),
+        std::vec![
+            (
+                a.clone(),
+                AuthorizedInvocation {
+                    function: AuthorizedFunction::Contract((
+                        contract.address.clone(),
+                        symbol_short!("swap"),
+                        (
+                            token_a.address.clone(),
+                            token_b.address.clone(),
+                            1000_i128,
+                            4500_i128
+                        )
+                            .into_val(&env),
+                    )),
+                    sub_invocations: std::vec![AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            token_a.address.clone(),
+                            symbol_short!("transfer"),
+                            (a.clone(), contract.address.clone(), 1000_i128,).into_val(&env),
+                        )),
+                        sub_invocations: std::vec![]
+                    }]
+                }
+            ),
+            (
+                b.clone(),
+                AuthorizedInvocation {
+                    function: AuthorizedFunction::Contract((
+                        contract.address.clone(),
+                        symbol_short!("swap"),
+                        (
+                            token_b.address.clone(),
+                            token_a.address.clone(),
+                            5000_i128,
+                            950_i128
+                        )
+                            .into_val(&env),
+                    )),
+                    sub_invocations: std::vec![AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            token_b.address.clone(),
+                            symbol_short!("transfer"),
+                            (b.clone(), contract.address.clone(), 5000_i128,).into_val(&env),
+                        )),
+                        sub_invocations: std::vec![]
+                    }]
+                }
+            ),
+        ]
+    );
+
+    assert_eq!(token_a.balance(&a), 50);
+    assert_eq!(token_a.balance(&b), 950);
+
+    assert_eq!(token_b.balance(&a), 4500);
+    assert_eq!(token_b.balance(&b), 500);
+}
