685d9933362f9829257aa018a04a1e52dd9913ae
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2023-12-04 12:04:45 -0500

    Allow for a --detached flag

--- cmd/soroban-cli/src/commands/network/start.rs
+++ cmd/soroban-cli/src/commands/network/start.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {}
@@ -25,6 +25,10 @@ pub struct Cmd {
     /// optional argument for docker tag
     #[arg(short = 't', long, default_value=DOCKER_TAG)]
     pub docker_tag: String,
+
+    /// optional argument to run docker process in detached mode
+    #[arg(short = 'd', long)]
+    pub detached: bool,
 }
 
 impl Cmd {
@@ -32,14 +36,15 @@ impl Cmd {
         println!("Starting {} network", &self.network);
         let docker_command = build_docker_command(&self);
 
-        run_docker_command(docker_command);
+        run_docker_command(docker_command, self.detached);
         Ok(())
     }
 }
 
 fn build_docker_command(cmd: &Cmd) -> String {
     let image_name = format!("{}:{}", cmd.docker_image, cmd.docker_tag);
-    let docker_command = format!("docker run --rm -d -p \"{from_port}:{to_port}\" --name {container_name} {image_name} --{network} --enable-soroban-rpc",
+    let docker_command = format!("docker run --rm {detached} -p \"{from_port}:{to_port}\" --name {container_name} {image_name} --{network} --enable-soroban-rpc",
+      detached = if cmd.detached { "-d" } else { "" },
       from_port=FROM_PORT,
       to_port=TO_PORT,
       container_name=cmd.container_name,
@@ -50,18 +55,20 @@ fn build_docker_command(cmd: &Cmd) -> String {
     docker_command
 }
 
-fn run_docker_command(docker_command: String) {
+fn run_docker_command(docker_command: String, detached: bool) {
     println!("Running docker command: `{}`", docker_command);
-    let output = Command::new("sh")
+    if detached {
+        println!("Running docker container id: ")
+    }
+    let mut cmd = Command::new("sh")
         .args(&["-c", &docker_command])
-        .output()
-        .expect("Failed to execute command");
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
 
-    if output.status.success() {
-        let result = String::from_utf8_lossy(&output.stdout);
-        println!("Docker container id started: {}", result);
-    } else {
-        let result = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Error executing Docker command: {}", result);
+    let status = cmd.wait();
+    if status.is_err() {
+        println!("Exited with status {:?}", status);
     }
 }
