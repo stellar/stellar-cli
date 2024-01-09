2bfad415c83ce84eebdd78eb7fe516cb868fa802
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-01-09 17:26:51 -0500

    Update doc strings

--- cmd/soroban-cli/src/commands/network/start.rs
+++ cmd/soroban-cli/src/commands/network/start.rs
@@ -8,17 +8,32 @@ const TO_PORT: i32 = 8000;
 const CONTAINER_NAME: &str = "stellar";
 const DOCKER_IMAGE: &str = "stellar/quickstart";
 
+/// This command allows for starting a stellar quickstart container. To run it, you can use the following command:
+/// `soroban network start <NETWORK> [OPTIONS] -- [DOCKER_RUN_ARGS]`
+///
+/// OPTIONS: refer to the options that are available to the quickstart image:
+/// --enable-soroban-rpc - is defaulted to be enabled
+/// --protocol-version (only for local network)
+/// --limits (only for local network)
+
+/// DOCKER_RUN_ARGS: These are arguments to be passed to the `docker run` command itself, and should be passed in after the slop `--`. Some common options are:
+/// -p <FROM_PORT>:<TO_PORT> - this maps the port from the container to the host machine. By default, the port is 8000.
+/// -d - this runs the container in detached mode, so that it runs in the background
+
+// By default, without any optional arguments, the following docker command will run:
+// docker run --rm -p 8000:8000 --name stellar stellar/quickstart:testing --testnet --enable-soroban-rpc
+
 #[derive(Debug, clap::Parser, Clone)]
 pub struct Cmd {
-    /// Network to start, e.g. local, testnet, futurenet
+    /// Network to start, e.g. local, testnet, futurenet, pubnet
     pub network: String,
 
     /// optional argument to override the default docker image tag for the given network
     #[arg(short = 't', long)]
     pub image_tag_override: Option<String>,
 
-    // optional arguments to pass to docker command
-    #[arg(last = true, id = "DOCKER_COMMAND_ARGS")]
+    // optional arguments to pass to the docker run command
+    #[arg(last = true, id = "DOCKER_RUN_ARGS")]
     pub slop: Vec<String>,
 }
 
@@ -33,8 +48,9 @@ impl Cmd {
     }
 }
 
-fn build_docker_command(cmd: &Cmd) -> String {
-    let image_tag = match cmd.network.as_str() {
+fn get_image_name(cmd: &Cmd) -> String {
+    // this can be overriden with the `-t` flag
+    let mut image_tag = match cmd.network.as_str() {
         "local" => "latest",
         "pubnet" => "latest",
         "testnet" => "testing",
@@ -52,7 +68,11 @@ fn build_docker_command(cmd: &Cmd) -> String {
         image_tag = override_tag;
     }
 
-    let image = format!("{}:{}", DOCKER_IMAGE, image_tag);
+    format!("{}:{}", DOCKER_IMAGE, image_tag)
+}
+
+fn build_docker_command(cmd: &Cmd) -> String {
+    let image = get_image_name(cmd);
 
     let container_name = if cmd.slop.contains(&"--name".to_string()) {
         cmd.slop[cmd.slop.iter().position(|x| x == "--name").unwrap() + 1].clone()
