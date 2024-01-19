4f7eac47456d8724a9f0ce86770c4b34415eac83
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-01-19 11:09:11 -0500

    Use hello-world instead of increment as default contract

---
+++ cmd/soroban-cli/src/utils/contract-init-template/contracts/hello_world/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+
+#[contract]
+pub struct HelloContract;
+
+#[contractimpl]
+impl HelloContract {
+    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
+        vec![&env, symbol_short!("Hello"), to]
+    }
+}
+
+mod test;
