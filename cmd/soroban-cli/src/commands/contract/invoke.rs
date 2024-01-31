1ee71df848b9c37bdbab90d70f9f65234aa4b2a6
Willem Wyndham <willem@ahalabs.dev>
2024-01-30 20:36:16 -0500

    feat: add a sign argument to force sign and sending
    SYSTEM_TEST_VERBOSE_OUTPUT was added to get system tests to pass this

--- cmd/soroban-cli/src/commands/contract/invoke.rs
+++ cmd/soroban-cli/src/commands/contract/invoke.rs
@@ -47,6 +47,9 @@ pub struct Cmd {
     /// Number of instructions to simulate
     #[arg(long)]
     pub instructions: Option<u32>,
+    /// Sign and submit transaction regardless of whether it's a view function  
+    #[arg(long, env = "SOROBAN_INVOKE_SIGN", env = "SYSTEM_TEST_VERBOSE_OUTPUT")]
+    pub sign: bool,
     /// Function name as subcommand, then arguments for that function as `--arg-name value`
     #[arg(last = true, id = "CONTRACT_FN_AND_ARGS")]
     pub slop: Vec<OsString>,
@@ -301,7 +304,7 @@ impl Cmd {
         if let Some(instructions) = self.instructions {
             txn = txn.set_max_instructions(instructions);
         }
-        let (return_value, events) = if txn.is_view() {
+        let (return_value, events) = if txn.is_view() && !self.sign {
             (
                 txn.sim_res().results()?[0].xdr.clone(),
                 txn.sim_res().events()?,
