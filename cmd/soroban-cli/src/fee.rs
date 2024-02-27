6f35f7a40cc85a43c45bff90d9399e0e28a0250b
Willem Wyndham <willem@ahalabs.dev>
2024-02-27 09:21:33 -0500

    feat: move cost and instruction to Args struct and apply the instructions if present (#1221)

--- cmd/soroban-cli/src/fee.rs
+++ cmd/soroban-cli/src/fee.rs
@@ -1,5 +1,7 @@
-use crate::commands::HEADING_RPC;
 use clap::arg;
+use soroban_rpc::Assembled;
+
+use crate::commands::HEADING_RPC;
 
 #[derive(Debug, clap::Args, Clone)]
 #[group(skip)]
@@ -7,10 +9,30 @@ pub struct Args {
     /// fee amount for transaction, in stroops. 1 stroop = 0.0000001 xlm
     #[arg(long, default_value = "100", env = "SOROBAN_FEE", help_heading = HEADING_RPC)]
     pub fee: u32,
+    /// Output the cost execution to stderr
+    #[arg(long = "cost", help_heading = HEADING_RPC)]
+    pub cost: bool,
+    /// Number of instructions to simulate
+    #[arg(long, help_heading = HEADING_RPC)]
+    pub instructions: Option<u32>,
+}
+
+impl Args {
+    pub fn apply_to_assembled_txn(&self, txn: Assembled) -> Assembled {
+        if let Some(instructions) = self.instructions {
+            txn.set_max_instructions(instructions)
+        } else {
+            txn
+        }
+    }
 }
 
 impl Default for Args {
     fn default() -> Self {
-        Self { fee: 100 }
+        Self {
+            fee: 100,
+            cost: false,
+            instructions: None,
+        }
     }
 }
