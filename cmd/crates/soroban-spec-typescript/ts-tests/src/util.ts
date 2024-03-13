5161053c19832ee7bb4ffc11de1bed596f089aa4
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-03-13 16:38:24 -0400

    build: remove redundant TS tests
    All tests that are being removed are now duplicates of tests that are in
    stellar-sdk. So are the test-custom-types tests, but those ones actually
    caught some edge cases, so maybe they're worth keeping.

--- cmd/crates/soroban-spec-typescript/ts-tests/src/util.ts
+++ cmd/crates/soroban-spec-typescript/ts-tests/src/util.ts
@@ -3,22 +3,10 @@ import { Address, Keypair } from "@stellar/stellar-sdk";
 import { basicNodeSigner } from "@stellar/stellar-sdk/lib/contract_client/index.js";
 
 const rootKeypair = Keypair.fromSecret(spawnSync("./soroban", ["keys", "show", "root"], { shell: true, encoding: "utf8" }).stdout.trim());
-const aliceKeypair = Keypair.fromSecret(spawnSync("./soroban", ["keys", "show", "alice"], { shell: true, encoding: "utf8" }).stdout.trim());
-const bobKeypair = Keypair.fromSecret(spawnSync("./soroban", ["keys", "show", "bob"], { shell: true, encoding: "utf8" }).stdout.trim());
 
 export const root = {
   keypair: rootKeypair,
   address: Address.fromString(rootKeypair.publicKey()),
-}
-
-export const alice = {
-  keypair: aliceKeypair,
-  address: Address.fromString(aliceKeypair.publicKey()),
-}
-
-export const bob = {
-  keypair: bobKeypair,
-  address: Address.fromString(bobKeypair.publicKey()),
 }
 
 export const rpcUrl = process.env.SOROBAN_RPC_URL ?? "http://localhost:8000/";
