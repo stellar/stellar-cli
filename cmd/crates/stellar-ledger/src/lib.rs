63bafad59b29d57424790cf6e4214869a935ca10
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-04-04 10:25:29 -0400

    Allow for enabling tx hash signing on the emulator

--- cmd/crates/stellar-ledger/src/lib.rs
+++ cmd/crates/stellar-ledger/src/lib.rs
@@ -27,7 +27,7 @@ enum Error {}
 #[cfg(test)]
 mod test {
 
-    use std::{str::FromStr, time::Duration};
+    use std::{collections::HashMap, str::FromStr, time::Duration};
 
     use super::*;
     use once_cell::sync::Lazy;
@@ -117,9 +117,10 @@ mod test {
         let result = ledger.sign_transaction_hash(path, test_hash.into()).await;
         if let Err(APDUExchangeError(msg)) = result {
             assert_eq!(msg, "Ledger APDU retcode: 0x6C66");
+            // this error code is SW_TX_HASH_SIGNING_MODE_NOT_ENABLED from https://github.com/LedgerHQ/app-stellar/blob/develop/docs/COMMANDS.md
         } else {
             stop_emulator(&mut emulator).await;
-            panic!("Unexpected result");
+            panic!("Unexpected result: {:?}", result);
         }
 
         stop_emulator(&mut emulator).await;
@@ -128,7 +129,43 @@ mod test {
     //TODO: implement this test
     // not sure how to enable hash signing on the emulator yet. zemu has methods that emulate pressing the buttons to choose the option
     #[tokio::test]
-    async fn test_sign_tx_hash_when_hash_signing_is_enabled() {}
+    async fn test_sign_tx_hash_when_hash_signing_is_enabled() {
+        //when hash signing isnt enabled on the device we expect an error
+        let mut emulator = Emulator::new().await;
+        start_emulator(&mut emulator).await;
+        enable_hash_signing().await;
+
+        let transport = get_zemu_transport("127.0.0.1", 9998).unwrap();
+        let ledger = app::Ledger::new(transport);
+
+        let path = slip10::BIP32Path::from_str("m/44'/148'/0'").unwrap();
+        let mut test_hash = vec![0u8; 32];
+
+        match hex::decode_to_slice(
+            "3389e9f0f1a65f19736cacf544c2e825313e8447f569233bb8db39aa607c8889",
+            &mut test_hash as &mut [u8],
+        ) {
+            Ok(()) => {}
+            Err(e) => {
+                stop_emulator(&mut emulator).await;
+                panic!("Unexpected result: {e}");
+            }
+        }
+
+        // this is what the js code is doing:
+        // let hash_as_bytes = [ 51, 137, 233, 240, 241, 166,  95,  25, 115, 108, 172, 245,  68, 194, 232,  37, 49,  62, 132,  71, 245, 105,  35,  59, 184, 219,  57, 170,  96, 124, 136, 137 ].to_vec();
+        match ledger.sign_transaction_hash(path, test_hash).await {
+            Ok(result) => {
+                println!("this is the response from signing the hash: {result:?}");
+            }
+            Err(e) => {
+                stop_emulator(&mut emulator).await;
+                panic!("Unexpected result: {e}");
+            }
+        }
+
+        stop_emulator(&mut emulator).await;
+    }
 
     async fn start_emulator(e: &mut Emulator) {
         let start_result = e.run().await;
@@ -142,5 +179,62 @@ mod test {
     async fn stop_emulator(e: &mut Emulator) {
         let stop_result = e.stop().await;
         assert!(stop_result.is_ok());
+    }
+
+    // FIXME lol/sob
+    async fn enable_hash_signing() {
+        // let client = reqwest::Client::new();
+        // client.post("http://localhost:5001/button/right")
+        let mut map = HashMap::new();
+        map.insert("action", "press-and-release");
+
+        let client = reqwest::Client::new();
+        // right button press
+        client
+            .post("http://localhost:5001/button/right")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        // both button press
+        client
+            .post("http://localhost:5001/button/both")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        // both button press
+        client
+            .post("http://localhost:5001/button/both")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        // right button press
+        client
+            .post("http://localhost:5001/button/right")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        // right button press
+        client
+            .post("http://localhost:5001/button/right")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
+
+        // both button press
+        client
+            .post("http://localhost:5001/button/both")
+            .json(&map)
+            .send()
+            .await
+            .unwrap();
     }
 }
