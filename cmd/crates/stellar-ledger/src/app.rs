98a0ffad78f34e5a054c5d15900df1dc9ab85d95
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-04-09 11:21:41 -0400

    not sure which is right

--- cmd/crates/stellar-ledger/src/app.rs
+++ cmd/crates/stellar-ledger/src/app.rs
@@ -24,10 +24,10 @@ const P2_GET_PUBLIC_KEY_NO_DISPLAY: u8 = 0x00;
 const P2_GET_PUBLIC_KEY_DISPLAY: u8 = 0x01;
 
 const SIGN_TX: u8 = 0x04;
-const P1_SIGN_TX_FIRST: u8 = 0x00;
-const P1_SIGN_TX_NOT_FIRST: u8 = 0x80;
-const P2_SIGN_TX_LAST: u8 = 0x00;
-const P2_SIGN_TX_MORE: u8 = 0x80;
+const P1_SIGN_TX_FIRST: u8 = 0x00; // 0
+const P1_SIGN_TX_NOT_FIRST: u8 = 0x80; // 128
+const P2_SIGN_TX_LAST: u8 = 0x00; // 0
+const P2_SIGN_TX_MORE: u8 = 0x80; // 128
 
 const GET_APP_CONFIGURATION: u8 = 0x06;
 const P1_GET_APP_CONFIGURATION: u8 = 0x00;
@@ -144,11 +144,6 @@ where
         ]
         .to_vec();
 
-        // data
-        //  data[0] = pathElts.length
-        // data pathElts
-        // chunk
-
         let mut data: Vec<u8> = Vec::new();
 
         let mut hd_path_to_bytes = hd_path_to_bytes(&hd_path);
@@ -166,11 +161,29 @@ where
         let mut result = Vec::new();
         println!("chunks_count: {:?}", chunks_count);
 
-        for (i, chunk) in chunks.enumerate() {
+        let chunks_hardcoded: Vec<Vec<u8>> = [
+            [
+                3, 128, 0, 0, 44, 128, 0, 0, 148, 128, 0, 0, 0, 122, 195, 57, 151, 84, 78, 49, 117,
+                210, 102, 189, 2, 36, 57, 178, 44, 219, 22, 80, 140, 1, 22, 63, 38, 229, 203, 42,
+                62, 16, 69, 169, 121, 0, 0, 0, 2, 0, 0, 0, 0, 32, 218, 153, 139, 117, 228, 43, 31,
+                127, 133, 208, 117, 193, 39, 245, 178, 70, 223, 18, 173, 150, 240, 16, 188, 247,
+                247, 111, 114, 177, 110, 87, 19, 0, 0, 0, 100, 0, 197, 180, 165, 0, 0, 0, 25, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 149, 65, 240, 39,
+                70, 36, 12, 30, 159, 56, 67, 210, 142, 86, 240, 165, 131, 236, 210, 117, 2, 251,
+                15, 74, 39, 212, 208, 146, 4,
+            ]
+            .to_vec(),
+            [
+                224, 100, 162, 0, 0, 0, 0, 0, 0, 0, 0, 0, 152, 150, 128, 0, 0, 0, 0,
+            ]
+            .to_vec(),
+        ]
+        .to_vec();
+
+        for (i, chunk) in chunks_hardcoded.iter().enumerate() {
             let is_first_chunk = i == 0;
             let is_last_chunk = chunks_count == i + 1;
-            println!("is_first_chunk {is_first_chunk:?}");
-            println!("is_last_chunk {is_last_chunk:?}");
+
             let mut data = data.clone();
             data.append(&mut chunk.to_vec());
             println!("DATA BEING SENT for i {i}: {data:?}");
@@ -188,7 +201,7 @@ where
                 } else {
                     P2_SIGN_TX_MORE
                 },
-                data: data.to_vec(),
+                data: chunk.to_vec(),
             };
 
             println!("command: {:?}", command);
