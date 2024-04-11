28f999d8431d7505f8785bfd893d86971e97624c
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-04-11 10:18:41 -0400

    Making progress
    i needed to look at the js implementation again (hw-app-str) - they have
    a note that says to pass the `signatureBase` into the `signTransaction`
    fn. I had forgotten about this. The `signatureBase` fn is defined in
    js-stellar-base and is the value that should be sent to the network. it
    is TransactionSignaturePayload.toXDR(). Which is also in the signer
    train and i didn't notice it. 🙈

--- cmd/crates/stellar-ledger/src/app.rs
+++ cmd/crates/stellar-ledger/src/app.rs
@@ -1,7 +1,18 @@
 use byteorder::{BigEndian, WriteBytesExt};
 use reqwest::Response;
-use std::{io::Write, str::FromStr, thread::sleep, time::Duration, vec};
-use stellar_xdr::curr::{Limits, ReadXdr, WriteXdr};
+use sha2::{Digest, Sha256};
+use std::{
+    io::{Cursor, Write},
+    str::FromStr,
+    thread::sleep,
+    time::Duration,
+    vec,
+};
+use stellar_xdr::curr::{
+    self, Hash, Limited, Limits, ReadXdr, TransactionEnvelope, TransactionSignaturePayload,
+    TransactionSignaturePayloadTaggedTransaction, TransactionV0, TransactionV0Envelope,
+    TransactionV1Envelope, WriteXdr,
+};
 
 use ledger_transport::{APDUCommand, Exchange};
 use ledger_transport_hid::{
@@ -119,30 +130,85 @@ where
         hd_path: slip10::BIP32Path,
         transaction: Transaction,
     ) -> Result<Vec<u8>, LedgerError> {
-        // let mut tx_as_bytes = vec![0u8; 32];
+        let tagged_transaction =
+            TransactionSignaturePayloadTaggedTransaction::Tx(transaction.clone());
+        // FIXME
+        let testnet_passphrase = "Test SDF Network ; September 2015";
+        let network_hash = Hash(Sha256::digest(testnet_passphrase.as_bytes()).into());
 
-        // let tx_as_hex = "7ac33997544e3175d266bd022439b22cdb16508c01163f26e5cb2a3e1045a979000000020000000020da998b75e42b1f7f85d075c127f5b246df12ad96f010bcf7f76f72b16e57130000006400c5b4a5000000190000000000000000000000010000000000000001000000009541f02746240c1e9f3843d28e56f0a583ecd27502fb0f4a27d4d0922fe064a200000000000000000098968000000000";
-        // match hex::decode_to_slice(
-        //     tx_as_hex,
-        //     &mut tx_as_bytes as &mut [u8],
-        // ) {
-        //     Ok(()) => {}
+        let signature_payload = TransactionSignaturePayload {
+            network_id: network_hash,
+            tagged_transaction: tagged_transaction,
+        };
+
+        let mut signature_payload_as_bytes = signature_payload.to_xdr(Limits::none()).unwrap();
+
+        // let tx_env = TransactionEnvelope::TxV0(TransactionV0Envelope {
+        //     tx: &transaction,
+        //     signatures: vec![].try_into().unwrap(),
+        // });
+
+        // let tx_as_bytes = &transaction.to_xdr(Limits::none()).unwrap();
+        // // let mut tx_env_as_xdr = tx_env.to_xdr(Limits::none()).unwrap();
+
+        // match TransactionV0::from_xdr(tx_as_bytes, Limits::none()) {
+        //     Ok(tx) => {
+        //         println!("tx: {:?}", tx);
+        //     }
         //     Err(e) => {
-        //         panic!("Unexpected result: {e}");
+        //         println!("error: {:?}", e);
         //     }
         // }
 
+        // the test tx from // https://github.com/LedgerHQ/ledger-live/blob/bd5188b5368849cccab74c11cbc64870bd5edbcd/libs/ledgerjs/packages/hw-app-str/tests/Str.test.ts#L47 works. I converted the hex into a byte vec in js:
+        // const transaction = Buffer.from(
+        //     "7ac33997544e3175d266bd022439b22cdb16508c01163f26e5cb2a3e1045a979000000020000000020da998b75e42b1f7f85d075c127f5b246df12ad96f010bcf7f76f72b16e57130000006400c5b4a5000000190000000000000000000000010000000000000001000000009541f02746240c1e9f3843d28e56f0a583ecd27502fb0f4a27d4d0922fe064a200000000000000000098968000000000",
+        //         "hex"
+        //       );
+
+        // console.dir(transaction, {'maxArrayLength': 200});
+
+        // which is this byte vec here:
+
+        // let mut tx_as_bytes: Vec<u8> = [
+        //     122, 195, 57, 151, 84, 78, 49, 117, 210, 102, 189, 2, 36, 57, 178, 44, 219, 22, 80,
+        //     140, 1, 22, 63, 38, 229, 203, 42, 62, 16, 69, 169, 121, 0, 0, 0, 2, 0, 0, 0, 0, 32,
+        //     218, 153, 139, 117, 228, 43, 31, 127, 133, 208, 117, 193, 39, 245, 178, 70, 223, 18,
+        //     173, 150, 240, 16, 188, 247, 247, 111, 114, 177, 110, 87, 19, 0, 0, 0, 100, 0, 197,
+        //     180, 165, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+        //     0, 0, 0, 149, 65, 240, 39, 70, 36, 12, 30, 159, 56, 67, 210, 142, 86, 240, 165, 131,
+        //     236, 210, 117, 2, 251, 15, 74, 39, 212, 208, 146, 47, 224, 100, 162, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 152, 150, 128, 0, 0, 0, 0,
+        // ]
+        // .to_vec();
+
+        //this is from js-stellar-base tests somewhere (transaction builder test) AHHH THIS ONE WORKED!!! but also seems to be v1 :thinkin:
+        // i bet that `signatureBase` is doing something that I need to bake in here somewhere.
+
+        // ah it makes a sig payload... i think that we have that in signer too. i just didnt realize how to put the two things togheter
         let mut tx_as_bytes: Vec<u8> = [
-            122, 195, 57, 151, 84, 78, 49, 117, 210, 102, 189, 2, 36, 57, 178, 44, 219, 22, 80,
-            140, 1, 22, 63, 38, 229, 203, 42, 62, 16, 69, 169, 121, 0, 0, 0, 2, 0, 0, 0, 0, 32,
-            218, 153, 139, 117, 228, 43, 31, 127, 133, 208, 117, 193, 39, 245, 178, 70, 223, 18,
-            173, 150, 240, 16, 188, 247, 247, 111, 114, 177, 110, 87, 19, 0, 0, 0, 100, 0, 197,
-            180, 165, 0, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-            0, 0, 0, 149, 65, 240, 39, 70, 36, 12, 30, 159, 56, 67, 210, 142, 86, 240, 165, 131,
-            236, 210, 117, 2, 251, 15, 74, 39, 212, 208, 146, 47, 224, 100, 162, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 152, 150, 128, 0, 0, 0, 0,
+            206, 224, 48, 45, 89, 132, 77, 50, 189, 202, 145, 92, 130, 3, 221, 68, 179, 63, 187,
+            126, 220, 25, 5, 30, 163, 122, 190, 223, 40, 236, 212, 114, 0, 0, 0, 2, 0, 0, 0, 0,
+            137, 155, 40, 64, 237, 86, 54, 197, 109, 220, 95, 20, 178, 57, 117, 247, 159, 27, 162,
+            56, 141, 38, 148, 228, 197, 110, 205, 221, 201, 96, 229, 239, 0, 0, 0, 100, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0,
+            0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 210, 152, 197,
+            129, 87, 17, 101, 145, 233, 50, 9, 233, 92, 122, 91, 255, 18, 27, 46, 29, 63, 97, 139,
+            104, 208, 252, 200, 239, 229, 229, 228, 112, 0, 0, 0, 0, 0, 0, 0, 2, 84, 11, 228, 0, 0,
+            0, 0, 0,
         ]
         .to_vec();
+
+        // from what i can tell, this tx does this:
+        // max fee: 0.00001 XLM
+        // tx source: GAQNVGMLOXSCWH37QXIHLQJH6WZENXYSVWLPAEF4673W64VRNZLRHMFM
+        // send 1xlm
+        // tx destination:GCKUD4BHIYSAYHU7HBB5FDSW6CSYH3GSOUBPWD2KE7KNBERP4BSKEJDV
+
+        // i feel like i replicated that, but when i try to send the Transaction that is passed into this function, i keep getting back 0x6C24	SW_UNKNOWN_OP	Unknown Stellar operation
+        // i wish i could figure out how to decode that tx into a Transaction, so i could see what the heck it is doing differently
+
+        // i wonder if speculos is using a specific xdr version, or maybe its not speculos but the elf file that i created... somehow
 
         let mut data: Vec<u8> = Vec::new();
 
@@ -151,42 +217,24 @@ where
 
         data.insert(0, hd_path_elements_count);
         data.append(&mut hd_path_to_bytes);
+        data.append(&mut signature_payload_as_bytes);
 
         let buffer_size = 1 + hd_path_elements_count * 4;
         let chunk_size = APDU_MAX_SIZE - buffer_size;
 
-        let chunks = tx_as_bytes.chunks(chunk_size as usize);
+        let chunks = data.chunks(chunk_size as usize);
         let chunks_count = chunks.len();
 
         let mut result = Vec::new();
         println!("chunks_count: {:?}", chunks_count);
 
-        let chunks_hardcoded: Vec<Vec<u8>> = [
-            [
-                3, 128, 0, 0, 44, 128, 0, 0, 148, 128, 0, 0, 0, 122, 195, 57, 151, 84, 78, 49, 117,
-                210, 102, 189, 2, 36, 57, 178, 44, 219, 22, 80, 140, 1, 22, 63, 38, 229, 203, 42,
-                62, 16, 69, 169, 121, 0, 0, 0, 2, 0, 0, 0, 0, 32, 218, 153, 139, 117, 228, 43, 31,
-                127, 133, 208, 117, 193, 39, 245, 178, 70, 223, 18, 173, 150, 240, 16, 188, 247,
-                247, 111, 114, 177, 110, 87, 19, 0, 0, 0, 100, 0, 197, 180, 165, 0, 0, 0, 25, 0, 0,
-                0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 149, 65, 240, 39,
-                70, 36, 12, 30, 159, 56, 67, 210, 142, 86, 240, 165, 131, 236, 210, 117, 2, 251,
-                15, 74, 39, 212, 208, 146, 4,
-            ]
-            .to_vec(),
-            [
-                224, 100, 162, 0, 0, 0, 0, 0, 0, 0, 0, 0, 152, 150, 128, 0, 0, 0, 0,
-            ]
-            .to_vec(),
-        ]
-        .to_vec();
+        // notes:
+        // the first chunk has the hd_path_elements_count and the hd_path at the beginning, before the tx [3, 128...122...47]
+        // the second chunk has just the end of the tx [224, 100... 0, 0, 0, 0]
 
-        for (i, chunk) in chunks_hardcoded.iter().enumerate() {
+        for (i, chunk) in chunks.enumerate() {
             let is_first_chunk = i == 0;
             let is_last_chunk = chunks_count == i + 1;
-
-            let mut data = data.clone();
-            data.append(&mut chunk.to_vec());
-            println!("DATA BEING SENT for i {i}: {data:?}");
 
             let command = APDUCommand {
                 cla: CLA,
@@ -204,7 +252,6 @@ where
                 data: chunk.to_vec(),
             };
 
-            println!("command: {:?}", command);
             match self.send_command_to_ledger(command).await {
                 Ok(mut r) => {
                     result.append(&mut r);
@@ -217,53 +264,6 @@ where
 
         Ok(result)
     }
-
-    // // should this take in a transaction or an assembled transaction?
-    // // i think i need to convert this tranaction to a byte array
-    // pub async fn sign_transaction(
-    //     &self,
-    //     hd_path: slip10::BIP32Path,
-    //     transaction: Transaction,
-    // ) -> Result<Vec<u8>, LedgerError> {
-    //     // TODO: check tx size and make sure it is less than TX_MAX_SIZE
-
-    //     // convert the hd_path into bytes to be sent as `data` to the Ledger
-    //     // the first element of the data should be the number of elements in the path
-    //     let mut hd_path_to_bytes = hd_path_to_bytes(&hd_path);
-    //     let hd_path_elements_count = hd_path.depth();
-
-    //     hd_path_to_bytes.insert(0, hd_path_elements_count);
-
-    //     let buffer_size = 1 + hd_path_elements_count * 4;
-
-    //     let chunkSize = APDU_MAX_SIZE - buffer_size;
-
-    //     // if (transaction.length <= chunkSize) {
-    //     //     // it fits in a single apdu
-    //     //     apdus.push(Buffer.concat([buffer, transaction]));
-    // // }
-
-    //     // this is copilot generated:
-    //     // let mut hd_path_to_bytes = hd_path_to_bytes(&hd_path);
-    //     // let hd_path_elements_count = hd_path.depth();
-    //     // hd_path_to_bytes.insert(0, hd_path_elements_count);
-
-    //     // let mut data = Vec::new();
-    //     // data.write_u32::<BigEndian>(transaction.len() as u32)
-    //     //     .unwrap();
-    //     // data.extend(transaction);
-
-    //     let command = APDUCommand {
-    //         cla: CLA,
-    //         ins: SIGN_TX,
-    //         p1: 0x00,
-    //         p2: 0x00,
-    //         data: vec![],
-    //     };
-
-    //     self.send_command_to_ledger(command).await
-
-    // }
 
     /// The display_and_confirm bool determines if the Ledger will display the public key on its screen and requires user approval to share
     async fn get_public_key_with_display_flag(
--- cmd/crates/stellar-ledger/src/lib.rs
+++ cmd/crates/stellar-ledger/src/lib.rs
@@ -35,8 +35,8 @@ mod test {
     use serial_test::serial;
 
     use stellar_xdr::curr::{
-        HostFunction, InvokeContractArgs, Memo, MuxedAccount, Preconditions, SequenceNumber,
-        StringM, TransactionExt, VecM,
+        HostFunction, InvokeContractArgs, Memo, MuxedAccount, PaymentOp, Preconditions,
+        SequenceNumber, StringM, TransactionExt, TransactionV0, TransactionV0Ext, VecM,
     };
     // should MuxedAccount be stellar_strkey::ed25519::MuxedAccount; instead?
     use tokio::time::sleep;
@@ -118,28 +118,90 @@ mod test {
 
         let path = slip10::BIP32Path::from_str("m/44'/148'/0'").unwrap();
 
+        // this transaction came from https://github.com/stellar/rs-stellar-xdr/blob/main/tests/tx_small.rs
+        // and i am getting a retcode of 27684 which is unknown op
+        // built this tx with https://laboratory.stellar.org/#xdr-viewer?input=AAAAAgAAAAAg2pmLdeQrH3%2BF0HXBJ%2FWyRt8SrZbwELz3929ysW5XEwAAAGQAAAAAAAAAAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAAAQAAAACVQfAnRiQMHp84Q9KOVvClg%2BzSdQL7D0on1NCSL%2BBkogAAAAAAAAAAAJiWgAAAAAAAAAAA&type=TransactionEnvelope
+
+        let source_account_str = "GAQNVGMLOXSCWH37QXIHLQJH6WZENXYSVWLPAEF4673W64VRNZLRHMFM";
+        let source_account_bytes = match stellar_strkey::Strkey::from_string(source_account_str) {
+            Ok(key) => match key {
+                stellar_strkey::Strkey::PublicKeyEd25519(p) => p.0,
+                _ => {
+                    eprintln!("Error decoding public key: {:?}", key);
+                    return;
+                }
+            },
+            Err(err) => {
+                eprintln!("Error decoding public key: {}", err);
+                return;
+            }
+        };
+
+        let destination_account_str = "GCKUD4BHIYSAYHU7HBB5FDSW6CSYH3GSOUBPWD2KE7KNBERP4BSKEJDV";
+        let destination_account_bytes =
+            match stellar_strkey::Strkey::from_string(source_account_str) {
+                Ok(key) => match key {
+                    stellar_strkey::Strkey::PublicKeyEd25519(p) => p.0,
+                    _ => {
+                        eprintln!("Error decoding public key: {:?}", key);
+                        return;
+                    }
+                },
+                Err(err) => {
+                    eprintln!("Error decoding public key: {}", err);
+                    return;
+                }
+            };
+
+        // let tx_v0 = TransactionV0 {
+        //     source_account_ed25519: Uint256(source_account_bytes),
+        //     fee: 100,
+        //     seq_num: SequenceNumber(1),
+        //     time_bounds: None,
+        //     memo: Memo::Text("Stellar".as_bytes().try_into().unwrap()),
+        //     operations: vec![Operation {
+        //         source_account: Some(MuxedAccount::Ed25519(Uint256(source_account_bytes))),
+        //         body: OperationBody::Payment(PaymentOp {
+        //             destination: MuxedAccount::Ed25519(Uint256(destination_account_bytes)),
+        //             asset: xdr::Asset::Native,
+        //             amount: 100,
+        //         }),
+        //     }]
+        //     .try_into()
+        //     .unwrap(),
+        //     ext: TransactionV0Ext::V0,
+        // };
+
         let tx = Transaction {
-            source_account: MuxedAccount::Ed25519(Uint256([0; 32])),
-            fee: 0,
+            source_account: MuxedAccount::Ed25519(Uint256(source_account_bytes)), // was struggling to create a real account in this way with the G... address
+            fee: 100,
             seq_num: SequenceNumber(1),
             cond: Preconditions::None,
             memo: Memo::Text("Stellar".as_bytes().try_into().unwrap()),
-            operations: [].to_vec().try_into().unwrap(),
             ext: TransactionExt::V0,
+            operations: [Operation {
+                source_account: Some(MuxedAccount::Ed25519(Uint256(source_account_bytes))),
+                body: OperationBody::Payment(PaymentOp {
+                    destination: MuxedAccount::Ed25519(Uint256(destination_account_bytes)),
+                    asset: xdr::Asset::Native,
+                    amount: 100,
+                }),
+            }]
+            .try_into()
+            .unwrap(),
         };
 
-        let result = ledger.sign_transaction(path, tx).await;
-        println!("result: {result:?}");
-        // match ledger.sign_transaction(hd_path, transaction).await {
-        //     Ok(config) => {
-        //         assert_eq!(config, vec![0, 5, 0, 3]);
-        //     }
-        //     Err(e) => {
-        //         println!("{e}");
-        //         assert!(false);
-        //         stop_emulator(&mut emulator).await;
-        //     }
-        // };
+        match ledger.sign_transaction(path, tx).await {
+            Ok(response) => {
+                stop_emulator(&mut emulator).await;
+                assert_eq!( hex::encode(response), "ab5de404a9a28ef6cee7387610d7a4c876f5a4051647eeaf077b909eb77ab309ca6dad4ec127da3537d2663204e4a8f4d0e2163d63af9e9d33471069e1d5c90b");
+            }
+            Err(e) => {
+                stop_emulator(&mut emulator).await;
+                println!("{e}");
+                assert!(false);
+            }
+        };
 
         stop_emulator(&mut emulator).await;
     }
