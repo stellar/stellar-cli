c0bf97714b9bb129e6c55f85abaf9c8d7c4c7b28
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-05-02 15:57:25 +1000

    simply the actionlog cache output

--- cmd/soroban-cli/src/commands/cache/actionlog/read.rs
+++ cmd/soroban-cli/src/commands/cache/actionlog/read.rs
@@ -1,10 +1,4 @@
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
-
-use clap::ValueEnum;
-use ulid::Ulid;
+use std::{fs, io, path::PathBuf};
 
 use super::super::super::config::locator;
 use crate::commands::config::data;
@@ -15,8 +9,6 @@ pub enum Error {
     Config(#[from] locator::Error),
     #[error(transparent)]
     Data(#[from] data::Error),
-    #[error(transparent)]
-    Ulid(#[from] ulid::DecodeError),
     #[error("failed to find cache entry {0}")]
     NotFound(String),
     #[error(transparent)]
@@ -26,48 +18,22 @@ pub enum Error {
 #[derive(Debug, clap::Parser, Clone)]
 #[group(skip)]
 pub struct Cmd {
-    /// ULID of the cache entry
+    /// ID of the cache entry
     #[arg(long)]
     pub id: String,
-    #[arg(long)]
-    pub output: Option<OutputType>,
-}
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, ValueEnum, Default)]
-pub enum OutputType {
-    // Status,
-    #[default]
-    Envelope,
-    // ResultMeta,
-    // Result,
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
         let file = self.file()?;
         tracing::debug!("reading file {}", file.display());
-        let (action, _) = data::read(&self.ulid()?)?;
-        let output = if self.output.is_some() {
-            match action {
-                data::Action::Send(sim) => sim.envelope_xdr.expect("missing envelope"),
-                data::Action::Simulate(_) => todo!("Only read transactions"),
-            }
-        } else {
-            serde_json::to_string_pretty(&action)?
-        };
-        println!("{output}");
+        let mut file = fs::File::open(file).map_err(|_| Error::NotFound(self.id.clone()))?;
+        let mut stdout = io::stdout();
+        let _ = io::copy(&mut file, &mut stdout);
         Ok(())
     }
 
     pub fn file(&self) -> Result<PathBuf, Error> {
         Ok(data::actions_dir()?.join(&self.id).with_extension("json"))
-    }
-
-    pub fn read_file(&self, file: &Path) -> Result<String, Error> {
-        fs::read_to_string(file).map_err(|_| Error::NotFound(self.id.clone()))
-    }
-
-    pub fn ulid(&self) -> Result<Ulid, Error> {
-        Ok(Ulid::from_string(&self.id)?)
     }
 }
