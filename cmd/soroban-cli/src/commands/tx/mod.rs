151026a7d720fd9f3123bcb43397bcd918656e58
Willem Wyndham <willem@ahalabs.dev>
2024-05-12 18:34:03 -0400

    fix: remove `tx sign`

--- cmd/soroban-cli/src/commands/tx/mod.rs
+++ cmd/soroban-cli/src/commands/tx/mod.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 
 pub mod send;
-pub mod sign;
+
 pub mod simulate;
 pub mod xdr;
 
@@ -11,8 +11,6 @@ use stellar_xdr::cli as xdr_cli;
 pub enum Cmd {
     /// Add a new identity (keypair, ledger, macOS keychain)
     Inspect(xdr_cli::Root),
-    /// Given an identity return its address (public key)
-    Sign(sign::Cmd),
     /// Submit a transaction to the network
     Send(send::Cmd),
     /// Simulate a transaction
@@ -27,9 +25,6 @@ pub enum Error {
     /// An error during the inspect
     #[error(transparent)]
     Inspect(#[from] xdr_cli::Error),
-    /// An error during the sign
-    #[error(transparent)]
-    Sign(#[from] sign::Error),
     /// An error during the send
     #[error(transparent)]
     Send(#[from] send::Error),
@@ -39,7 +34,6 @@ impl Cmd {
     pub async fn run(&self) -> Result<(), Error> {
         match self {
             Cmd::Inspect(cmd) => cmd.run()?,
-            Cmd::Sign(cmd) => cmd.run().await?,
             Cmd::Send(cmd) => cmd.run().await?,
             Cmd::Simulate(cmd) => cmd.run().await?,
         };
