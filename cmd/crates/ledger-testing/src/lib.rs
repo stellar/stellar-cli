f3f0dc72f5e68a230e642d35bc76e42cd97a613e
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-05-21 11:23:18 -0400

    Pass the docker client into the testing framework
    this helped get past the lifetime complexity

--- cmd/crates/ledger-testing/src/lib.rs
+++ cmd/crates/ledger-testing/src/lib.rs
@@ -1,51 +1,53 @@
 use serde::Deserialize;
 use std::path::PathBuf;
-use testcontainers::clients;
+use testcontainers::{clients::Cli, Container};
 
 use speculos::{Args, Speculos};
 
 pub mod emulator_http_transport;
 pub mod speculos;
 
+const DEFAULT_HOST: &str = "localhost";
+const TRANSPORT_PORT: u16 = 9998;
+const SPECULOS_API_PORT: u16 = 5000;
+
 #[derive(Debug)]
-pub struct LedgerTesting {
+pub struct LedgerTesting<'a> {
     host: String,
     local_elfs_dir: PathBuf,
     device_model: String,
-    client: clients::Cli,
-    pub transport_port: Option<u16>,
+    transport_port: Option<u16>,
     speculos_api_port: Option<u16>,
+    container: Option<Container<'a, Speculos>>,
 }
 
-const DEFAULT_HOST: &str = "localhost";
-impl LedgerTesting {
+impl<'a> LedgerTesting<'a> {
     pub fn new(local_elfs_dir: PathBuf, device_model: String) -> Self {
         Self {
             host: DEFAULT_HOST.to_string(),
             local_elfs_dir,
             device_model,
-            client: clients::Cli::default(),
-
             transport_port: None,
             speculos_api_port: None,
+            container: None,
         }
     }
 
-    pub async fn start(&mut self) {
-        let docker = &self.client;
+    pub async fn start(&mut self, docker: &'a Cli) {
         let container_args = Args {
             ledger_device_model: self.device_model.clone(),
         };
 
         let emulator_image = Speculos::new(self.local_elfs_dir.clone());
 
-        let node = docker.run((emulator_image, container_args));
+        let container = docker.run((emulator_image, container_args));
 
-        let transport_port = node.get_host_port_ipv4(9998);
-        let speculos_api_port = node.get_host_port_ipv4(5000);
+        let transport_port = container.get_host_port_ipv4(TRANSPORT_PORT);
+        let speculos_api_port = container.get_host_port_ipv4(SPECULOS_API_PORT);
 
         self.transport_port = Some(transport_port);
         self.speculos_api_port = Some(speculos_api_port);
+        self.container = Some(container);
 
         wait_for_emulator_start_text(speculos_api_port).await;
     }
@@ -97,8 +99,10 @@ mod test {
     #[tokio::test]
     async fn test_start_nano_s() {
         let test_elfs_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/test_elfs");
+        let docker = Cli::default();
         let mut ledger_testing = LedgerTesting::new(test_elfs_dir, "nanos".to_string());
-        println!("Starting emulator: {:?}", ledger_testing);
-        ledger_testing.start().await;
+        ledger_testing.start(&docker).await;
+
+        assert!(ledger_testing.transport_port.is_some());
     }
 }
