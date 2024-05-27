87f965c56134fdf32b6cdf2e26e8dc9bc16908cc
Willem Wyndham <willem@ahalabs.dev>
2024-05-27 10:29:37 -0600

    fix: remove send as it is hard to test without sign

--- cmd/soroban-cli/src/commands/tx/mod.rs
+++ cmd/soroban-cli/src/commands/tx/mod.rs
@@ -2,14 +2,11 @@ use clap::Parser;
 
 use super::global;
 
-pub mod send;
 pub mod simulate;
 pub mod xdr;
 
 #[derive(Debug, Parser)]
 pub enum Cmd {
-    /// Submit a transaction envelope from stdin to the network
-    Send(send::Cmd),
     /// Simulate a transaction envelope from stdin
     Simulate(simulate::Cmd),
 }
@@ -19,15 +16,11 @@ pub enum Error {
     /// An error during the simulation
     #[error(transparent)]
     Simulate(#[from] simulate::Error),
-    /// An error during the send
-    #[error(transparent)]
-    Send(#[from] send::Error),
 }
 
 impl Cmd {
     pub async fn run(&self, global_args: &global::Args) -> Result<(), Error> {
         match self {
-            Cmd::Send(cmd) => cmd.run(global_args).await?,
             Cmd::Simulate(cmd) => cmd.run(global_args).await?,
         };
         Ok(())
