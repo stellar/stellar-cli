71dc74b9f592ec7bc3e559008115b3976b742f8a
Willem Wyndham <willem@ahalabs.dev>
2024-07-09 14:06:44 -0400

    chore: initial sign and send test

--- cmd/crates/soroban-test/tests/it/integration/tx.rs
+++ cmd/crates/soroban-test/tests/it/integration/tx.rs
@@ -1,10 +1,10 @@
 use soroban_sdk::xdr::{Limits, ReadXdr, TransactionEnvelope, WriteXdr};
 use soroban_test::{AssertExt, TestEnv};
 
-use crate::integration::util::{deploy_contract, DeployKind, HELLO_WORLD};
+use crate::integration::util::{deploy_contract, deploy_hello, DeployKind, HELLO_WORLD};
 
 #[tokio::test]
-async fn txn_simulate() {
+async fn simulate() {
     let sandbox = &TestEnv::new();
     let xdr_base64_build_only = deploy_contract(sandbox, HELLO_WORLD, DeployKind::BuildOnly).await;
     let xdr_base64_sim_only = deploy_contract(sandbox, HELLO_WORLD, DeployKind::SimOnly).await;
@@ -32,7 +32,7 @@ async fn txn_simulate() {
 }
 
 #[tokio::test]
-async fn txn_send() {
+async fn send() {
     let sandbox = &TestEnv::new();
     sandbox
         .new_assert_cmd("contract")
@@ -44,7 +44,7 @@ async fn txn_send() {
     let xdr_base64 = deploy_contract(sandbox, HELLO_WORLD, DeployKind::SimOnly).await;
     println!("{xdr_base64}");
     let tx_env = TransactionEnvelope::from_xdr_base64(&xdr_base64, Limits::none()).unwrap();
-    let tx_env = sign(sandbox, &tx_env);
+    let tx_env = sign_manually(sandbox, &tx_env);
 
     println!(
         "Transaction to send:\n{}",
@@ -54,7 +54,6 @@ async fn txn_send() {
     let assembled_str = sandbox
         .new_assert_cmd("tx")
         .arg("send")
-        .arg("--source=test")
         .write_stdin(tx_env.to_xdr_base64(Limits::none()).unwrap())
         .assert()
         .success()
@@ -62,7 +61,7 @@ async fn txn_send() {
     println!("Transaction sent: {assembled_str}");
 }
 
-fn sign(sandbox: &TestEnv, tx_env: &TransactionEnvelope) -> TransactionEnvelope {
+fn sign_manually(sandbox: &TestEnv, tx_env: &TransactionEnvelope) -> TransactionEnvelope {
     TransactionEnvelope::from_xdr_base64(
         sandbox
             .new_assert_cmd("tx")
@@ -75,4 +74,60 @@ fn sign(sandbox: &TestEnv, tx_env: &TransactionEnvelope) -> TransactionEnvelope 
         Limits::none(),
     )
     .unwrap()
+}
+
+#[tokio::test]
+async fn sign() {
+    let sandbox = &TestEnv::new();
+    let id = &deploy_hello(sandbox).await;
+    // Create new test_other account
+    sandbox
+        .new_assert_cmd("keys")
+        .arg("fund")
+        .arg("test_other")
+        .assert()
+        .stderr(predicates::str::contains("Account already exists"));
+
+    // Get Xdr for transaction where auth is required for test_other
+    let xdr_base64 = sandbox
+        .new_assert_cmd("contract")
+        .arg("invoke")
+        .arg("--id")
+        .arg(id)
+        .arg("--sim-only")
+        .arg("--")
+        .arg("auth")
+        .arg("--world=world")
+        .arg("--addr=test_other")
+        .assert()
+        .success()
+        .stdout_as_str();
+    // Sign the transaction's auth entry with test_other
+    let xdr_base64 = sandbox
+        .new_assert_cmd("tx")
+        .arg("sign")
+        .arg("--auth")
+        .arg("--source=test_other")
+        .write_stdin(xdr_base64.as_bytes())
+        .assert()
+        .success()
+        .stdout_as_str();
+    // Sign the transaction with test as source account
+    let xdr_base64 = sandbox
+        .new_assert_cmd("tx")
+        .arg("sign")
+        .write_stdin(xdr_base64.as_bytes())
+        .assert()
+        .success()
+        .stdout_as_str();
+    // Send transaction
+    let res = sandbox
+        .new_assert_cmd("tx")
+        .arg("send")
+        .write_stdin(xdr_base64.as_bytes())
+        .assert()
+        .success()
+        .stdout_as_str();
+    println!("{res}");
+    assert!(res == "hello world");
 }
