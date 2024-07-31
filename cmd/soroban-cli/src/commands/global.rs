6422e1f08d2cb1c93914fbd20ddff188b4acf559
Nando Vieira <me@fnando.com>
2024-07-31 11:29:44 -0300

    Make global arguments... global.

--- cmd/soroban-cli/src/commands/global.rs
+++ cmd/soroban-cli/src/commands/global.rs
@@ -11,27 +11,27 @@ pub struct Args {
     pub locator: config::locator::Args,
 
     /// Filter logs output. To turn on `stellar_cli::log::footprint=debug` or off `=off`. Can also use env var `RUST_LOG`.
-    #[arg(long, short = 'f')]
+    #[arg(long, short = 'f', global = true)]
     pub filter_logs: Vec<String>,
 
     /// Do not write logs to stderr including `INFO`
-    #[arg(long, short = 'q')]
+    #[arg(long, short = 'q', global = true)]
     pub quiet: bool,
 
     /// Log DEBUG events
-    #[arg(long, short = 'v')]
+    #[arg(long, short = 'v', global = true)]
     pub verbose: bool,
 
     /// Log DEBUG and TRACE events
-    #[arg(long, visible_alias = "vv")]
+    #[arg(long, visible_alias = "vv", global = true)]
     pub very_verbose: bool,
 
     /// List installed plugins. E.g. `stellar-hello`
-    #[arg(long)]
+    #[arg(long, global = true)]
     pub list: bool,
 
     /// Do not cache your simulations and transactions
-    #[arg(long, env = "STELLAR_NO_CACHE")]
+    #[arg(long, env = "STELLAR_NO_CACHE", global = true)]
     pub no_cache: bool,
 }
 
