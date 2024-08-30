d11c7e0750311d149d5f85d5e44ab6e4f1f2e6cb
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2024-08-30 08:20:59 -0400

    fix(init): use String, not Symbol, in hello_world (#1577)
    Updates `hello_world` to match the version that has been in
    `soroban-examples` since https://github.com/stellar/soroban-examples/pull/314

--- cmd/soroban-cli/src/utils/contract-init-template/contracts/hello_world/src/test.rs
+++ cmd/soroban-cli/src/utils/contract-init-template/contracts/hello_world/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{symbol_short, vec, Env};
+use soroban_sdk::{vec, Env, String};
 
 #[test]
 fn test() {
@@ -9,9 +9,13 @@ fn test() {
     let contract_id = env.register_contract(None, HelloContract);
     let client = HelloContractClient::new(&env, &contract_id);
 
-    let words = client.hello(&symbol_short!("Dev"));
+    let words = client.hello(&String::from_str(&env, "Dev"));
     assert_eq!(
         words,
-        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
+        vec![
+            &env,
+            String::from_str(&env, "Hello"),
+            String::from_str(&env, "Dev"),
+        ]
     );
 }
