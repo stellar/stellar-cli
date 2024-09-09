38f58ac567839b3da20970a88fd7a9fb9728d45a
Willem Wyndham <willem@ahalabs.dev>
2024-09-09 14:17:08 -0400

    fix: merge test

--- cmd/crates/soroban-test/tests/it/integration/tx/operations.rs
+++ cmd/crates/soroban-test/tests/it/integration/tx/operations.rs
@@ -133,6 +133,7 @@ async fn account_merge() {
     let (test, test1) = setup_accounts(sandbox);
     let before = client.get_account(&test).await.unwrap();
     let before1 = client.get_account(&test1).await.unwrap();
+    let fee = 100;
     sandbox
         .new_assert_cmd("tx")
         .args([
@@ -142,12 +143,14 @@ async fn account_merge() {
             "test1",
             "--account",
             test.as_str(),
+            "--fee",
+            fee.to_string().as_str(),
         ])
         .assert()
         .success();
     let after = client.get_account(&test).await.unwrap();
     assert!(client.get_account(&test1).await.is_err());
-    assert_eq!(before.balance + before1.balance, after.balance);
+    assert_eq!(before.balance + before1.balance - fee, after.balance);
 }
 
 #[tokio::test]
