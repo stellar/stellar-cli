8808af65c8e0d81094cd9e579d5e0bb02c8f6563
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-09-30 09:31:03 -0400

    Cleanup

--- cmd/crates/soroban-test/src/lib.rs
+++ cmd/crates/soroban-test/src/lib.rs
@@ -224,7 +224,7 @@ impl TestEnv {
         config::Args {
             network: network::Args {
                 rpc_url: Some(self.rpc_url.clone()),
-                rpc_header: None,
+                rpc_headers: [].to_vec(),
                 network_passphrase: Some(LOCAL_NETWORK_PASSPHRASE.to_string()),
                 network: None,
             },
