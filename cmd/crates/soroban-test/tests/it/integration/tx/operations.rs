e8aba2531ff2e59cd912a9759384e99a344cea34
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-10-10 14:55:02 +1000

    Fix tests (#1666)

--- cmd/crates/soroban-test/tests/it/integration/tx/operations.rs
+++ cmd/crates/soroban-test/tests/it/integration/tx/operations.rs
@@ -1,8 +1,8 @@
 use soroban_cli::{
     tx::{builder, ONE_XLM},
     utils::contract_id_hash_from_asset,
+    xdr::{self, ReadXdr, SequenceNumber},
 };
-use soroban_sdk::xdr::{self, ReadXdr, SequenceNumber};
 use soroban_test::{AssertExt, TestEnv};
 
 use crate::integration::{
