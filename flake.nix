e812281dab6a92ccbecd97cbeda5444cf4f22d1e
Thomas M. DuBuisson <tdubuisson@paxos.com>
2024-10-29 07:13:20 -0700

    remove old comment

--- flake.nix
+++ flake.nix
@@ -48,7 +48,6 @@
               "stellar-rpc-client-21.4.0" = "sha256-ue7Ynux9YaDP3f/XkHz2OPd2g0iCX5R0yS5SaVHEYxQ";
           };
 
-          # As of writing 'cargo test' fails
           doCheck = true;
 
           GIT_REVISION = "${self.rev or self.dirtyRev or "unknown"}";
