3d6498f8ee8c6fd084f2ae2179a19d468bf6b586
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2024-11-06 17:08:21 +1000

    Revert "remove unnecessary change"
    This reverts commit 6db67b30a75fab162a9f6e27a47b2122d99136ce.

--- cmd/soroban-cli/src/commands/contract/deploy/asset.rs
+++ cmd/soroban-cli/src/commands/contract/deploy/asset.rs
@@ -93,10 +93,11 @@ impl NetworkRunnable for Cmd {
             .verify_network_passphrase(Some(&network.network_passphrase))
             .await?;
         let source_account = config.source_account()?;
-        let public_strkey = source_account.to_string();
         // Get the account sequence number
         // TODO: use symbols for the method names (both here and in serve)
-        let account_details = client.get_account(&public_strkey).await?;
+        let account_details = client
+            .get_account(&source_account.clone().to_string())
+            .await?;
         let sequence: i64 = account_details.seq_num.into();
         let network_passphrase = &network.network_passphrase;
         let contract_id = contract_id_hash_from_asset(asset, network_passphrase);
