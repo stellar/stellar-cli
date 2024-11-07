f8f12eb1816bfa173eec530dc9a27f66fd4f8872
Gleb <gleb@stellar.org>
2024-11-06 21:12:07 -0800

    Add test to reproduce the bug

--- cmd/crates/soroban-test/tests/it/build.rs
+++ cmd/crates/soroban-test/tests/it/build.rs
@@ -1,5 +1,9 @@
 use predicates::prelude::predicate;
+use soroban_cli::wasm;
+use soroban_spec_tools::contract::Spec;
 use soroban_test::TestEnv;
+use std::collections::HashMap;
+use stellar_xdr::curr::{ScMetaEntry, ScMetaV0};
 
 #[test]
 fn build_all() {
@@ -135,18 +139,55 @@ fn build_with_metadata() {
         .assert()
         .success();
 
-    // verify that the metadata added in the contract code via contractmetadata! macro is present
-    // as well as the meta that is included on build
-    let wasm_path = fixture_path.join(&outdir).join("add.wasm");
     sandbox
         .new_assert_cmd("contract")
         .current_dir(&fixture_path)
-        .arg("info")
-        .arg("meta")
-        .arg("--wasm")
-        .arg(wasm_path)
+        .arg("build")
+        .arg("--meta")
+        .arg("meta_replaced=some_new_meta")
+        .arg("--out-dir")
+        .arg(&outdir)
         .assert()
-        .success()
-        .stdout(predicate::str::contains("Description: A test add contract"))
-        .stdout(predicate::str::contains("contract meta: added on build"));
+        .success();
+
+    // verify that the metadata added in the contract code via contractmetadata! macro is present
+    // as well as the meta that is included on build
+    let wasm_path = fixture_path.join(&outdir).join("add.wasm");
+
+    let wasm_bytes = wasm::Args {
+        wasm: wasm_path.clone(),
+    }
+    .read()
+    .unwrap();
+    let spec = Spec::new(&wasm_bytes).unwrap();
+    let mut expected_entries = HashMap::new();
+
+    expected_entries.insert("Description".to_string(), None);
+    expected_entries.insert("rsver".to_string(), None);
+    expected_entries.insert("rssdkver".to_string(), None);
+    expected_entries.insert("meta_replaced".to_string(), Some("some_new_meta"));
+
+    for meta_entry in &spec.meta {
+        match meta_entry {
+            ScMetaEntry::ScMetaV0(ScMetaV0 { key, val }) => {
+                let key = key.to_string();
+                if let Some(entry) = expected_entries.remove(&key) {
+                    if let Some(str) = entry {
+                        assert_eq!(
+                            str,
+                            val.to_string(),
+                            "Unexpected value ${val} for key ${key} (expecting ${str})"
+                        );
+                    }
+                } else {
+                    panic!("Unexpected key {key}");
+                }
+            }
+        }
+    }
+
+    assert!(
+        expected_entries.is_empty(),
+        "Some entries were not found in the metadata: {expected_entries:?}",
+    );
 }
