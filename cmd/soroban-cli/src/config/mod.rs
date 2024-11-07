b71134d18a160e8d2b2c42fbb33dab8882a4bf8d
Nando Vieira <me@fnando.com>
2024-11-06 17:35:41 -0800

    Resolve identity from default config.

--- cmd/soroban-cli/src/config/mod.rs
+++ cmd/soroban-cli/src/config/mod.rs
@@ -47,7 +47,12 @@ pub struct Args {
     #[command(flatten)]
     pub network: network::Args,
 
-    #[arg(long, visible_alias = "source", env = "STELLAR_ACCOUNT")]
+    #[arg(
+        long,
+        visible_alias = "source",
+        env = "STELLAR_ACCOUNT",
+        default_value = ""
+    )]
     /// Account that where transaction originates from. Alias `source`.
     /// Can be an identity (--source alice), a public key (--source GDKW...),
     /// a muxed account (--source MDA…), a secret key (--source SC36…),
