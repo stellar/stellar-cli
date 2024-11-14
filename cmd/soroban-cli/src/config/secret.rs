d4e25003db21f05fef829d600e98ff25515342bc
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-11-14 16:25:03 -0500

    Add Secret::Keychain

--- cmd/soroban-cli/src/config/secret.rs
+++ cmd/soroban-cli/src/config/secret.rs
@@ -34,11 +34,15 @@ pub enum Error {
 pub struct Args {
     /// Add using `secret_key`
     /// Can provide with `SOROBAN_SECRET_KEY`
-    #[arg(long, conflicts_with = "seed_phrase")]
+    #[arg(long, conflicts_with_all = ["seed_phrase", "keychain"])]
     pub secret_key: bool,
     /// Add using 12 word seed phrase to generate `secret_key`
-    #[arg(long, conflicts_with = "secret_key")]
+    #[arg(long, conflicts_with_all = ["secret_key", "keychain"])]
     pub seed_phrase: bool,
+
+    /// Add using `keychain`
+    #[arg(long, conflicts_with_all = ["seed_phrase", "secret_key"])]
+    pub keychain: bool,
 }
 
 impl Args {
@@ -67,6 +71,15 @@ impl Args {
                     .collect::<Vec<_>>()
                     .join(" "),
             })
+        } else if self.keychain {
+            // generate a secret, and save it in the keychain
+            // return a new type of secret?
+            // for now, put it all in here
+            println!("generate a secret in the keychain");
+            // let keychain = keyring::Keyring::new("
+            Ok(Secret::SecretKey {
+                secret_key: "test".to_owned(),
+            })
         } else {
             Err(Error::PasswordRead {})
         }
@@ -78,6 +91,7 @@ impl Args {
 pub enum Secret {
     SecretKey { secret_key: String },
     SeedPhrase { seed_phrase: String },
+    Keychain,
 }
 
 impl FromStr for Secret {
@@ -92,6 +106,8 @@ impl FromStr for Secret {
             Ok(Secret::SeedPhrase {
                 seed_phrase: s.to_string(),
             })
+        } else if s == "keychain" {
+            Ok(Secret::Keychain)
         } else {
             Err(Error::InvalidAddress(s.to_string()))
         }
@@ -116,6 +132,7 @@ impl Secret {
                     .private()
                     .0,
             )?,
+            Secret::Keychain => panic!("Keychain does not reveal secret key"),
         })
     }
 
@@ -132,6 +149,7 @@ impl Secret {
                 let key = self.key_pair(index)?;
                 SignerKind::Local(LocalKey { key })
             }
+            Secret::Keychain => todo!(),
         };
         Ok(Signer { kind, print })
     }
@@ -159,4 +177,79 @@ impl Secret {
 fn read_password() -> Result<String, Error> {
     std::io::stdout().flush().map_err(|_| Error::PasswordRead)?;
     rpassword::read_password().map_err(|_| Error::PasswordRead)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_SECRET_KEY: &str = "SBF5HLRREHMS36XZNTUSKZ6FTXDZGNXOHF4EXKUL5UCWZLPBX3NGJ4BH";
+    const TEST_SEED_PHRASE: &str =
+        "depth decade power loud smile spatial sign movie judge february rate broccoli";
+
+    #[test]
+    fn test_secret_from_key() {
+        let secret = Secret::from_str(TEST_SECRET_KEY).unwrap();
+        // assert that it is a Secret::SecretKey
+        match secret {
+            Secret::SecretKey { secret_key: _ } => assert!(true),
+            _ => assert!(false),
+        }
+        // assert that we can get the private key from it
+        let private_key = secret.private_key(None).unwrap();
+        assert_eq!(private_key.to_string(), TEST_SECRET_KEY);
+
+        let signer = secret.signer(None, Print::new(false)).unwrap();
+        println!("signer: {:?}", signer.kind);
+    }
+
+    #[test]
+    fn test_secret_from_seed_phrase() {
+        let secret = Secret::from_str(TEST_SEED_PHRASE).unwrap();
+        match secret {
+            Secret::SeedPhrase { seed_phrase: _ } => assert!(true),
+            _ => assert!(false),
+        }
+
+        let private_key = secret.private_key(None).unwrap();
+        assert_eq!(private_key.to_string(), TEST_SECRET_KEY);
+    }
+
+    #[test]
+    fn test_ledger_secret() {
+        let secret = Secret::from_str("ledger").unwrap();
+        match secret {
+            Secret::Ledger => assert!(true),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_ledger_secret_will_not_reveal_private_key() {
+        let secret = Secret::from_str("ledger").unwrap();
+        secret.private_key(None).unwrap();
+    }
+
+    #[test]
+    fn test_keychain_secret() {
+        let keychain_secret = Secret::from_str("keychain").unwrap();
+        match keychain_secret {
+            Secret::Keychain => assert!(true),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_keychain_secret_will_not_reveal_private_key() {
+        let secret = Secret::from_str("keychain").unwrap();
+        secret.private_key(None).unwrap();
+    }
+
+    #[test]
+    fn test_secret_from_invalid_string() {
+        let secret = Secret::from_str("invalid");
+        assert!(secret.is_err());
+    }
 }
