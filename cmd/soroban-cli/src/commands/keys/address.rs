3ad6b9b482d18f3d035560d2bfe841171c71d65d
Elizabeth Engelman <4752801+elizabethengelman@users.noreply.github.com>
2024-12-03 17:23:16 -0500

    Use resolve_muxed_account to get public key

--- cmd/soroban-cli/src/commands/keys/address.rs
+++ cmd/soroban-cli/src/commands/keys/address.rs
@@ -1,7 +1,9 @@
-use crate::commands::config::secret;
-
-use super::super::config::locator;
 use clap::arg;
+
+use crate::{
+    commands::config::{address, locator, secret},
+    xdr,
+};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -13,13 +15,16 @@ pub enum Error {
 
     #[error(transparent)]
     StrKey(#[from] stellar_strkey::DecodeError),
+
+    #[error(transparent)]
+    Address(#[from] address::Error),
 }
 
 #[derive(Debug, clap::Parser, Clone)]
 #[group(skip)]
 pub struct Cmd {
     /// Name of identity to lookup, default test identity used if not provided
-    pub name: String,
+    pub name: address::Address,
 
     /// If identity is a seed phrase use this hd path, default is 0
     #[arg(long)]
@@ -35,20 +40,15 @@ impl Cmd {
         Ok(())
     }
 
-    pub fn private_key(&self) -> Result<ed25519_dalek::SigningKey, Error> {
-        Ok(self
-            .locator
-            .read_identity(&self.name)?
-            .key_pair(self.hd_path)?)
-    }
-
     pub fn public_key(&self) -> Result<stellar_strkey::ed25519::PublicKey, Error> {
-        if let Ok(key) = stellar_strkey::ed25519::PublicKey::from_string(&self.name) {
-            Ok(key)
-        } else {
-            Ok(stellar_strkey::ed25519::PublicKey::from_payload(
-                self.private_key()?.verifying_key().as_bytes(),
-            )?)
+        match self
+            .name
+            .resolve_muxed_account(&self.locator, self.hd_path)?
+        {
+            xdr::MuxedAccount::Ed25519(pk) => Ok(stellar_strkey::ed25519::PublicKey(pk.0)),
+            xdr::MuxedAccount::MuxedEd25519(xdr::MuxedAccountMed25519 { ed25519, .. }) => {
+                Ok(stellar_strkey::ed25519::PublicKey(ed25519.0))
+            }
         }
     }
 }
