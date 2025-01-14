8e234bc8049ce710c3206d2ebab726a9c678ec3f
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-01-14 13:49:08 -0500

    Use printer for printing prompt when reading password from cli

--- cmd/soroban-cli/src/commands/keys/add.rs
+++ cmd/soroban-cli/src/commands/keys/add.rs
@@ -6,7 +6,7 @@ use sep5::SeedPhrase;
 use crate::{
     commands::global,
     config::{address::KeyName, locator, secret::{self, Secret}},
-    print::Print, signer::secure_store::{self, SecureStore},
+    print::{self, Print}, signer::secure_store::{self, SecureStore},
 };
 
 #[derive(thiserror::Error, Debug)]
@@ -43,30 +43,32 @@ pub struct Cmd {
 impl Cmd {
     pub fn run(&self, global_args: &global::Args) -> Result<(), Error> {
         let print = Print::new(global_args.quiet);
-        let secret = self.read_secret()?;
+        let secret = self.read_secret(&print)?;
         let path = self.config_locator.write_identity(&self.name, &secret)?;
         print.checkln(format!("Key saved with alias {:?} in {path:?}", self.name));
         Ok(())
     }
 
-    fn read_secret(&self) -> Result<Secret, Error> {
+    fn read_secret(&self, print: &Print) -> Result<Secret, Error> {
         if let Ok(secret_key) = std::env::var("SOROBAN_SECRET_KEY") {
             return Ok(Secret::SecretKey { secret_key })
-        }
+        } else if self.secrets.secure_store {
+            let prompt = "Type a 12 or 24 word seed phrase:";
+            let secret_key = read_password(print, prompt)?;
 
-        println!("Type a secret key or 12/24 word seed phrase:");
-        let secret_key = read_password()?;
-        if self.secrets.secure_store {
             let seed_phrase: SeedPhrase = secret_key.parse()?;
-            let print = &Print::new(false);
+
             Ok(SecureStore::save_secret(print, &self.name, seed_phrase)?)
         } else {
+            let prompt = "Type a secret key or 12/24 word seed phrase:";
+            let secret_key = read_password(print, prompt)?;
             Ok(secret_key.parse()?)
         }
     }
 }
 
-fn read_password() -> Result<String, Error> {
+fn read_password(print: &Print, prompt: &str) -> Result<String, Error> {
+    print.arrowln(prompt);
     std::io::stdout().flush().map_err(|_| Error::PasswordRead)?;
     rpassword::read_password().map_err(|_| Error::PasswordRead)
 }
