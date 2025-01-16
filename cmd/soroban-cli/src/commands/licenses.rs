fd332e9edd71e30750fdf15e28d4e70b09dcf1cb
Nando Vieira <me@fnando.com>
2025-01-16 11:16:36 -0800

    Make command functional.

--- cmd/soroban-cli/src/commands/licenses.rs
+++ cmd/soroban-cli/src/commands/licenses.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use std::fmt::Debug;
 
+use license_fetcher::get_package_list_macro;
+
 #[derive(Parser, Debug, Clone)]
 #[group(skip)]
 pub struct Cmd;
@@ -8,6 +10,28 @@ pub struct Cmd;
 impl Cmd {
     #[allow(clippy::unused_self)]
     pub fn run(&self) {
-        println!("show licenses");
+        let package_list = get_package_list_macro!();
+
+        package_list.iter().for_each(|pkg| {
+            println!(
+                "Name: {name}\nVersion: {version}\nLicense: {license}",
+                name = pkg.name,
+                version = pkg.version,
+                license = pkg
+                    .license_identifier
+                    .clone()
+                    .unwrap_or("Unknown".to_string()),
+            );
+
+            if let Some(repo) = pkg.repository.clone() {
+                println!("Repo: {repo}");
+            }
+
+            if let Some(url) = pkg.homepage.clone() {
+                println!("URL: {url}");
+            }
+
+            println!();
+        });
     }
 }
