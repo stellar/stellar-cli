560d07f173d9efb43ec8bc525c50e40486cacb07
Nando Vieira <me@fnando.com>
2025-01-20 16:08:13 -0800

    Add an explanatory comment to the generated file.

--- cmd/soroban-cli/src/utils/contract-template/src/lib.rs
+++ cmd/soroban-cli/src/utils/contract-template/src/lib.rs
@@ -4,6 +4,12 @@ use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
 #[contract]
 pub struct Contract;
 
+// This is a sample contract. Replace this placeholder with your own contract logic.
+// A corresponding test example is available in `test.rs`.
+//
+// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
+// The repository includes use cases for the Stellar ecosystem, such as data storage on
+// the blockchain, token swaps, liquidity pools, and more.
 #[contractimpl]
 impl Contract {
     pub fn hello(env: Env, to: String) -> Vec<String> {
