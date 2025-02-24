08cd5610eba4e27d519166f42d1f7e8ff29a317a
Gleb <gleb@stellar.org>
2025-02-24 11:03:34 -0800

    Implement stellar tx to accept string/file as an input (#1885)
    * Implement stellar tx to accept string/file as an input
    
    * Update help doc
    
    * PR comments

--- cmd/soroban-cli/src/commands/tx/xdr.rs
+++ cmd/soroban-cli/src/commands/tx/xdr.rs
@@ -1,20 +1,17 @@
-use std::{
-    io::{stdin, Read},
-    path::PathBuf,
-};
-
 use crate::xdr::{
     Limits, Operation, ReadXdr, Transaction, TransactionEnvelope, TransactionV1Envelope,
 };
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::Cursor;
+use std::io::{stdin, Read};
+use std::path::Path;
+use stellar_xdr::curr::Limited;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("failed to decode XDR from base64")]
-    Base64Decode,
-    #[error("failed to decode XDR from file: {0}")]
-    FileDecode(PathBuf),
-    #[error("failed to decode XDR from stdin")]
-    StdinDecode,
+    #[error("failed to decode XDR: {0}")]
+    XDRDecode(#[from] stellar_xdr::curr::Error),
     #[error(transparent)]
     Io(#[from] std::io::Error),
     #[error("only transaction v1 is supported")]
@@ -23,16 +20,49 @@ pub enum Error {
     TooManyOperations,
 }
 
-pub fn tx_envelope_from_stdin() -> Result<TransactionEnvelope, Error> {
-    from_stdin()
+pub fn tx_envelope_from_input(input: &Option<OsString>) -> Result<TransactionEnvelope, Error> {
+    let read: &mut dyn Read = if let Some(input) = input {
+        let exist = Path::new(input).try_exists();
+        if let Ok(true) = exist {
+            &mut File::open(input)?
+        } else {
+            &mut Cursor::new(input.clone().into_encoded_bytes())
+        }
+    } else {
+        &mut stdin()
+    };
+
+    let mut lim = Limited::new(SkipWhitespace::new(read), Limits::none());
+    Ok(TransactionEnvelope::read_xdr_base64_to_end(&mut lim)?)
 }
-pub fn from_stdin<T: ReadXdr>() -> Result<T, Error> {
-    let mut buf = String::new();
-    let _ = stdin()
-        .read_to_string(&mut buf)
-        .map_err(|_| Error::StdinDecode)?;
-    T::from_xdr_base64(buf.trim(), Limits::none()).map_err(|_| Error::StdinDecode)
+
+// TODO: use SkipWhitespace from rs-stellar-xdr once it's updated to 23.0
+pub struct SkipWhitespace<R: Read> {
+    pub inner: R,
 }
+
+impl<R: Read> SkipWhitespace<R> {
+    pub fn new(inner: R) -> Self {
+        SkipWhitespace { inner }
+    }
+}
+
+impl<R: Read> Read for SkipWhitespace<R> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let n = self.inner.read(buf)?;
+
+        let mut written = 0;
+        for read in 0..n {
+            if !buf[read].is_ascii_whitespace() {
+                buf[written] = buf[read];
+                written += 1;
+            }
+        }
+
+        Ok(written)
+    }
+}
+//
 
 pub fn unwrap_envelope_v1(tx_env: TransactionEnvelope) -> Result<Transaction, Error> {
     let TransactionEnvelope::Tx(TransactionV1Envelope { tx, .. }) = tx_env else {
