5834646a9b426ec61d62c12c43cf2a051f387ab2
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-02-24 16:10:47 -0500

    Move set source-account to \'tx edit source-account set \'

--- cmd/soroban-cli/src/commands/tx/edit/mod.rs
+++ cmd/soroban-cli/src/commands/tx/edit/mod.rs
@@ -1,9 +1,13 @@
 use super::global;
 
+pub mod source_account;
 pub mod sequence_number;
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Cmd {
+    /// Change the source account on a transaction
+    #[command(subcommand, visible_alias = "source")]
+    SourceAccount(source_account::Cmd),
     /// Set the sequence number on a transaction
     #[command(subcommand, visible_alias = "seq-num")]
     SequenceNumber(sequence_number::Cmd)
@@ -12,12 +16,15 @@ pub enum Cmd {
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
+    SourceAccount(#[from] source_account::Error),
+    #[error(transparent)]
     SequenceNumber(#[from] sequence_number::Error)
 }
 
 impl Cmd {
     pub fn run(&self, global_args: &global::Args) -> Result<(), Error> {
         match self {
+            Cmd::SourceAccount(cmd) => cmd.run(global_args)?,
             Cmd::SequenceNumber(cmd) => cmd.run(global_args)?,
         };
         Ok(())
---
+++ cmd/soroban-cli/src/commands/tx/edit/source_account/set.rs
@@ -0,0 +1,59 @@
+use crate::{
+    commands:: {
+        global,
+        tx::xdr::{tx_envelope_from_stdin, Error as XdrParsingError},
+    },
+    config::{address, UnresolvedMuxedAccount},
+    xdr::{
+        self,
+        TransactionEnvelope,
+        WriteXdr,
+    }
+};
+
+
+#[derive(clap::Parser, Debug, Clone)]
+pub struct Cmd { 
+    pub source_account: UnresolvedMuxedAccount
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Address(#[from] address::Error),
+    #[error("only V1 transactions are supported")]
+    Unsupported,
+    #[error(transparent)]
+    XdrStdin(#[from] XdrParsingError),
+    #[error(transparent)]
+    Xdr(#[from] xdr::Error),
+}
+
+impl Cmd {
+    pub fn run(&self, global_args: &global::Args) -> Result<(), Error> { 
+        println!("setting source account");
+        
+        let mut tx = tx_envelope_from_stdin()?;
+        self.update_tx_env(&mut tx, global_args)?;
+        println!("{}", tx.to_xdr_base64(xdr::Limits::none())?);
+        Ok(())
+    }
+
+
+    pub fn update_tx_env(
+        &self,
+        tx_env: &mut TransactionEnvelope,
+        global: &global::Args,
+    ) -> Result<(), Error> {
+        match tx_env {
+            TransactionEnvelope::Tx(transaction_v1_envelope) => {
+                    transaction_v1_envelope.tx.source_account =
+                        self.source_account.resolve_muxed_account_sync(&global.locator, None)?;
+            }
+            TransactionEnvelope::TxV0(_) | TransactionEnvelope::TxFeeBump(_) => {
+                return Err(Error::Unsupported);
+            }
+        };
+        Ok(())
+    }
+}
