82b1f37c3166461f688f1890a99ee712f81e7836
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-05-14 09:35:44 -0400

    Add claimable balance subcommand

---
+++ cmd/soroban-cli/src/commands/ledger/entry/fetch/claimable_balance.rs
@@ -0,0 +1,98 @@
+use crate::commands::config::network;
+use crate::config::locator;
+use crate::rpc;
+use clap::{command, Parser};
+use hex::FromHexError;
+use soroban_spec_tools::utils::padded_hex_from_str;
+use stellar_xdr::curr::{
+    ClaimableBalanceId::ClaimableBalanceIdTypeV0,  Hash,
+    LedgerKey, LedgerKeyClaimableBalance,
+};
+
+#[derive(Parser, Debug, Clone)]
+#[group(skip)]
+pub struct Cmd {
+    #[command(flatten)]
+    pub network: network::Args,
+
+    #[command(flatten)]
+    pub locator: locator::Args,
+
+    /// Claimable Balance Ids to fetch an entry for
+    pub ids: Vec<String>,
+     
+    /// Format of the output
+    #[arg(long, default_value = "json")]
+    pub output: OutputFormat,
+
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    FromHexError(#[from] FromHexError),
+    #[error("provided hash value is invalid: {0}")]
+    InvalidHash(String),
+    #[error(transparent)]
+    Network(#[from] network::Error),
+    #[error(transparent)]
+    Rpc(#[from] rpc::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+} 
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ValueEnum, Default)]
+pub enum OutputFormat {
+    /// JSON output of the ledger entry with parsed XDRs (one line, not formatted)
+    #[default]
+    Json,
+    /// Formatted (multiline) JSON output of the ledger entry with parsed XDRs
+    JsonFormatted,
+    /// Original RPC output (containing XDRs)
+    Xdr,
+}
+
+impl Cmd {
+    pub async fn run(&self) -> Result<(), Error> {
+        let network = self.network.get(&self.locator)?;
+        let client = network.rpc_client()?;
+        let mut ledger_keys = vec![];
+
+        self.insert_keys(&mut ledger_keys)?;
+
+        match self.output {
+            OutputFormat::Json => {
+                let resp = client.get_full_ledger_entries(&ledger_keys).await?;
+                println!("{}", serde_json::to_string(&resp)?);
+            }
+            OutputFormat::Xdr => {
+                let resp = client.get_ledger_entries(&ledger_keys).await?;
+                println!("{}", serde_json::to_string(&resp)?);
+            }
+            OutputFormat::JsonFormatted => {
+                let resp = client.get_full_ledger_entries(&ledger_keys).await?;
+                println!("{}", serde_json::to_string_pretty(&resp)?);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn insert_keys(
+        &self,
+        ledger_keys: &mut Vec<LedgerKey>,
+    ) -> Result<(), Error> {
+        for x in &self.ids {
+            let padded_hex = padded_hex_from_str(&x, 32)?;
+            let hash_bytes: [u8; 32] = padded_hex
+                .try_into()
+                .map_err(|_| Error::InvalidHash(x.to_string()))?;
+            let hash = Hash(hash_bytes);
+            let key = LedgerKey::ClaimableBalance(LedgerKeyClaimableBalance {
+                balance_id: ClaimableBalanceIdTypeV0(hash),
+            });
+            ledger_keys.push(key);
+        }
+        Ok(())
+    }
+}
--- cmd/soroban-cli/src/commands/ledger/entry/fetch/mod.rs
+++ cmd/soroban-cli/src/commands/ledger/entry/fetch/mod.rs
@@ -4,12 +4,14 @@ use clap::Parser;
 pub mod account;
 pub mod contract;
 pub mod config;
+pub mod claimable_balance;
 
 #[derive(Debug, Parser)]
 pub enum Cmd {
     Account(account::Cmd),
     Contract(contract::Cmd),
     Config(config::Cmd),
+    ClaimableBalance(claimable_balance::Cmd),
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -20,6 +22,8 @@ pub enum Error {
     Contract(#[from] contract::Error),
     #[error(transparent)]
     Config(#[from] config::Error),
+    #[error(transparent)]
+    ClaimableBalance(#[from] claimable_balance::Error),
 }
 
 impl Cmd {
@@ -28,6 +32,7 @@ impl Cmd {
             Cmd::Account(cmd) => cmd.run().await?,
             Cmd::Contract(cmd) => cmd.run().await?,
             Cmd::Config(cmd) => cmd.run().await?,
+            Cmd::ClaimableBalance(cmd) => cmd.run().await?,
         }
         Ok(())
     }
