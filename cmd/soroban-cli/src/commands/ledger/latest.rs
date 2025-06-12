e79ade8b97b08d15a94d096a949b835bbe1c81ee
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-06-12 13:26:34 -0400

    Add ledger latest

---
+++ cmd/soroban-cli/src/commands/ledger/latest.rs
@@ -0,0 +1,58 @@
+use crate::{
+    commands::global,
+    config::network,
+    rpc,
+};
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+    #[error(transparent)]
+    Network(#[from] network::Error),
+    #[error(transparent)]
+    Rpc(#[from] rpc::Error),
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ValueEnum, Default)]
+pub enum OutputFormat {
+    /// Text output of network info
+    #[default]
+    Text,
+    /// JSON result of the RPC request
+    Json,
+    /// Formatted (multiline) JSON output of the RPC request
+    JsonFormatted,
+}
+
+#[derive(Debug, clap::Parser)]
+pub struct Cmd {
+    pub seq: Option<i64>,
+
+    #[command(flatten)]
+    pub network: network::Args,
+
+    /// Format of the output
+    #[arg(long, default_value = "text")]
+    pub output: OutputFormat,
+}
+
+impl Cmd {
+    pub async fn run(&self, global_args: &global::Args) -> Result<(), Error> {
+        let network = self.network.get(&global_args.locator)?;
+        let client = network.rpc_client()?;
+        let ledger = client.get_latest_ledger().await?;
+
+        match self.output {
+            OutputFormat::Text => {
+                println!("Sequence: {}", ledger.sequence);
+                println!("Protocol Version: {}", ledger.protocol_version);
+                println!("ID: {}", ledger.id);
+            },
+            OutputFormat::Json => println!("{}", serde_json::to_string(&ledger)?),
+            OutputFormat::JsonFormatted => println!("{}", serde_json::to_string_pretty(&ledger)?),
+        };
+
+        Ok(())
+    }
+}
---
+++ cmd/soroban-cli/src/commands/ledger/mod.rs
@@ -0,0 +1,25 @@
+use crate::{
+    commands::global,
+};
+use clap::Subcommand;
+mod latest;
+
+#[derive(Debug, Subcommand)]
+pub enum Cmd {
+    Latest(latest::Cmd),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Latest(#[from] latest::Error),
+}
+
+impl Cmd {
+    pub async fn run(&self,  global_args: &global::Args) -> Result<(), Error> {
+        match &self {
+            Cmd::Latest(cmd) => cmd.run(global_args).await?,
+        }
+        Ok(())
+    }
+}
