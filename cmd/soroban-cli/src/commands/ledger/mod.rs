ec93ae81d3bd3240a2202bc633c20ed5496925da
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-06-23 11:52:53 -0400

    WIP: stellar ledger fetch

--- cmd/soroban-cli/src/commands/ledger/mod.rs
+++ cmd/soroban-cli/src/commands/ledger/mod.rs
@@ -1,23 +1,30 @@
 use crate::commands::global;
 use clap::Subcommand;
 mod latest;
+mod fetch;
 
 #[derive(Debug, Subcommand)]
 pub enum Cmd {
     /// Get the latest ledger sequence and information from the network
     Latest(latest::Cmd),
+    Fetch(fetch::Cmd),
+
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
     Latest(#[from] latest::Error),
+    #[error(transparent)]
+    Fetch(#[from] fetch::Error),
 }
 
 impl Cmd {
     pub async fn run(&self, global_args: &global::Args) -> Result<(), Error> {
         match &self {
             Cmd::Latest(cmd) => cmd.run(global_args).await?,
+            Cmd::Fetch(cmd) => cmd.run(global_args).await?,
+
         }
         Ok(())
     }
