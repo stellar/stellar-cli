449019c7610d4000c1b594bfba26aebff3dcebeb
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-06-24 15:31:44 -0400

    Print out header and metadata xdr as json

--- cmd/soroban-cli/src/commands/ledger/fetch.rs
+++ cmd/soroban-cli/src/commands/ledger/fetch.rs
@@ -1,56 +1,83 @@
+use soroban_rpc::GetLedgersResponse;
+
 use crate::{
     commands::global,
     config::network,
     rpc,
-    xdr::{self, Hash},
+    xdr::{self, Hash, ReadXdr},
 };
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-        #[error(transparent)]
+    #[error(transparent)]
     Network(#[from] network::Error),
     #[error(transparent)]
     Rpc(#[from] rpc::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ValueEnum, Default)]
+pub enum OutputFormat {
+    /// Text output of network info
+    #[default]
+    Text,
+    /// JSON result of the RPC request
+    Json,
+    /// Formatted (multiline) JSON output of the RPC request
+    JsonFormatted,
 }
 
 #[derive(Debug, clap::Parser)]
 pub struct Cmd {
+    /// Ledger Sequence to start fetch (inclusive)
     pub seq: u32,
+
+    /// Number of ledgers to fetch 
+    #[arg(long, default_value_t=1)]
+    pub limit: usize,
 
     #[command(flatten)]
     pub network: network::Args,
+
+    /// Format of the output
+    #[arg(long, value_enum, default_value_t)]
+    pub output: OutputFormat,
 }
 
 impl Cmd {
     pub async fn run(&self, global_args: &global::Args) -> Result<(), Error> {
         let network = self.network.get(&global_args.locator)?;
         let client = network.rpc_client()?;
-        let result = client.get_ledgers(self.start(), Some(1)).await;
-        println!("RESULT: {:?}", result);
+        let result = client.get_ledgers(self.start(), Some(self.limit)).await?;
+
+        match self.output {
+            OutputFormat::Text => {
+                let start = match self.start() {
+                    soroban_rpc::LedgerStart::Ledger(l) => l.to_string(),
+                    soroban_rpc::LedgerStart::Cursor(c) => c,
+                };
+                println!("Latest Ledger {} at close time {}", result.latest_ledger, result.latest_ledger_close_time);
+                println!("Oldest Ledger {} at close time {}", result.oldest_ledger, result.oldest_ledger_close_time);
+                println!("Ledgers (limit {} starting at {} )", self.limit, start);
+                println!("----------------------------------------------------\n");
+                for ledger in result.ledgers.clone() {
+                    let ledger_header = xdr::LedgerHeaderHistoryEntry::from_xdr_base64(&ledger.header_xdr, xdr::Limits::none()).unwrap();
+                    let ledger_meta = xdr::LedgerCloseMeta::from_xdr_base64(&ledger.metadata_xdr, xdr::Limits::none()).unwrap();
+                    println!("Ledger Sequence: {}", ledger.sequence);
+                    println!("Hash: {}", ledger.hash);
+                    println!("Close Time: {}", ledger.ledger_close_time);
+                    println!("Header: {}", serde_json::to_string_pretty(&ledger_header)?);
+                    println!("Meta XDR: {}", serde_json::to_string_pretty(&ledger_meta)?);
+                    println!("----------------------------------------------------\n");
+                } 
+            },
+            OutputFormat::Json => println!("{}", serde_json::to_string(&result)?),
+            OutputFormat::JsonFormatted => println!("{}", serde_json::to_string_pretty(&result)?),
+        }
+
         Ok(())
     }
-
-    // pub async fn fetch_transaction(
-    //     &self,
-    //     global_args: &global::Args,
-    // ) -> Result<GetTransactionResponse, Error> {
-    //     let network = self.network.get(&global_args.locator)?;
-    //     let client = network.rpc_client()?;
-    //     let tx_hash = self.hash.clone();
-    //     let tx = client.get_transaction(&tx_hash).await?;
-    //     match tx.status.clone() {
-    //         val if val == *"NOT_FOUND" => {
-    //             if let Some(n) = &self.network.network {
-    //                 return Err(Error::NotFound {
-    //                     tx_hash,
-    //                     network: n.to_string(),
-    //                 });
-    //             }
-    //         }
-    //         _ => {}
-    //     }
-    //     Ok(tx)
-    // }
 
     fn start(&self) -> rpc::LedgerStart {
         rpc::LedgerStart::Ledger(self.seq)
