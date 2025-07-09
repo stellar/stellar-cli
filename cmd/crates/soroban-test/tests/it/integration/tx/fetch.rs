e8bd5be5596c0df1d0744cf2bf98ee1e3e108e36
Nando Vieira <me@fnando.com>
2025-07-09 12:13:11 -0700

    Fix `stellar tx fetch` tests.

--- cmd/crates/soroban-test/tests/it/integration/tx/fetch.rs
+++ cmd/crates/soroban-test/tests/it/integration/tx/fetch.rs
@@ -55,7 +55,7 @@ async fn tx_fetch() {
         .stdout_as_str();
 
     let parsed: TransactionMeta = serde_json::from_str(&output).unwrap();
-    assert!(matches!(parsed, TransactionMeta::V3 { .. }));
+    assert!(matches!(parsed, TransactionMeta::V4 { .. }));
 
     // fetch the tx envelope
     let output = sandbox
@@ -153,7 +153,7 @@ async fn tx_fetch_xdr_output() {
         .stdout_as_str();
 
     let parsed_xdr = TransactionMeta::from_xdr_base64(output, Limits::none()).unwrap();
-    assert!(matches!(parsed_xdr, TransactionMeta::V3 { .. }));
+    assert!(matches!(parsed_xdr, TransactionMeta::V4 { .. }));
 
     // fetch the tx envelope
     let output = sandbox
