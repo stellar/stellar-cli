8429f97bc30fd1f64afa95683af1f9ff39dcb984
Nando Vieira <me@fnando.com>
2025-07-18 09:33:41 -0700

    Add command to show the global config location.

--- cmd/soroban-cli/src/commands/cfg/mod.rs
+++ cmd/soroban-cli/src/commands/cfg/mod.rs
@@ -1,4 +1,5 @@
 mod migrate;
+mod show;
 
 use clap::Parser;
 
@@ -6,25 +7,33 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 pub enum Cmd {
     /// Migrate the local configuration to the global directory.
+    Migrate(migrate::Cmd),
+
+    /// Show the global configuration directory.
     ///
     /// The location will depend on how your system is configured.
     ///
-    /// - It looks up for `XDG_CONFIG_HOME` environment variable.
+    /// - It looks up for `XDG_CONFIG_HOME` environment variable. If it's set,
+    ///  `$XDG_CONFIG_HOME/stellar` will be used.
     /// - If not set, it defaults to `$HOME/.config`.
     /// - Can be overridden by `--config-dir` flag.
-    Migrate(migrate::Cmd),
+    Show(show::Cmd),
 }
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
     Migrate(#[from] migrate::Error),
+
+    #[error(transparent)]
+    Show(#[from] show::Error),
 }
 
 impl Cmd {
     pub fn run(&self) -> Result<(), Error> {
         match self {
             Cmd::Migrate(cmd) => cmd.run()?,
+            Cmd::Show(cmd) => cmd.run()?,
         }
         Ok(())
     }
