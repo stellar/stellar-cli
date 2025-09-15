5184f8da957fbf2ff3987809247646c2145bb13c
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-09-15 11:41:03 -0400

    Remove redundant ledger field

--- cmd/soroban-cli/src/config/data.rs
+++ cmd/soroban-cli/src/config/data.rs
@@ -186,7 +186,6 @@ impl TryFrom<GetTransactionResponse> for Action {
                 envelope_xdr: res.envelope.as_ref().map(to_xdr).transpose()?,
                 result_xdr: res.result.as_ref().map(to_xdr).transpose()?,
                 result_meta_xdr: res.result_meta.as_ref().map(to_xdr).transpose()?,
-                ledger: res.ledger,
                 events: None,
             },
         })
