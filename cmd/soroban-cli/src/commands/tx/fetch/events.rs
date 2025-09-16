f9877ae9f507a52e7b8d5bf96cebf785557b5cda
elizabethengelman <4752801+elizabethengelman@users.noreply.github.com>
2025-09-16 11:02:23 -0400

    Add text output options for tx events

--- cmd/soroban-cli/src/commands/tx/fetch/events.rs
+++ cmd/soroban-cli/src/commands/tx/fetch/events.rs
@@ -29,22 +29,112 @@ pub enum EventsOutputFormat {
     /// JSON output of the events with parsed XDRs (one line, not formatted)
     Json,
     /// Formatted (multiline) JSON output of events with parsed XDRs
-    #[default]
     JsonFormatted,
+    /// Human readable event output with parsed XDRs
+    #[default]
+    Text,
 }
 
 impl Cmd {
     pub async fn run(&self, global_args: &global::Args) -> Result<(), Error> {
-        let events = self.args.fetch_transaction(global_args).await?.events;
+        let resp = self.args.fetch_transaction(global_args).await?;
+        let events = &resp.events;
+        let contract_events: &Vec<Vec<xdr::ContractEvent>> = &events.contract_events;
+        let diagnostic_events = &events.diagnostic_events;
+        let transaction_events = &events.transaction_events;
         match self.output {
+            EventsOutputFormat::Text => {
+                args::Args::print_tx_summary(&resp);
+                Self::print_contract_events(contract_events)?;
+                Self::print_transaction_events(transaction_events)?;
+                Self::print_diagnostic_events(diagnostic_events)?;
+            }
+            EventsOutputFormat::JsonFormatted => {
+                args::Args::print_tx_summary(&resp);
+                println!("{}", serde_json::to_string_pretty(&events)?);
+            }
             EventsOutputFormat::Json => {
                 println!("{}", serde_json::to_string(&events)?);
             }
-            EventsOutputFormat::JsonFormatted => {
-                println!("{}", serde_json::to_string_pretty(&events)?);
+        }
+        Ok(())
+    }
+
+    fn get_sc_val_string(val: &xdr::ScVal) -> String {
+        match val {
+            xdr::ScVal::Symbol(sym) => {
+                format!("Symbol: {:?}", sym.to_string())
+            }
+            xdr::ScVal::Address(addr) => {
+                format!("Address: {:?}", addr.to_string())
+            }
+            xdr::ScVal::I128(val) => {
+                format!("I128: {:?}", val.to_string())
+            }
+            other => {
+                format!("Other: {:?}", other)
             }
         }
+    }
 
+    fn print_contract_event(event: &xdr::ContractEvent) -> Result<(), Error> {
+        if let Some(id) = event.contract_id.as_ref() {
+            println!("  Contract Id: {}", id.to_string());
+        }
+
+        match &event.body {
+            xdr::ContractEventBody::V0(body) => {
+                for (i, topic) in body.topics.iter().enumerate() {
+                    println!("  Topic[{i}]: {}", Self::get_sc_val_string(topic));
+                }
+                println!("  Data: {}", Self::get_sc_val_string(&body.data));
+                Ok(())
+            }
+        }
+    }
+
+    fn print_contract_events(events: &Vec<Vec<xdr::ContractEvent>>) -> Result<(), Error> {
+        if events.is_empty() {
+            println!("Contract Events: None");
+            return Ok(());
+        }
+        println!("Contract Events:");
+        for event in events.iter().flatten() {
+            Self::print_contract_event(event)?;
+            println!();
+        }
+        Ok(())
+    }
+
+    fn print_transaction_events(events: &Vec<xdr::TransactionEvent>) -> Result<(), Error> {
+        if events.is_empty() {
+            println!("Transaction Events: None");
+            return Ok(());
+        }
+        println!("Transaction Events:");
+        for event in events {
+            println!("  Transaction State: {:?}", event.stage);
+            Self::print_contract_event(&event.event)?;
+            println!();
+        }
+        Ok(())
+    }
+
+    fn print_diagnostic_events(events: &Vec<xdr::DiagnosticEvent>) -> Result<(), Error> {
+        if events.is_empty() {
+            println!("Diagnostic Events: None");
+            return Ok(());
+        }
+
+        println!("Diagnostic Events:");
+        for event in events {
+            println!(
+                "  In Successful Contract Call: {:?}",
+                event.in_successful_contract_call
+            );
+            Self::print_contract_event(&event.event)?;
+            println!();
+        }
         Ok(())
     }
 }
