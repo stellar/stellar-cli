b3d1ffa557e6816f7289d15bcf3127d7e888fad2
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-02-17 20:01:11 +1100

    remove soroban-spec-json crate and bindings

--- cmd/soroban-cli/src/commands/contract/bindings.rs
+++ cmd/soroban-cli/src/commands/contract/bindings.rs
@@ -9,9 +9,6 @@ pub mod typescript;
 
 #[derive(Debug, clap::Subcommand)]
 pub enum Cmd {
-    /// Generate Json Bindings
-    Json(json::Cmd),
-
     /// Generate Rust bindings
     Rust(rust::Cmd),
 
@@ -37,9 +34,6 @@ pub enum Cmd {
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    Json(#[from] json::Error),
-
-    #[error(transparent)]
     Rust(#[from] rust::Error),
 
     #[error(transparent)]
@@ -64,7 +58,6 @@ pub enum Error {
 impl Cmd {
     pub async fn run(&self) -> Result<(), Error> {
         match &self {
-            Cmd::Json(json) => json.run()?,
             Cmd::Rust(rust) => rust.run()?,
             Cmd::Typescript(ts) => ts.run().await?,
             Cmd::Python(python) => python.run()?,
