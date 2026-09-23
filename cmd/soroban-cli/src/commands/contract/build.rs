d0f6d7b05a7995f9b6677eeb579d6c864180f51b
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-09-23 06:11:26 +0000

    set only STELLAR_CLI_VERSION_MAJOR for sdk

--- cmd/soroban-cli/src/commands/contract/build.rs
+++ cmd/soroban-cli/src/commands/contract/build.rs
@@ -373,9 +373,9 @@ impl Cmd {
             // optimization using markers.
             cmd.env("SOROBAN_SDK_BUILD_SYSTEM_SUPPORTS_SPEC_SHAKING_V2", "1");
 
-            // Set env var to inform the SDK of the CLI version, which the SDK
-            // requires to be the same or greater major version than its own.
-            cmd.env("STELLAR_CLI_VERSION", env!("CARGO_PKG_VERSION"));
+            // Set env var to inform the SDK of the CLI major version, which the
+            // SDK requires to be the same or greater than its own.
+            cmd.env("STELLAR_CLI_VERSION_MAJOR", env!("CARGO_PKG_VERSION_MAJOR"));
 
             let cmd_str = serialize_command(&cmd);
 
